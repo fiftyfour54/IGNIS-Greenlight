767ca745694d32a54ac17ca0b5589af3dd858cdc
NaimSantos <naimsantos45@gmail.com>
2020-04-08 14:24:11 -0300

    Updated cdb and strings call

--- ROTD-JP - 101101XXX/c101101061.lua
+++ ROTD-JP - 101101XXX/c101101061.lua
@@ -7,6 +7,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Excavate equal to the number of rock monsters controlled +5, add 1 rock to hand
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
@@ -50,7 +51,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	end
 	--Cannot special summon monsters, except rock monsters, for rest of the turn
 	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetDescription(aux.Stringid(id,2))
+	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH+EFFECT_FLAG_CLIENT_HINT)
