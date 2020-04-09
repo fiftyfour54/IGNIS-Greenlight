00cab27cbb67d373182d10fab2da7602d5a6b2a6
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2020-04-09 05:25:26 -0400

    Added scripts
    Added scripts for the following cards:
    * Glacial Beast Blizzard Wolf
    * Glacial Beast Polar Penguin

---
+++ CP20 - 1002660XX/script/c100266032.lua
@@ -0,0 +1,66 @@
+--極氷獣ポーラ・ペンギン
+--Glacial Beast Polar Penguin
+--Logical Nonsense
+
+--Substitute ID
+local s,id=GetID()
+function s.initial_effect(c)
+	--If special summoned, return 1 of opponent's monsters to hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.thmtg)
+	e1:SetOperation(s.thmop)
+	c:RegisterEffect(e3)
+	--If destroyed by battle or card effect while on field, return 1 of opponent's cards to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_DESTROYED)
+	e1:SetCountLimit(1,id)
+	e2:SetCondition(s.thccon)
+	e2:SetTarget(s.thctg)
+	e2:SetOperation(s.thcop)
+	c:RegisterEffect(e2)
+end
+	--Specifically lists itself
+s.listed_names={id}
+
+	--Activation legality
+function s.thmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsAbleToHand() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToHand,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+	--Return 1 of opponent's monsters to hand
+function s.thmop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
+end
+	--If this card was destroyed by battle or card effect while on the field
+function s.thccon(e,tp,eg,ep,ev,re,r,rp)
+	return (r&REASON_EFFECT+REASON_BATTLE)~=0 and e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
+end
+	--Activation legality
+function s.thctg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsAbleToHand() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+	--Return 1 of opponent's cards to hand
+function s.thcop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
+end
