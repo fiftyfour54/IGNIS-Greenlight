ad2104260af01805cd0de2dd3126419c99c9ea36
ahtelel <32773013+ahtelel@users.noreply.github.com>
2020-04-10 14:52:21 +0700

    Added Dragma Saint Ecclesia, Apostle of the Nadir, Dragma Encounter (untested)

--- ROTD-JP - 101101XXX/c101101052.lua
+++ ROTD-JP - 101101XXX/c101101052.lua
@@ -1,6 +1,6 @@
 --天底の使徒
---Disciple of the Nadir
---Scripted by AlphaKretin
+--Apostle of the Nadir
+--Scripted by ahtelel
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -9,32 +9,32 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-	e1:SetTarget(s.thtg)
-	e1:SetOperation(s.thop)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_names={CARD_ALBUS}
 s.listed_series={0x244}
-function s.tgfilter(c,tp)
-	return c:IsAbleToGrave() and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,c)
+s.listed_names={CARD_ALBUS}
+function s.tgfilter(c,e,tp)
+	return c:IsAbleToGrave() and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,c:GetAttack(),e,tp,c)
 end
-function s.thfilter(c,tc)
-	return (c:IsCode(CARD_ALBUS) or c:IsSetCard(0x244)) and c:IsAttackBelow(tc:GetAttack()) and c:IsAbleToHand()
-end 
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_EXTRA,0,1,nil) end
+function s.thfilter2(c,atk)
+	return (c:IsCode(CARD_ALBUS) or c:IsSetCard(0x244)) and c:GetAttack()<=atk and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_EXTRA)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local tc=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_EXTRA,0,1,1,nil):GetFirst()
-	if tc and Duel.SendtoGrave(tc,REASON_EFFECT)~=0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,tc)
-		if #g>0 then
-			Duel.SendtoHand(g,nil,REASON_EFFECT)
-			Duel.ConfirmCards(1-tp,g)
-		end
+	local g1=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+	e:SetLabel(g1:GetFirst():GetAttack())
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g2=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e:GetLabel())
+	if #g1>0 and #g2>0 then
+		Duel.SendtoHand(g2,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g2)
+		Duel.SendtoGrave(g1,REASON_EFFECT)
 	end
 end
