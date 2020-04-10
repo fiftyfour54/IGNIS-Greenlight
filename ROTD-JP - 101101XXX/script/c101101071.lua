44704201f28e995f991a0f74425925a4287c506e
EerieCode <eeriecode@gmail.com>
2020-04-10 22:56:45 +0200

    Bug fixes and CDB update

--- ROTD-JP - 101101XXX/script/c101101071.lua
+++ ROTD-JP - 101101XXX/script/c101101071.lua
@@ -3,25 +3,25 @@
 --Scripted by ahtelel
 local s,id=GetID()
 function s.initial_effect(c)
-	--activate
+	--
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_SPSUMMON)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetTarget(s.thtg)
-	e1:SetOperation(s.thop)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	--
+	--activate
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_SPSUMMON)
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetType(EFFECT_TYPE_ACTIVATE)
 	e2:SetCode(EVENT_FREE_CHAIN)
-	e2:SetTarget(s.sptg)
-	e2:SetOperation(s.spop)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
 end
 s.listed_series={0x244}
@@ -61,7 +61,7 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 					return sc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP) and ft>0 end,
 					function(c)
 					Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP) end,
-					aux.Stringid(id,1)
+					aux.Stringid(id,2)
 				)
 		else
 			Duel.SendtoHand(sc,nil,REASON_EFFECT)
