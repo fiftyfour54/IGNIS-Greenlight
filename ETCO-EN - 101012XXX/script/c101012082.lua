2a752abdf86fa9815fe875ff319182dd9da8b476
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-04-17 01:06:50 +0300

    "Scrypton" hint message update

--- ETCO-EN - 101012XXX/script/c101012082.lua
+++ ETCO-EN - 101012XXX/script/c101012082.lua
@@ -56,7 +56,7 @@ end
 function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_REMOVED) and chkc:IsFaceup() and s.tdfilter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(aux.FilterFaceupFunction(s.tdfilter),tp,LOCATION_REMOVED,LOCATION_REMOVED,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
 	local g=Duel.SelectTarget(tp,aux.FilterFaceupFunction(s.tdfilter),tp,LOCATION_REMOVED,LOCATION_REMOVED,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,LOCATION_REMOVED)
 end
