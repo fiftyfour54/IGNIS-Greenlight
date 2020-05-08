60a4dd72918d600e30d59e0cad243e60e1263e77
AlphaKretin <deewhydeeecks@gmail.com>
2020-05-08 15:51:57 +0930

    Update "Numeron Gate" monsters
    For "Numeron Network" and style

--- CP20 - 1002660XX/script/c100266022.lua
+++ CP20 - 1002660XX/script/c100266022.lua
@@ -1,20 +1,16 @@
+--Ｎｏ．１ ゲート・オブ・ヌメロン－エーカム
 --Number 1: Numeron Gate Ekam
---Number 2: Numeron Gate Dve
---Number 3: Numeron Gate Trini
---Number 4: Numeron Gate Catvari
 local s,id=GetID()
 function s.initial_effect(c)
 	--xyz summon
 	Xyz.AddProcedure(c,nil,1,3)
 	c:EnableReviveLimit()
-	
 	--cannot be destroyed by battle
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
 	e1:SetValue(1)
 	c:RegisterEffect(e1)
-	
 	--attack up
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_ATKCHANGE)
@@ -22,21 +18,14 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_DAMAGE_STEP_END)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCost(s.cost)
+	e2:SetCost(aux.NumeronDetachCost)
 	e2:SetOperation(s.operation)
 	c:RegisterEffect(e2,false,REGISTER_FLAG_DETACH_XMAT)
 end
-
 s.listed_series={0x246}
 function s.filter(c)
     return c:IsFaceup() and c:IsSetCard(0x246)
 end
-
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
-	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
-end
-
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
     local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil)
     local tc=g:GetFirst()
