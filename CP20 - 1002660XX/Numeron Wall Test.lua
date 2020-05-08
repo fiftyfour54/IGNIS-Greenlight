bb50de2893ed563acef6671007b3c5c90217b664
KingYamato <58759661+KingYamato@users.noreply.github.com>
2020-05-08 02:22:37 -0300

    Add Numeron Wall Test

---
+++ CP20 - 1002660XX/Numeron Wall Test.lua
@@ -0,0 +1,12 @@
+Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI,5)
+Debug.SetPlayerInfo(0,8000,0,0)
+Debug.SetPlayerInfo(1,8000,0,0)
+
+Debug.AddCard(32274490,1,1,LOCATION_MZONE,2,1,true)
+
+Debug.AddCard(100266020,0,0,LOCATION_HAND,0,8)
+Debug.AddCard(27125110,0,0,LOCATION_HAND,0,8)
+
+Debug.AddCard(100266026,0,0,LOCATION_DECK,0,8)
+
+Debug.ReloadFieldEnd()
