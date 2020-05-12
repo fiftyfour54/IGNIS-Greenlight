7d83840162f04c46f52d102c34ad64562b1c60a1
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-05-12 12:07:30 +0200

    Thousand eyes Spell (Prototype)
    Thousand Eyes Spell with fusion part (need help for ritual part)

---
+++ Speed Duel Starter 05-2020/script/c300204003.lua
@@ -0,0 +1,94 @@
+--Thousand-Eyes Spell
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return aux.CanActivateSkill(tp)
+	and (s.fustarget(e,tp,eg,ep,ev,re,r,rp,0) 
+	-- or s.rittarget(e,tp,eg,ep,ev,re,r,rp,0)
+	)
+end
+s.listed_names={64631466,63519819}
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--cost
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD,nil)
+	--ritual
+	local g1=false	
+	--fusion
+	local g2=s.fustarget(e,tp,eg,ep,ev,re,r,rp,0)
+
+	local opt=0
+	if g1 and g2 then
+		opt=Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2))
+	elseif g1 then
+		opt=Duel.SelectOption(tp,aux.Stringid(id,1))
+	elseif g2 then
+		opt=Duel.SelectOption(tp,aux.Stringid(id,2))+1
+	else return end
+	if opt==0 then
+		
+	else
+		--fusion summoned
+		local chkf=tp
+		local mg1=Duel.GetFusionMaterial(tp):Filter(s.filter1,nil,e)
+		local sg1=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf)
+		local mg2=nil
+		local sg2=nil
+		local ce=Duel.GetChainMaterial(tp)
+		if ce~=nil then
+			local fgroup=ce:GetTarget()
+			mg2=fgroup(ce,e,tp)
+			local mf=ce:GetValue()
+			sg2=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg2,mf,chkf)
+		end
+		if sg1:GetCount()>0 or (sg2~=nil and sg2:GetCount()>0) then
+			local sg=sg1:Clone()
+			if sg2 then sg:Merge(sg2) end
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local tg=sg:Select(tp,1,1,nil)
+			local tc=tg:GetFirst()
+			if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
+				local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
+				tc:SetMaterial(mat1)
+				Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+				Duel.BreakEffect()
+				Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
+			else
+				local mat2=Duel.SelectFusionMaterial(tp,tc,mg2,nil,chkf)
+				local fop=ce:GetOperation()
+				fop(ce,e,tp,tc,mat2)
+			end
+			tc:CompleteProcedure()
+		end
+	end
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(2<<32))
+end
+function s.filter1(c,e)
+	return not c:IsImmuneToEffect(e)
+end
+function s.filter2(c,e,tp,m,f,chkf)
+	return c:IsType(TYPE_FUSION) and (not f or f(c)) and c:IsCode(63519819)
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,chkf)
+end
+function s.fustarget(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local chkf=tp
+		local mg1=Duel.GetFusionMaterial(tp)
+		local res=Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,chkf)
+		if not res then
+			local ce=Duel.GetChainMaterial(tp)
+			if ce~=nil then
+				local fgroup=ce:GetTarget()
+				local mg2=fgroup(ce,e,tp)
+				local mf=ce:GetValue()
+				res=Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg2,mf,chkf)
+			end
+		end
+		return res
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
