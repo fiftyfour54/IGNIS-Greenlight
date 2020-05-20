dbded941cb88c60783f42d2cda586ef941c99403
NaimSantos <naimsantos45@gmail.com>
2020-05-20 16:20:56 -0300

    Added test puzzle

---
+++ CP20 - 1002660XX/Test High Rate Draw.lua
@@ -0,0 +1,23 @@
+--Created using senpaizuri's Puzzle Maker (updated by Naim & Larry126)
+Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI,5)
+Debug.SetPlayerInfo(0,8000,0,0)
+Debug.SetPlayerInfo(1,8000,0,0)
+
+--Main Deck
+Debug.AddCard(19230407,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+Debug.AddCard(19230407,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+Debug.AddCard(511002938,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+Debug.AddCard(511002938,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+--GY
+Debug.AddCard(100266018,0,0,LOCATION_GRAVE,0,POS_FACEUP)
+--Monster Zones
+Debug.AddCard(41090784,0,0,LOCATION_MZONE,1,1,true)
+Debug.AddCard(41999284,0,0,LOCATION_MZONE,6,1,true)
+Debug.AddCard(41999284,0,0,LOCATION_MZONE,5,1,true)
+Debug.AddCard(16947147,0,0,LOCATION_MZONE,4,4,true)
+Debug.AddCard(69351984,0,0,LOCATION_MZONE,2,8,true)
+Debug.AddCard(69351984,0,0,LOCATION_MZONE,3,8,true)
+Debug.AddCard(69351984,0,0,LOCATION_MZONE,0,8,true)
+--Spell & Trap Zones
+Debug.AddCard(100266018,0,0,LOCATION_SZONE,1,10)
+Debug.ReloadFieldEnd()
