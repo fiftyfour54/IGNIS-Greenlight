16758e907f196503f74d7c908ab6326e9a3e49fc
KingYamato <58759661+KingYamato@users.noreply.github.com>
2020-05-26 02:16:50 -0300

    Add copybokkle test

---
+++ CP20 - 1002660XX/test copybokkle.lua
@@ -0,0 +1,17 @@
+--Created using senpaizuri's Puzzle Maker (updated by Naim & Larry126)
+Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI,5)
+Debug.SetPlayerInfo(0,8000,0,0)
+Debug.SetPlayerInfo(1,8000,0,0)
+
+--Main Deck
+Debug.AddCard(100266035,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+Debug.AddCard(100266037,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+--Hand
+Debug.AddCard(100266035,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(100266037,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+--GY
+Debug.AddCard(100266037,0,0,LOCATION_GRAVE,0,POS_FACEUP)
+--Monster Zones
+Debug.AddCard(100266035,0,0,LOCATION_MZONE,2,1,true)
+Debug.ReloadFieldEnd()
+aux.BeginPuzzle()
