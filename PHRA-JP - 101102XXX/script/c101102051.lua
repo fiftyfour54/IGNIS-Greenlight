fabf532aeefe06d340b106c617ba9b0f6426eaf8
Edoardo Lolletti <edoardo762@gmail.com>
2020-06-08 15:34:01 +0200

    Rewrite Rank-Up-Magic - Phantom Knights' Force selection loops

--- PHRA-JP - 101102XXX/script/c101102051.lua
+++ PHRA-JP - 101102XXX/script/c101102051.lua
@@ -1,6 +1,7 @@
 --ＲＵＭ－ファントム・フォース
 --Rank-Up-Magic - Phantom Knights' Force
 --Scripted by AlphaKretin
+--Card selection parts by edo9300
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -9,8 +10,8 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetCondition(s.condition)
-    e1:SetCost(s.cost)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
@@ -20,52 +21,50 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsMainPhase()
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-    e:SetLabel(100)
-    if chk==0 then return true end
+	e:SetLabel(100)
+	if chk==0 then return true end
 end
-function s.filter1(c,e,tp,dr)
+function s.filter1(c,e,tp,sg,exg,dr)
 	local rk=c:GetRank()
-    local pg=aux.GetMustBeMaterialGroup(tp,Group.FromCards(c),tp,nil,nil,REASON_XYZ)
-	return #pg<=1 and c:IsFaceup() and c:GetOverlayCount()==0 and (rk>0 or c:IsStatus(STATUS_NO_LEVEL)) 
-		and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,c,rk+dr,pg)
+	local pg=aux.GetMustBeMaterialGroup(tp,Group.FromCards(c),tp,nil,nil,REASON_XYZ)
+	return #pg<=1 and c:IsFaceup() and (rk>0 or c:IsStatus(STATUS_NO_LEVEL)) 
+		and exg:IsExists(s.filter2,1,nil,e,tp,c,rk+dr,pg,sg)
 end
-function s.filter2(c,e,tp,mc,rk,pg)
-	if c.rum_limit and not c.rum_limit(mc,e) or Duel.GetLocationCountFromEx(tp,tp,mc,c)<=0 then return false end
-	return c:IsType(TYPE_XYZ) and mc:IsType(TYPE_XYZ,c,SUMMON_TYPE_XYZ,tp) and mc:IsAttribute(ATTRIBUTE_DARK,c,SUMMON_TYPE_XYZ,tp) and c:IsRank(rk) and c:IsAttribute(ATTRIBUTE_DARK) 
-		and mc:IsCanBeXyzMaterial(c,tp) and (#pg<=0 or pg:IsContains(mc)) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false)
+function s.filter2(c,e,tp,mc,rk,pg,sg)
+	if c.rum_limit and not c.rum_limit(mc,e) or Duel.GetLocationCountFromEx(tp,tp,sg+mc,c)<=0 then return false end
+	return mc:IsType(TYPE_XYZ,c,SUMMON_TYPE_XYZ,tp) and mc:IsAttribute(ATTRIBUTE_DARK,c,SUMMON_TYPE_XYZ,tp) and c:IsRank(rk) 
+		and mc:IsCanBeXyzMaterial(c,tp) and (#pg<=0 or pg:IsContains(mc))
+end
+function s.rescon(exg,fg)
+	return	function(sg,e,tp,mg)
+		return fg:IsExists(s.filter1,1,sg,e,tp,sg,exg,#sg)
+	end
 end
 function s.rmfilter(c)
-    return c:IsAttribute(ATTRIBUTE_DARK) and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true,false)
+	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true,false)
+end
+function s.extrafil(c)
+	return c:IsSetCard(0x10db) or c:IsSetCard(0xba) or c:IsSetCard(0x2073) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false)
+end
+function s.fieldfil(c)
+	return c:IsType(TYPE_XYZ) and c:IsAttribute(ATTRIBUTE_DARK)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    local g=Duel.GetMatchingGroup(s.rmfilter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,nil)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.filter1(chkc,e,tp) end
-    if chk==0 then 
-        if e:GetLabel()~=100 then return false end
-        e:SetLabel(0)
-        local ct=#g
-        for i=1,ct do
-            if Duel.IsExistingTarget(s.filter1,tp,LOCATION_MZONE,0,1,nil,e,tp,i) then
-                return true
-            end
-        end
-        return false
-    end
-    local numbers={}
-    local ct=#g
-    for i=1,ct do
-        if Duel.IsExistingTarget(s.filter1,tp,LOCATION_MZONE,0,1,nil,e,tp,i) then
-            table.insert(numbers,i)
-        end
+	local exg=Duel.GetMatchingGroup(s.extrafil,tp,LOCATION_EXTRA,0,nil)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.fieldfil(chkc) and s.filter1(chkc,e,tp,Group.FromCards(chkc),exg,e:GetLabel()) end
+	local g=Duel.GetMatchingGroup(s.rmfilter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,nil)
+	local fg=Duel.GetMatchingGroup(s.fieldfil,tp,LOCATION_MZONE,0,nil)
+	if chk==0 then 
+		if e:GetLabel()~=100 then return false end
+		e:SetLabel(0)
+		return #fg>0 and #g>0 and #exg>0 and aux.SelectUnselectGroup(g,e,tp,nil,nil,s.rescon(exg,fg),0)
 	end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-    local dr=Duel.AnnounceNumber(tp,table.unpack(numbers))
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-    local rg=g:Select(tp,dr,dr)
-    Duel.Remove(rg,POS_FACEUP,REASON_COST)
+	local rg=aux.SelectUnselectGroup(g,e,tp,nil,nil,s.rescon(exg,fg),1,tp,HINTMSG_REMOVE,s.rescon(exg,fg))
+	local dr=#rg
+	Duel.Remove(rg,POS_FACEUP,REASON_COST)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-    local g=Duel.SelectTarget(tp,s.filter1,tp,LOCATION_MZONE,0,1,1,nil,e,tp,dr)
-    e:SetLabel(dr)
+	local g=Duel.SelectTarget(tp,s.filter1,tp,LOCATION_MZONE,0,1,1,nil,e,tp,rg,exg,dr)
+	e:SetLabel(dr)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
@@ -83,7 +82,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if not tc or tc:IsFacedown() or not tc:IsRelateToEffect(e) or tc:IsControler(1-tp) or tc:IsImmuneToEffect(e) then return end
 	local pg=aux.GetMustBeMaterialGroup(tp,Group.FromCards(tc),tp,nil,nil,REASON_XYZ)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,tc,tc:GetRank()+e:GetLabel(),pg)
+	local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,tc,tc:GetRank()+e:GetLabel(),pg,Group.FromCards(tc))
 	local sc=g:GetFirst()
 	if sc then
 		local mg=tc:GetOverlayGroup()
@@ -95,4 +94,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SpecialSummon(sc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)
 		sc:CompleteProcedure()
 	end
+end
+function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+	return not c:IsType(TYPE_XYZ) and c:IsLocation(LOCATION_EXTRA)
 end
