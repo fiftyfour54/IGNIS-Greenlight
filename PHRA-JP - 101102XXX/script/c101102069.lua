ffa3bbc746387ed1101c243d331b98321ea157d3
NaimSantos <naimsantos45@gmail.com>
2020-06-08 10:36:59 -0300

    Fix "Raidraptor - Phantom Knights' Claw"

--- PHRA-JP - 101102XXX/script/c101102069.lua
+++ PHRA-JP - 101102XXX/script/c101102069.lua
@@ -5,9 +5,11 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY+CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_CHAINING)
+	e1:SetCountLimit(1,id)
 	e1:SetCondition(s.condition)
 	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
@@ -18,10 +20,17 @@ s.listed_series={0x2073,0xba,0x10db}
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return re:IsActiveType(TYPE_MONSTER) and Duel.IsChainNegatable(ev)
 end
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsAttribute(ATTRIBUTE_DARK)
+end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=c:GetOverlayGroup()
+	local g=Group.CreateGroup()
+	local mg=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_MZONE,0,nil)
+	for tc in aux.Next(mg) do
+		g:Merge(tc:GetOverlayGroup())
+	end
 	if chk==0 then return #g>0 end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVEXYZ)
 	local sc=g:Select(tp,1,1,nil):GetFirst()
 	if sc:IsSetCard(0x2073) or sc:IsSetCard(0x10db) or sc:IsSetCard(0xba) then
 		e:SetLabel(1)
@@ -46,8 +55,9 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		and Duel.Destroy(eg,REASON_EFFECT)~=0 and e:GetLabel()==1 then
 		local atk=Duel.GetOperatedGroup():GetFirst():GetBaseAttack()
 		local g=Duel.GetMatchingGroup(s.atkfilter,tp,LOCATION_MZONE,0,nil)
-		if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+		if #g>0 then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,1))
 			local sc=g:Select(tp,1,1,nil):GetFirst()
 			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
