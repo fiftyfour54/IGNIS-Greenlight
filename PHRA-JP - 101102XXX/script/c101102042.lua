63d3003c2190b2fb1400e38cff7357f026f28c88
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-07-08 22:28:56 +0300

    "Datascape Shell - Jiajia" fix 2

--- PHRA-JP - 101102XXX/script/c101102042.lua
+++ PHRA-JP - 101102XXX/script/c101102042.lua
@@ -34,7 +34,7 @@ function s.indcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
 function s.indtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and chkc:IsFaceup() end
 	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
