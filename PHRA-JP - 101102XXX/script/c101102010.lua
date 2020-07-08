5a81f0eedcd23a78ed2cb9bbfbf8bd83f15c08b6
lrhel <lrhel@users.noreply.github.com>
2020-07-08 15:10:47 +0200

    Add "Datascape Lady - Ruirui"

---
+++ PHRA-JP - 101102XXX/script/c101102010.lua
@@ -0,0 +1,64 @@
+--電脳堺媛－瑞々
+--Datascape Lady - Ruirui
+--Scripted by Hel
+local s,id=GetID()
+function s.initial_effect(c)
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.foolishfilter(c,type)
+	return c:IsSetCard(0x248) and not c:IsType(type&7) and c:IsAbleToGrave()
+end
+function s.tohandfilter(c,type1,type2)
+	return c:IsSetCard(0x248) and not c:IsType(type1&7) and not c:IsType(type2&7) and not c:IsCode(id) and c:IsAbleToHand()
+end
+function s.tgfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x248) and Duel.IsExistingMatchingCard(s.foolishfilter,tp,LOCATION_DECK,0,1,nil,c:GetType())
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.tgfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+end
+function s.splimit(e,c,sump,sumtype,sumpos,targetp)
+	return not (c:IsLevelAbove(3) or c:IsRankAbove(3))
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	--Cannot Special Summon
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(s.splimit)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	aux.RegisterClientHint(c,nil,tp,1,0,aux.Stringid(id,1),nil)
+	--foolish and special summon
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.foolishfilter,tp,LOCATION_DECK,0,1,1,nil,tc:GetType())
+	if #g>0 then
+		if Duel.SendtoGrave(g,REASON_EFFECT)>0 and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
+			--add to hand
+			local gth=Duel.GetMatchingGroup(s.tohandfilter,tp,LOCATION_DECK,0,nil,tc:GetType(),g:GetFirst():GetType())
+			if #gth>1 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+				Duel.BreakEffect()
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+				local sg=gth:Select(tp,1,1,nil)
+				Duel.SendtoHand(sg,nil,REASON_EFFECT)
+				Duel.ConfirmCards(1-tp,sg)
+			end
+		end
+	end
+end
