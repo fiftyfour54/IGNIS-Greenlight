61187bbcfe8f1c5e28aef7987aa69f7c2c2895db
AlphaKretin <deewhydeeecks@gmail.com>
2020-07-08 22:14:18 +0930

    Fix "Prank-Kids Mew"
    Can banish from field

--- PHRA-JP - 101102XXX/script/c101102049.lua
+++ PHRA-JP - 101102XXX/script/c101102049.lua
@@ -19,7 +19,7 @@ function s.initial_effect(c)
     e2:SetType(EFFECT_TYPE_FIELD)
     e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
     e2:SetCode(id)
-    e2:SetRange(LOCATION_GRAVE)
+    e2:SetRange(LOCATION_MZONE+LOCATION_GRAVE)
     e2:SetTargetRange(1,0)
     c:RegisterEffect(e2)
 end
