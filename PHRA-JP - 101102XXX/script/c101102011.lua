034c41982162782a712576a87483d9152506226c
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-07-08 21:45:35 +0300

    "Datascape Savant - Laolao" fixes

--- PHRA-JP - 101102XXX/script/c101102011.lua
+++ PHRA-JP - 101102XXX/script/c101102011.lua
@@ -1,11 +1,12 @@
---電脳堺悟ー老々
---Datascape Savant – Laolao
+--電脳堺悟－老々
+--Datascape Savant - Laolao
 --Scripted by Hel
 local s,id=GetID()
 function s.initial_effect(c)
-	--spsummon
+	--Send 1 card to the GY and Special Summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCountLimit(1,id)
@@ -15,25 +16,23 @@ function s.initial_effect(c)
 end
 s.listed_series={0x248}
 local key=TYPE_MONSTER+TYPE_SPELL+TYPE_TRAP
-function s.foolishfilter(c,type)
-	return c:IsSetCard(0x248) and not c:IsType(type&key) and c:IsAbleToGrave()
+function s.togravefilter(c,ctype)
+	return c:IsSetCard(0x248) and not c:IsType(ctype&key) and c:IsAbleToGrave()
 end
-function s.ssummonfilter(c,tgid,e,tp)
-	return c:IsSetCard(0x248) and c:IsType(TYPE_MONSTER) and not c:IsCode(tgid) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function s.tgfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0x248) and Duel.IsExistingMatchingCard(s.foolishfilter,tp,LOCATION_DECK,0,1,nil,c:GetType())
+function s.tgfilter(c,tp)
+	return c:IsFaceup() and c:IsSetCard(0x248) and Duel.IsExistingMatchingCard(s.togravefilter,tp,LOCATION_DECK,0,1,nil,c:GetType())
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsControler(tp) and s.tgfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_ONFIELD,0,1,nil) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
-		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)end
+	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsControler(tp) and s.tgfilter(chkc,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_ONFIELD,0,1,nil,tp) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_ONFIELD,0,1,1,nil)
+	Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_ONFIELD,0,1,1,nil,tp)
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,LOCATION_HAND)
 end
-function s.splimit(e,c,sump,sumtype,sumpos,targetp)
-	return not (c:IsLevelAbove(3) or c:IsRankAbove(3))
+function s.ssummonfilter(c,tgid,e,tp)
+	return c:IsSetCard(0x248) and c:IsType(TYPE_MONSTER) and not c:IsCode(tgid) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
@@ -42,42 +41,44 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetTargetRange(1,0)
 	e1:SetTarget(s.splimit)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 	aux.RegisterClientHint(c,nil,tp,1,0,aux.Stringid(id,1),nil)
-	--foolish and special summon
+	--Send to GY and Special Summon
 	if tc and tc:IsRelateToEffect(e) and not tc:IsFacedown() then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-		local g=Duel.SelectMatchingCard(tp,s.foolishfilter,tp,LOCATION_DECK,0,1,1,nil,tc:GetType())
-		if #g>0 then
-			if Duel.SendtoGrave(g,REASON_EFFECT)>0 then
-				if not c:IsRelateToEffect(e) then return end
-				if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
-					--Special Summon from GY
-					local gss=Duel.GetMatchingGroup(s.ssummonfilter,tp,LOCATION_GRAVE,0,nil,g:GetFirst():GetCode(),e,tp)
-					if #gss>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
-						Duel.BreakEffect()
-						Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-						local sg=gss:Select(tp,1,1,nil)
-						if sg:GetFirst() and Duel.SpecialSummonStep(sg:GetFirst(),0,tp,tp,false,false,POS_FACEUP_DEFENSE) then
-							local e2=Effect.CreateEffect(c)
-							e2:SetType(EFFECT_TYPE_SINGLE)
-							e2:SetCode(EFFECT_DISABLE)
-							e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-							sg:GetFirst():RegisterEffect(e2,true)
-							local e3=Effect.CreateEffect(c)
-							e3:SetType(EFFECT_TYPE_SINGLE)
-							e3:SetCode(EFFECT_DISABLE_EFFECT)
-							e3:SetReset(RESET_EVENT+RESETS_STANDARD)
-							sg:GetFirst():RegisterEffect(e3,true)
-							Duel.SpecialSummonComplete()
-						end
+		local g=Duel.SelectMatchingCard(tp,s.togravefilter,tp,LOCATION_DECK,0,1,1,nil,tc:GetType())
+		if #g>0 and Duel.SendtoGrave(g,REASON_EFFECT)>0 then
+			local ogc=Duel.GetOperatedGroup():GetFirst()
+			if ogc:IsLocation(LOCATION_GRAVE) and c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+				--Special Summon from GY
+				local gss=Duel.GetMatchingGroup(s.ssummonfilter,tp,LOCATION_GRAVE,0,nil,g:GetFirst():GetCode(),e,tp)
+				if #gss>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+					Duel.BreakEffect()
+					Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+					local sg=gss:Select(tp,1,1,nil)
+					if sg:GetFirst() and Duel.SpecialSummonStep(sg:GetFirst(),0,tp,tp,false,false,POS_FACEUP_DEFENSE) then
+						--Negate its effects	
+						local e2=Effect.CreateEffect(c)
+						e2:SetType(EFFECT_TYPE_SINGLE)
+						e2:SetCode(EFFECT_DISABLE)
+						e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+						sg:GetFirst():RegisterEffect(e2,true)
+						local e3=Effect.CreateEffect(c)
+						e3:SetType(EFFECT_TYPE_SINGLE)
+						e3:SetCode(EFFECT_DISABLE_EFFECT)
+						e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+						sg:GetFirst():RegisterEffect(e3,true)
+						Duel.SpecialSummonComplete()
 					end
 				end
 			end
 		end
 	end
+end
+function s.splimit(e,c,sump,sumtype,sumpos,targetp)
+	return not (c:IsLevelAbove(3) or c:IsRankAbove(3))
 end
