fdba23125cdd7b451138bfcccbcc1601518a5a59
Naim <naimsantos45@gmail.com>
2020-07-14 11:31:18 -0300

    Add "Draitron Fafnir"

---
+++ DBGI-JP - 100425XXX/script/c100425031.lua
@@ -0,0 +1,45 @@
+--竜輝巧-ファフニール
+--Draitron Fafnir
+--Scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_SINGLE)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	--Prevent effect and activation from being negated
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_INACTIVATE)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetValue(s.effectfilter)
+	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_DISEFFECT)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetValue(s.effectfilter)
+	c:RegisterEffect(e3)
+end
+s.listed_series={0x24a}
+function s.thfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x24a) and c:IsAbleToHand()
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
+	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
+end
+function s.effectfilter(e,ct)
+	local te=Duel.GetChainInfo(ct,CHAININFO_TRIGGERING_EFFECT)
+	return te:GetHandler():IsRitualSpell()
+end
