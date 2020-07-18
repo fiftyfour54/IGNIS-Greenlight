6fcf54b26e515d6c4b6bf74f05a26f6220e04dce
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-07-18 18:08:25 +0300

    Update c160002037.lua

--- Rush/Deck Modification Pack - Shocking Lightning Attack!!/c160002037.lua
+++ Rush/Deck Modification Pack - Shocking Lightning Attack!!/c160002037.lua
@@ -7,32 +7,19 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_POSITION)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function s.costfilter(c)
-	return c:IsRace(RACE_PSYCHIC) and c:IsFaceup()
-end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,2,nil) end
-end
-function s.filter(c)
-	return c:IsFaceup() and c:IsCanTurnSet()
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local g=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE,e:GetHandler())
-	if #g>0 then
-		Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
-	end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsRace,RACE_PSYCHIC),tp,LOCATION_MZONE,0,2,nil)
+		and Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsCanTurnSet),tp,0,LOCATION_MZONE,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,nil,1,0,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	--effect
-	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil):GetFirst()
+	--Effect
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local tc=Duel.SelectMatchingCard(tp,aux.FilterFaceupFunction(Card.IsCanTurnSet),tp,0,LOCATION_MZONE,1,1,nil):GetFirst()
 	if tc and tc:IsFaceup() then
 		Duel.ChangePosition(tc,POS_FACEDOWN_DEFENSE)
 	end
