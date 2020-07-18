66c2df9ab16b4426df85eee2136d1b8f89790983
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-07-18 18:17:21 +0300

    Update c160002032.lua

--- Rush/Deck Modification Pack - Shocking Lightning Attack!!/c160002032.lua
+++ Rush/Deck Modification Pack - Shocking Lightning Attack!!/c160002032.lua
@@ -2,7 +2,7 @@
 -- Hydrocannon Big Magurom 
 local s,id=GetID()
 function s.initial_effect(c)
-	--Position change
+	--Direct attack
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
@@ -19,27 +19,23 @@ function s.dirfilter(c)
 	return c:IsFaceup() and c:IsRace(RACE_FISH) and c:IsAttackBelow(1000) and (not c:IsHasEffect(EFFECT_DIRECT_ATTACK))
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.dirfilter,tp,LOCATION_MZONE,0,1,e:GetHandler()) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.dirfilter,tp,LOCATION_MZONE,0,1,nil) end
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	--requirement
+	--Requirement
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g1=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,1,nil)
 	if g1 and Duel.SendtoGrave(g1,REASON_COST)>0 then
-		--effect
-		if c:IsRelateToEffect(e) and c:IsFaceup() then
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-			local g2=Duel.SelectMatchingCard(tp,s.dirfilter,tp,LOCATION_MZONE,0,1,2,nil)
-			if #g2>0 then
-				local tc=g2:GetFirst()
-				for tc in aux.Next(g2) do
-					local e1=Effect.CreateEffect(e:GetHandler())
-					e1:SetType(EFFECT_TYPE_SINGLE)
-					e1:SetCode(EFFECT_DIRECT_ATTACK)
-					e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-					tc:RegisterEffect(e1)
-				end
+		--Effect
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+		local g2=Duel.SelectMatchingCard(tp,s.dirfilter,tp,LOCATION_MZONE,0,1,2,nil)
+		if #g2>0 then
+			for tc in aux.Next(g2) do
+				local e1=Effect.CreateEffect(e:GetHandler())
+				e1:SetType(EFFECT_TYPE_SINGLE)
+				e1:SetCode(EFFECT_DIRECT_ATTACK)
+				e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+				tc:RegisterEffect(e1)
 			end
 		end
 	end
