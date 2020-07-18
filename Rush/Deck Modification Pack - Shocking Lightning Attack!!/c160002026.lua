1328c6c434ed40226a841792d35c8352d0b3d5e5
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-07-18 18:03:01 +0300

    Update c160002026.lua

--- Rush/Deck Modification Pack - Shocking Lightning Attack!!/c160002026.lua
+++ Rush/Deck Modification Pack - Shocking Lightning Attack!!/c160002026.lua
@@ -1,4 +1,4 @@
--- 闘将ナンデス
+--闘将ナンデス
 --Battle Commander Nandes
 local s,id=GetID()
 function s.initial_effect(c)
@@ -12,22 +12,18 @@ function s.initial_effect(c)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
-
 function s.posfilter(c)
-	return c:IsPosition(POS_FACEUP_DEFENSE) and c:IsRace(RACE_WARRIOR)
+	return c:IsPosition(POS_FACEUP_DEFENSE) and c:IsRace(RACE_WARRIOR) and c:IsCanChangePosition()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.posfilter,tp,LOCATION_MZONE,0,1,e:GetHandler()) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.posfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,nil,1,0,0)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	--effect
-	if c:IsRelateToEffect(e) and c:IsFaceup() then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-		local g=Duel.SelectMatchingCard(tp,s.posfilter,tp,LOCATION_MZONE,0,1,1,nil)
-		if #g>0 then
-			Duel.ChangePosition(g,0,0,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK)
-		end
+	--Effect
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectMatchingCard(tp,s.posfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	if #g>0 then
+		Duel.ChangePosition(g,0,0,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK)
 	end
-	
 end
