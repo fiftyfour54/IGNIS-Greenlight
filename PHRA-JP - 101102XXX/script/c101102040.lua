87397cf43fa197d39270e9dd4dcc63b7106f42b1
NaimSantos <naimsantos45@gmail.com>
2020-07-24 07:20:15 -0300

    Some updates for "Raider's Night"

--- PHRA-JP - 101102XXX/script/c101102040.lua
+++ PHRA-JP - 101102XXX/script/c101102040.lua
@@ -22,51 +22,58 @@ function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
 	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
-function s.spfilter(c,e,tp,mc,rk)
-	local pg=aux.GetMustBeMaterialGroup(tp,Group.FromCards(mc),tp,nil,nil,REASON_XYZ)
-	if c.rum_limit and not c.rum_limit(mc,e) then return false end
+function s.spfilter(c,e,tp,mc,rk,pg)
 	return c:IsType(TYPE_XYZ) and (c:IsSetCard(0xba) or c:IsSetCard(0x2073) or c:IsSetCard(0x10db))
-		and mc:IsType(TYPE_XYZ,c,SUMMON_TYPE_XYZ,tp)
-		and (c:IsRank(rk-1) or c:IsRank(rk+1))
+		and (c:IsRank(rk-1) or c:IsRank(rk+1))  and not c:IsCode(id)
 		and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0
-		and mc:IsCanBeXyzMaterial(c,tp) and (#pg<=0 or pg:IsContains(mc))
+		and (#pg<=0 or pg:IsContains(mc))
+		and mc:IsCanBeXyzMaterial(c,tp)
 		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,c,c:GetRank()) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,LOCATION_EXTRA)
+	if chk==0 then 
+		local c=e:GetHandler()
+		local pg=aux.GetMustBeMaterialGroup(tp,Group.FromCards(c),tp,nil,nil,REASON_XYZ)
+		return (#pg<=0 or (#pg==1 and pg:IsContains(c)))
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,c,c:GetRank(),pg)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) then return end
 	local pg=aux.GetMustBeMaterialGroup(tp,Group.FromCards(c),tp,nil,nil,REASON_XYZ)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,c,c:GetRank())
-	local sc=g:GetFirst()
-	if sc then
-		sc:SetMaterial(Group.FromCards(c))
-		Duel.Overlay(sc,Group.FromCards(c))
-		if Duel.SpecialSummon(sc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP) then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e1:SetCode(EVENT_PHASE+PHASE_END)
-		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-		e1:SetCountLimit(1)
-		e1:SetLabelObject(sc)
-		e1:SetCondition(s.descon)
-		e1:SetOperation(s.desop)
-		if Duel.GetCurrentPhase()==PHASE_END and Duel.GetTurnPlayer()~=tp then
-			e1:SetLabel(Duel.GetTurnCount())
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN,2)
-		else
-			e1:SetLabel(0)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
+	if c:IsFaceup() and c:IsRelateToEffect(e) and c:IsControler(tp) and not c:IsImmuneToEffect(e) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,c,c:GetRank(),pg)
+		local sc=g:GetFirst()
+		if sc then
+			Duel.BreakEffect()
+			local mg=c:GetOverlayGroup()
+			if #mg~=0 then
+				Duel.Overlay(sc,mg)
+			end
+			sc:SetMaterial(Group.FromCards(c))
+			Duel.Overlay(sc,Group.FromCards(c))
+			if Duel.SpecialSummon(sc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)>0 then
+				sc:CompleteProcedure()
+				local e1=Effect.CreateEffect(c)
+				e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+				e1:SetCode(EVENT_PHASE+PHASE_END)
+				e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+				e1:SetCountLimit(1)
+				e1:SetLabelObject(sc)
+				e1:SetCondition(s.descon)
+				e1:SetOperation(s.desop)
+				if Duel.GetCurrentPhase()==PHASE_END and Duel.GetTurnPlayer()~=tp then
+					e1:SetLabel(Duel.GetTurnCount())
+					e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN,2)
+				else
+					e1:SetLabel(0)
+					e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
+				end
+				Duel.RegisterEffect(e1,tp)
+			end
 		end
-		Duel.RegisterEffect(e1,tp)
-		end
-		sc:CompleteProcedure()
 	end
 end
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
