40b36535bf674f274a73bde6f0faae8c3ae38884
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-07-29 15:29:41 +0300

    Update c101102065.lua

--- PHRA-JP - 101102XXX/script/c101102065.lua
+++ PHRA-JP - 101102XXX/script/c101102065.lua
@@ -17,11 +17,9 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	Duel.AddCustomActivityCounter(id,ACTIVITY_ATTACK,s.counterfilter)
 end
+s.listed_series={0x114}
 function s.counterfilter(c)
 	return c:IsSetCard(0x114)
-end
-function s.atktg(e,c)
-	return not c:IsSetCard(0x114)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:SetLabel(100)
@@ -36,20 +34,22 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.RegisterEffect(e1,tp)
 	aux.RegisterClientHint(e:GetHandler(),nil,tp,1,0,aux.Stringid(id,1),nil)
 end
-function s.costfilter(c,e,tp)
-	return c:HasLevel() and	Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_HAND,0,1,nil,c:GetLevel(),e,tp)
+function s.atktg(e,c)
+	return not c:IsSetCard(0x114)
+end
+function s.costfilter(c,e,tp,ft)
+	return c:HasLevel() and (ft>0 or c:IsInMainMZone()) and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_HAND,0,1,nil,c:GetLevel(),e,tp)
 end
 function s.spfilter(c,lv,e,tp)
-	return c:GetOriginalLevel()==lv+1
-		and c:IsSetCard(0x114)
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return (c:GetOriginalLevel()==lv+1 or c:GetOriginalLevel()==lv-1) and c:IsSetCard(0x114) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		if e:GetLabel()~=100 then return false end
 		e:SetLabel(0)
-		return e:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
-			and Duel.CheckReleaseGroupCost(tp,s.costfilter,1,false,nil,nil,e,tp)
+		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+		return e:IsHasType(EFFECT_TYPE_ACTIVATE) and ft>-1
+			and Duel.CheckReleaseGroupCost(tp,s.costfilter,1,false,nil,nil,e,tp,ft)
 	end
 	e:SetLabel(0)
 	local g=Duel.SelectReleaseGroupCost(tp,s.costfilter,1,1,false,nil,nil,e,tp)
@@ -66,4 +66,3 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
-
