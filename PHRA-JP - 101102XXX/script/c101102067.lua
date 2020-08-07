0d542f571ba87cf920e3048355aaaa3f066b1e59
NaimSantos <naimsantos45@gmail.com>
2020-08-07 07:59:14 -0300

    Add "Jack in the Hand"

---
+++ PHRA-JP - 101102XXX/script/c101102067.lua
@@ -0,0 +1,45 @@
+--ジャック・イン・ザ・ハンド
+--Jack in the Hand
+--Scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.thfilter(c,e,tp)
+	return c:IsLevel(1) and c:IsType(TYPE_MONSTER) and c:c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local rvg=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil,e,tp)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and rvg:GetClassCount(Card.GetCode)>=3 end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,PLAYER_ALL,LOCATION_DECK)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,3,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local rvg=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil,e,tp)
+	local g=aux.SelectUnselectGroup(rvg,e,tp,3,3,aux.dncheck,1,tp,HINTMSG_CONFIRM)
+	if #g==3 then
+		Duel.ConfirmCards(1-tp,g)
+		Duel.Hint(HINT_SELECTMSG,1-tp,aux.Stringid(id,1))
+		local sg=g:Select(1-tp,1,1,nil):GetFirst()
+		g:Remove(sg)
+		Duel.SendtoHand(sg,1-tp,REASON_EFFECT)
+		Duel.ConfirmCards(tp,sg)
+		Duel.Hint(HINT_SELECTMSG,1-tp,aux.Stringid(id,1))
+		local tg=g:Select(tp,1,1,nil):GetFirst()
+		Duel.SendtoHand(tg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,tg)
+	end
+end
