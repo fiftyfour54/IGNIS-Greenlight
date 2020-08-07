0d47566af8d5a4f185b02975048bdac8ba77916f
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-08-07 17:12:38 +0300

    Update c101102024.lua

--- PHRA-JP - 101102XXX/script/c101102024.lua
+++ PHRA-JP - 101102XXX/script/c101102024.lua
@@ -3,20 +3,20 @@
 --Scripted by Hel
 local s,id=GetID()
 function s.initial_effect(c)
-	--Banish 1 Trap form Deck to gain 1 level
+	--Banish 1 Trap form Deck to gain 1 Level
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetCountLimit(1,id)
-	e1:SetCost(s.scost)
-	e1:SetOperation(s.sop)
+	e1:SetCost(s.bdcost)
+	e1:SetOperation(s.bdop)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
-	--Banish 1 Trap to gain 1 level
+	--Banish 1 Trap from the GY to gain 1 Level
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_REMOVE)
@@ -24,10 +24,10 @@ function s.initial_effect(c)
 	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetCountLimit(1,id+100)
-	e3:SetTarget(s.ttg)
-	e3:SetOperation(s.top)
+	e3:SetTarget(s.bgtg)
+	e3:SetOperation(s.bgop)
 	c:RegisterEffect(e3)
-	--Set a banished Trap
+	--Set 1 of your banished Normal Traps
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(id,2))
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
@@ -39,18 +39,20 @@ function s.initial_effect(c)
 	e4:SetOperation(s.stop)
 	c:RegisterEffect(e4)
 end
-function s.sfilter(c)
+function s.cfilter(c)
 	return c:IsType(TYPE_TRAP) and c:IsAbleToRemoveAsCost()
 end
-function s.scost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.bdcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return Duel.IsExistingMatchingCard(s.sfilter,tp,LOCATION_DECK,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,s.sfilter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_DECK,0,1,1,nil)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
-function s.sop(e,tp,eg,ep,ev,re,r,rp)
+function s.bdop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	--Increase Level
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_UPDATE_LEVEL)
@@ -58,33 +60,34 @@ function s.sop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetValue(1)
 	c:RegisterEffect(e1)
 end
-function s.ttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and s.tfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.sfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil) end
+function s.bgfilter(c)
+	return c:IsType(TYPE_TRAP) and c:IsAbleToRemove()
+end
+function s.bgtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and s.bgfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.bgfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectTarget(tp,s.sfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,1,nil)
+	local g=Duel.SelectTarget(tp,s.bgfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
 end
-function s.top(e,tp,eg,ep,ev,re,r,rp)
+function s.bgop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) then return end
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
-		if Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)>0 then
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_UPDATE_LEVEL)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
-			e1:SetValue(1)
-			c:RegisterEffect(e1)
-		end
+	if tc and tc:IsRelateToEffect(e) and Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)>0 then
+		--Increase Level
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_LEVEL)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+		e1:SetValue(1)
+		c:RegisterEffect(e1)
 	end
-end
-function s.stfilter(c)
-	return c:GetType()==TYPE_TRAP and c:IsSSetable() and c:IsFaceup()
 end
 function s.stcon(e,tp,eg,ep,ev,re,r,rp)
 	return rp==1-tp and e:GetHandler():IsPreviousControler(tp)
+end
+function s.stfilter(c)
+	return c:GetType()==TYPE_TRAP and c:IsSSetable() and c:IsFaceup()
 end
 function s.sttg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
