eb0a095154009d4e69b9b21e49bc131f31e63706
Naim <naimsantos45@gmail.com>
2020-08-13 07:36:59 -0300

    Add files via upload

---
+++ MP20-EN/script/c246.lua
@@ -0,0 +1,92 @@
+--JP name
+--Succesor Soul
+--Scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	Duel.AddCustomActivityCounter(id,ACTIVITY_ATTACK,s.counterfilter)
+end
+function s.counterfilter(c)
+    return true --filter here for 1 monster only
+end
+function s.filter(c,e)
+	return c:IsFaceup() and c:IsType(TYPE_EFFECT) and c:IsAbleToGrave() and (not e or c:IsCanBeEffectTarget(e))
+end
+function s.cfilter(c)
+    return c:IsType(TYPE_MONSTER) and c:IsType(TYPE_EFFECT)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+    local dg=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE,nil,e)
+    if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.cfilter,1,false,aux.ReleaseCheckTarget,nil,dg) and
+		Duel.GetCustomActivityCount(id,tp,ACTIVITY_ATTACK)==0
+	end
+    local g=Duel.SelectReleaseGroupCost(tp,s.cfilter,1,1,false,aux.ReleaseCheckTarget,nil,dg)
+    Duel.Release(g,REASON_COST)
+	--Register can only attack with 1 monster
+    --Prevents other attacks
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetCondition(s.atkcon)
+	e1:SetTarget(s.atktg)
+	e1:SetReset(RESET_PHASE+PHASE_BATTLE)
+	Duel.RegisterEffect(e1,tp)
+	--Check attacking monster
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_OATH)
+	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e2:SetOperation(s.checkop)
+	e2:SetLabelObject(e1)
+	e2:SetReset(RESET_PHASE+PHASE_BATTLE)
+	Duel.RegisterEffect(e2,tp)
+    aux.RegisterClientHint(e:GetHandler(),nil,tp,1,0,aux.Stringid(id,1),nil)
+end
+function s.spfilter(c,e,tp)
+	return c:IsType(TYPE_NORMAL) and c:IsLevelAbove(7) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and s.filter(chkc,e) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_MZONE,1,nil,e)
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,LOCATION_HAND+LOCATION_DECK,1,nil,e,tp)	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil,e)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,LOCATION_HAND+LOCATION_DECK)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and Duel.SendtoGrave(tc,REASON_EFFECT)>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
+		if #g>0 then
+			Duel.BreakEffect()
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end
+function s.atkcon(e)
+	return Duel.GetFlagEffect(e:GetHandlerPlayer(),id)~=0
+end
+function s.atktg(e,c)
+	return c:GetFieldID()~=e:GetLabel()
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():GetFlagEffect(id)~=0 then return end
+	local fid=eg:GetFirst():GetFieldID()
+	Duel.RegisterFlagEffect(e:GetHandlerPlayer(),id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+	e:GetLabelObject():SetLabel(fid)
+end
