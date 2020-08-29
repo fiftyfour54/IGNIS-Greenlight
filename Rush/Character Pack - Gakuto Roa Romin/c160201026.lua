7a3d686615d1dc7f0abb3e14260567d8435478b3
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-08-29 15:54:03 +0200

    Fixes

--- Rush/Character Pack - Gakuto Roa Romin/c160201026.lua
+++ Rush/Character Pack - Gakuto Roa Romin/c160201026.lua
@@ -1,30 +1,25 @@
---キングス・ディグニティ
---King's Dignity
+-- キングス・ディグニティ
+-- King's Dignity
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e1:SetCondition(s.condition)
-	e1:SetCost(s.cost)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
-	c:RegisterEffect(e1)
+    --Activate
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e1:SetCondition(s.condition)
+    e1:SetCost(s.cost)
+    e1:SetOperation(s.activate)
+    c:RegisterEffect(e1)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return tp~=Duel.GetTurnPlayer()
 end
 function s.filter(c)
-	return c:IsRace(TYPE_NORMAL) and c:IsType(TYPE_MONSTER) and c:IsRace(RACE_PSYCHIC) and c:IsLevel(1) and c:IsAbleToDeckAsCost()
+	return c:IsType(TYPE_NORMAL) and c:IsType(TYPE_MONSTER) and c:IsRace(RACE_FIEND) and c:IsLevel(1) and c:IsAbleToDeckAsCost()
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil) end
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.chlimit(e,ep,tp)
 	return not e:IsHasType(EFFECT_TYPE_ACTIVATE)
