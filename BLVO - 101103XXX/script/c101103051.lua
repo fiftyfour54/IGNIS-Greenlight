bdfc93f787ccc577f633482bd6e246315bec4583
Sanity Sierota <47074795+that-hatter@users.noreply.github.com>
2020-09-09 14:48:33 +0800

    Update c101103051.lua

--- BLVO - 101103XXX/script/c101103051.lua
+++ BLVO - 101103XXX/script/c101103051.lua
@@ -1,11 +1,11 @@
---
+--武装竜の霹靂
 --Armed Dragon Flash
 --scripted by Hatter
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
