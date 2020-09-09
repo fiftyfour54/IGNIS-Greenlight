d6c00ba2eaf7db87fcaaadbe69b36abfce1b240b
AlphaKretin <deewhydeeecks@gmail.com>
2020-09-09 22:03:16 +0930

    Add test puzzle and fix initial issues
    Test puzzle is not comprehensive and not all issues are yet fixed

--- BLVO - 101103XXX/script/c101103002.lua
+++ BLVO - 101103XXX/script/c101103002.lua
@@ -59,7 +59,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and Duel.SendtoGrave(c,REASON_EFFECT)~=0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil)
+		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
 		if #g>0 then
 			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 		end
