d06a682095a77d021288293121c70b0624df77ae
Sanity Sierota <47074795+that-hatter@users.noreply.github.com>
2020-10-12 21:14:20 +0800

    Update c101103021.lua

--- BLVO - 101103XXX/script/c101103021.lua
+++ BLVO - 101103XXX/script/c101103021.lua
@@ -19,7 +19,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--summon
 	local e2=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,1))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_HAND)
