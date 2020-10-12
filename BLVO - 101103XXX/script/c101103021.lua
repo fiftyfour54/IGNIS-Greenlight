da0eb57ecfafa51144ae4bd535dd49799c2ee217
Sanity Sierota <47074795+that-hatter@users.noreply.github.com>
2020-10-12 21:10:38 +0800

    Update c101103021.lua

--- BLVO - 101103XXX/script/c101103021.lua
+++ BLVO - 101103XXX/script/c101103021.lua
@@ -5,36 +5,37 @@ function s.initial_effect(c)
 	--pendulum summon
 	Pendulum.AddProcedure(c)
 	--pendulum
-	 local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_TOHAND)
-    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-    e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-    e1:SetCode(EVENT_DESTROYED)
-    e1:SetRange(LOCATION_PZONE)
-    e1:SetCountLimit(1,id)
-    e1:SetCondition(s.thcon)
-    e1:SetTarget(s.thtg)
-    e1:SetOperation(s.thop)
-    c:RegisterEffect(e1)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_DESTROYED)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.thcon)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
 	--summon
 	local e2=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_HAND)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetCountLimit(1,id+1)
-	e2:SetTarget(s.sptg1)
-	e2:SetOperation(s.spop1)
+	e2:SetCountLimit(1,id+100)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
 	--remove
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetDescription(aux.Stringid(id,2))
 	e3:SetCategory(CATEGORY_REMOVE)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e3:SetProperty(EFFECT_FLAG_DELAY)
-	e3:SetCountLimit(1,id+1)
+	e3:SetCountLimit(1,id+200)
 	e3:SetCondition(s.rmcon)
 	e3:SetTarget(s.rmtg)
 	e3:SetOperation(s.rmop)
@@ -43,14 +44,13 @@ end
 s.listed_series={0xe1}
 --to hand
 function s.cfilter(c,tp)
-	return c:IsReason(REASON_EFFECT) and c:IsPreviousSetCard(0xe1)
-		and c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousPosition(POS_FACEUP)
+  return c:IsReason(REASON_EFFECT) and c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_MZONE)
 end
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.cfilter,1,nil,tp)
 end
 function s.thfilter(c)
-	return c:IsSetCard(0xe1) and c:IsAbleToHand()
+	return c:IsSetCard(0xe1) and c:IsAbleToHand() and not c:IsCode(id)
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
@@ -71,17 +71,15 @@ end
 function s.filter(c,e)
 	return c:IsFaceup() and c:IsCanBeEffectTarget(e)
 end
-function s.sptg1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil,e)
-	if chk==0 then return not 
-		aux.SelectUnselectGroup(g,1,tp,2,2,s.rescon,chk) 
-	end
-	local tg=aux.SelectUnselectGroup(g,1,tp,2,2,s.rescon,chk,tp) 
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_ONFIELD,0,nil,e)
+	if chk==0 then return g:IsExists(Card.IsSetCard,1,nil,0xe1) end
+	local tg=aux.SelectUnselectGroup(g,1,tp,2,2,s.rescon,chk,tp)
 	Duel.SetTargetCard(tg)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,2,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
-function s.spop1(e,tp,eg,ep,ev,re,r,rp)
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetTargetCards(e)
 	if Duel.Destroy(g,REASON_EFFECT)~=0 then
 		local c=e:GetHandler()
@@ -91,7 +89,7 @@ function s.spop1(e,tp,eg,ep,ev,re,r,rp)
 end
 --remove
 function s.rmcon(e,tp,eg,ep,ev,re,r,rp)
-	return re and re:IsActiveType(TYPE_MONSTER) and re:GetHandler():IsSetCard(0xe1)
+	return re and re:GetHandler():IsSetCard(0xe1)
 end
 function s.rmfilter(c)
 	return c:IsType(TYPE_MONSTER) and c:IsAbleToRemove() and aux.SpElimFilter(c,false,true)
