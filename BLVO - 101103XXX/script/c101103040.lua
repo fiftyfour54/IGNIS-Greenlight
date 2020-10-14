749fd612137974953d5f04c8df7adc183381b694
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2020-10-14 12:02:16 -0400

    Cleaned up "El Rey Conquista Eldlich"

--- BLVO - 101103XXX/script/c101103040.lua
+++ BLVO - 101103XXX/script/c101103040.lua
@@ -1,59 +1,64 @@
--- 黄金狂エルドリッチ
--- El Di Conquista Eldlich
+--黄金狂エルドリッチ
+--El Rey Conquista Eldlich
+
 local s,id=GetID()
 function s.initial_effect(c)
-	--fusion
+	--Must be properly summoned before reviving
 	c:EnableReviveLimit()
+	--Fusion procedure
 	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunctionEx(Card.IsSetCard,0x142),s.matfilter)
-	--change name
+	--Name becomes "Eldlich the Golden Lord" while in monster zones
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetCode(EFFECT_CHANGE_CODE)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetValue(95440946)
+	e1:SetValue(CARD_GOLDEN_LORD)
 	c:RegisterEffect(e1)
-	--indes
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-	e3:SetValue(1)
+	--Cannot be destroyed by battle or card effects
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 	c:RegisterEffect(e3)
-	local e4=e3:Clone()
-	e4:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	--Take control 1 of opponent's monsters
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetCategory(CATEGORY_CONTROL)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1)
+	e4:SetCost(s.ctcost)
+	e4:SetTarget(s.cttg)
+	e4:SetOperation(s.ctop)
 	c:RegisterEffect(e4)
-	local e7=Effect.CreateEffect(c)
-	e7:SetDescription(aux.Stringid(id,0))
-	e7:SetCategory(CATEGORY_CONTROL)
-	e7:SetType(EFFECT_TYPE_IGNITION)
-	e7:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e7:SetRange(LOCATION_MZONE)
-	e7:SetCountLimit(1)
-	e7:SetCost(s.ctcost)
-	e7:SetTarget(s.cttg)
-	e7:SetOperation(s.ctop)
-	c:RegisterEffect(e7)
 end
+s.listed_names={CARD_GOLDEN_LORD}
 s.listed_series={0x142}
 s.material_setcode={0x142}
+
 function s.matfilter(c,fc,sumtype,tp)
 	return c:IsRace(RACE_ZOMBIE,fc,sumtype,tp) and c:IsLevelAbove(5)
 end
+function s.filter(c,e)
+	return c:IsFaceup() and c:IsAbleToChangeControler() and (not e or c:IsCanBeEffectTarget(e))
+end
 function s.cfilter(c,ft,tp)
 	return c:IsRace(RACE_ZOMBIE)
-		and (ft>0 or (c:IsControler(tp) and c:GetSequence()<5)) and (c:IsControler(tp) or c:IsFaceup())
+		and (ft>0 or (c:IsControler(tp) and c:GetSequence()<5))-- and (c:IsControler(tp) or c:IsFaceup())
 end
 function s.ctcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local dg=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE,nil,e)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.cfilter,1,false,aux.ReleaseCheckTarget,e:GetHandler(),dg,ft,tp) end
-	local sg=Duel.SelectReleaseGroupCost(tp,s.cfilter,1,1,false,aux.ReleaseCheckTarget,e:GetHandler(),dg,ft,tp)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.cfilter,1,false,nil,nil,dg,ft,tp) end
+	local sg=Duel.SelectReleaseGroupCost(tp,s.cfilter,1,1,false,false,nil,dg,ft,tp)
 	Duel.Release(sg,REASON_COST)
-end
-function s.filter(c,e)
-	return c:IsFaceup() and c:IsAbleToChangeControler() and (not e or c:IsCanBeEffectTarget(e))
 end
 function s.cttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and s.filter(chkc) end
@@ -67,20 +72,19 @@ function s.ctop(e,tp,eg,ep,ev,re,r,rp)
 	if tc:IsRelateToEffect(e) and tc:IsFaceup() and Duel.GetControl(tc,tp) then
 		local c=e:GetHandler()
 		local reset=RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET+RESET_PHASE+PHASE_END
+		--Cannot attack this turn
 		local e1=Effect.CreateEffect(c)
+		e1:SetDescription(3206)
+		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_DISABLE)
-		e1:SetReset(reset)
+		e1:SetCode(EFFECT_CANNOT_ATTACK)
 		tc:RegisterEffect(e1)
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_DISABLE_EFFECT)
-		e2:SetReset(reset)
+		e1:SetReset(reset)
+		tc:RegisterEffect(e1)		
+		--Cannot activate its effect
+		local e2=e1:Clone()
+		e2:SetDescription(3302)
+		e2:SetCode(EFFECT_CANNOT_TRIGGER)
 		tc:RegisterEffect(e2)
-		local e3=Effect.CreateEffect(c)
-		e3:SetType(EFFECT_TYPE_SINGLE)
-		e3:SetCode(EFFECT_CANNOT_ATTACK)
-		e3:SetReset(reset)
-		tc:RegisterEffect(e3)
 	end
 end
