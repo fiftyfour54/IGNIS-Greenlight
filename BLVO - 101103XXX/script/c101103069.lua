87c584e2579d3cb676236439aca22dcf7277f783
ahtelel <32773013+ahtelel@users.noreply.github.com>
2020-10-17 23:00:53 +0700

    Update c101103069.lua
    Updated zone handling

--- BLVO - 101103XXX/script/c101103069.lua
+++ BLVO - 101103XXX/script/c101103069.lua
@@ -9,36 +9,28 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-	e1:SetCondition(s.condition)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
 s.listed_series={0x258}
 s.listed_names={CARD_ALBAZ}
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,0x258),tp,LOCATION_MZONE,0,1,nil)
-end
-function s.bonusfilter(c)
-	return c:IsFaceup() and c:IsType(TYPE_FUSION) and aux.IsMaterialListCode(c,CARD_ALBAZ)
-end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return true end
 	local seq2=0
-	local zone2=0
 	local zone1=Duel.SelectFieldZone(tp,1,0,LOCATION_MZONE,0x60<<16)
 	local seq1=math.log(zone1,2)-16
 	Duel.Hint(HINT_ZONE,tp,zone1)
-	if Duel.IsExistingMatchingCard(s.bonusfilter,tp,LOCATION_MZONE,0,1,nil) and Duel.SelectYesNo(tp,0) then
+	if Duel.SelectYesNo(tp,0) then
 		zone2=Duel.SelectFieldZone(tp,1,0,LOCATION_MZONE,(0x60<<16)|zone1)
 		seq2=math.log(zone2,2)-16
 		Duel.Hint(HINT_ZONE,tp,zone2)
 	end
-	e:SetLabel(seq1,seq2,zone1,zone2)
+	e:SetLabel(seq1,seq2)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local s1,s2,zone1,zone2=e:GetLabel()
+	local s1,s2=e:GetLabel()
 	local z1=Duel.GetFieldCard(1-tp,LOCATION_MZONE,s1)
 	local z2=Duel.GetFieldCard(1-tp,LOCATION_MZONE,s2)
 	if z1 and z1:IsFaceup() then
@@ -63,7 +55,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		local e4=Effect.CreateEffect(c)
 		e4:SetType(EFFECT_TYPE_FIELD)
 		e4:SetCode(EFFECT_DISABLE_FIELD)
-		e4:SetValue(zone1)
+		e4:SetValue((1<<s1)<<((1-tp)*16))
 		e4:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		Duel.RegisterEffect(e4,tp)
 	end
@@ -89,7 +81,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		local e8=Effect.CreateEffect(c)
 		e8:SetType(EFFECT_TYPE_FIELD)
 		e8:SetCode(EFFECT_DISABLE_FIELD)
-		e8:SetValue(zone2)
+		e8:SetValue((1<<s2)<<((1-tp)*16))
 		e8:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		Duel.RegisterEffect(e8,tp)
 	end
