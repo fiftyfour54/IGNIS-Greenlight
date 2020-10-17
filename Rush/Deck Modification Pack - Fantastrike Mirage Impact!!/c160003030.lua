fa32b36e5f3db7f8ed8c62ab12e7ee7d7716becd
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-10-17 14:10:19 +0200

    Update c160003030.lua

--- Rush/Deck Modification Pack - Fantastrike Mirage Impact!!/c160003030.lua
+++ Rush/Deck Modification Pack - Fantastrike Mirage Impact!!/c160003030.lua
@@ -21,12 +21,12 @@ end
 function s.filter(c,e,sp)
 	return c:IsRace(RACE_BEASTWARRIOR) and c:IsLevelBelow(7) and c:IsCanBeSpecialSummoned(e,0,sp,false,false,POS_FACEUP)
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	--Effect
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
