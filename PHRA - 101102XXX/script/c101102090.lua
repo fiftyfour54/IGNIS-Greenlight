5814ee58de4b23bf3ce375b6b1c62df641fba772
lrhel <lrhel@users.noreply.github.com>
2020-10-22 17:01:19 +0200

    Fix "Myutant Synthesis"
    Typo

--- PHRA - 101102XXX/script/c101102090.lua
+++ PHRA - 101102XXX/script/c101102090.lua
@@ -140,7 +140,7 @@ function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_REMOVED)
 end
 	--Add 1 of your banished "Myutant" cards to hand
-function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_REMOVED,0,1,1,nil)
 	if #g>0 then
