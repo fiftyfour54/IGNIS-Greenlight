7d1adc831f1ce7453fe1cb777489d594e3922ce8
edo9300 <edoardo762@gmail.com>
2020-10-22 12:29:19 +0200

    Myutant Fusion: handle ghost belle and spirit elimination
    Also add checks for cards to be removed

--- PHRA - 101102XXX/script/c101102093.lua
+++ PHRA - 101102XXX/script/c101102093.lua
@@ -4,9 +4,12 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	local e1=Fusion.CreateSummonEff({handler=c,fusfilter=aux.FilterBoolFunction(Card.IsSetCard,0x259),extrafil=s.extrafil,extraop=Fusion.BanishMaterial})
+	local e1=Fusion.CreateSummonEff({handler=c,fusfilter=aux.FilterBoolFunction(Card.IsSetCard,0x259),
+										matfilter=Card.IsAbleToRemove,extrafil=s.extrafil,extraop=Fusion.BanishMaterial})
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	c:RegisterEffect(e1)
+	if not GhostBelleTable then GhostBelleTable={} end
+	table.insert(GhostBelleTable,e1)
 	Duel.AddCustomActivityCounter(id,ACTIVITY_CHAIN,aux.FALSE)
 end
 s.listed_series={0x259}
@@ -19,6 +22,10 @@ function s.check(g1,g2)
 end
 function s.extrafil(e,tp,mg)
 	if Duel.GetCustomActivityCount(id,1-e:GetHandlerPlayer(),ACTIVITY_CHAIN)==0 then return nil end
-	local g=Duel.GetMatchingGroup(nil,tp,LOCATION_DECK+LOCATION_GRAVE,0,mg)
+	local loc=LOCATION_DECK
+	if not Duel.IsPlayerAffectedByEffect(tp,69832741) then
+		loc=loc|LOCATION_GRAVE
+	end
+	local g=Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToRemove),tp,LOCATION_DECK+LOCATION_GRAVE,0,mg)
 	return g,s.check(g:Split(Card.IsLocation,nil,LOCATION_GRAVE))
 end
