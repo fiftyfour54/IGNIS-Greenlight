2b887e2f95fdec7c1be44790c4389fd7db8bdec1
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2020-10-22 09:15:44 -0400

    Updated "Myutant" fusion monsters Should be checking if they were fusion summoned as well.

--- PHRA - 101102XXX/script/c101102090.lua
+++ PHRA - 101102XXX/script/c101102090.lua
@@ -9,7 +9,7 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--Fusion summon procedure
 	Fusion.AddProcMixN(c,true,true,s.ffilter,2)
-	--If fusion summoned
+	--If fusion summoned, destroy 1 card on the field
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DESTROY)
@@ -123,10 +123,10 @@ end
 function s.efilter(e,te)
 	return te:IsActiveType(TYPE_TRAP) and te:GetOwnerPlayer()~=e:GetHandlerPlayer()
 end
-	--If destroyed by opponent's card
+	--If this fusion summoned card was destroyed by opponent's card
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:IsReason(REASON_BATTLE)
+	return c:IsSummonType(SUMMON_TYPE_FUSION) and c:IsReason(REASON_BATTLE)
 		or (rp==1-tp and c:IsReason(REASON_EFFECT) and c:GetPreviousControler()==tp)
 end
 	--Check for face-up banished "Myutant" card
--- PHRA - 101102XXX/script/c101102091.lua
+++ PHRA - 101102XXX/script/c101102091.lua
@@ -76,10 +76,10 @@ function s.negop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.Remove(eg,POS_FACEUP,REASON_EFFECT)
 	end
 end
-	--If destroyed by opponent's card
+	--If this fusion summoned card was destroyed by opponent's card
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:IsReason(REASON_BATTLE)
+	return c:IsSummonType(SUMMON_TYPE_FUSION) and c:IsReason(REASON_BATTLE)
 		or (rp==1-tp and c:IsReason(REASON_EFFECT) and c:GetPreviousControler()==tp)
 end
 	--Check for a face-up banished "Myutant" card
