ac1be5dae203439c061070a852ecfafde34366bf
edo9300 <edoardo762@gmail.com>
2020-10-23 19:49:13 +0200

    Add aux.SecurityTarget function

---
+++ BLVO - 101103XXX/script/utilityfinc.lua
@@ -0,0 +1,3 @@
+function aux.SecurityTarget(e,_c)
+    return _c:GetColumnGroup():IsExists(function(c,tp)return c:IsControler(tp) and c:IsFaceup() and c:IsSetCard(0x259) end,1,nil,e:GetHandlerPlayer())
+end
