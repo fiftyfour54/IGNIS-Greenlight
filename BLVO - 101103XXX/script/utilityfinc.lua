8addd38a1f888ef7d7b66b862bdfba69f67876ba
edo9300 <edoardo762@gmail.com>
2020-10-23 20:32:04 +0200

    Update utilityfinc.lua

--- BLVO - 101103XXX/script/utilityfinc.lua
+++ BLVO - 101103XXX/script/utilityfinc.lua
@@ -1,3 +1,3 @@
 function aux.SecurityTarget(e,_c)
-    return _c:GetColumnGroup():IsExists(function(c,tp)return c:IsControler(tp) and c:IsFaceup() and c:IsSetCard(0x259) end,1,nil,e:GetHandlerPlayer())
+    return _c:GetColumnGroup():IsExists(function(c,tp)return c:IsControler(tp) and c:IsFaceup() and c:IsSetCard(0x259) end,1,_c,e:GetHandlerPlayer())
 end
