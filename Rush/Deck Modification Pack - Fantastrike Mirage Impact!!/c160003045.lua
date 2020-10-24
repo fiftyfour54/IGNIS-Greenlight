df859e3954d1ffbfe0b814e80342b9fc9060e17c
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-10-24 13:17:45 +0200

    added 2 rush card
    - Art of Masked Fiery Noodle Red Hot
    - Art of Masked Fiery Noodle Heartburn-Inducing Pork Roast Fat

--- Rush/Deck Modification Pack - Fantastrike Mirage Impact!!/c160003045.lua
+++ Rush/Deck Modification Pack - Fantastrike Mirage Impact!!/c160003045.lua
@@ -1,5 +1,5 @@
---火麺胸焼け背脂の術
---Art of Masked Fiery Noodle Heartburn-Inducing Pork Roast Fat
+--火麺激辛レッドの術
+--Art of Masked Fiery Noodle Red Hot
 
 --Substitute ID
 local s,id=GetID()
@@ -13,30 +13,29 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-
-function s.filter(c)
-	return c:IsFaceup() and c:IsAttackPos() and c:GetAttack()>0
-end
 	--Activation legality
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsRace,RACE_PYRO),tp,LOCATION_MZONE,0,1,nil) 
-	and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_ONFIELD,0,1,nil)
-	end
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsRace,RACE_PYRO),tp,LOCATION_MZONE,0,1,nil) end
 end
 	--Make 1 fish monster you control gain ATK
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local ct=Duel.GetMatchingGroupCount(Card.IsRace,c:GetControler(),LOCATION_MZONE,0,nil,RACE_PYRO)
 	--Effect
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,aux.FilterFaceupFunction(Card.IsRace,RACE_PYRO),tp,LOCATION_MZONE,0,1,1,nil)
 	if #g>0 then
 		Duel.HintSelection(g)
+		local tc=g:GetFirst()
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(ct*(-400))
+		e1:SetValue(300)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		g:GetFirst():RegisterEffect(e1)
+		tc:RegisterEffect(e1)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_PIERCE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
 	end
 end
