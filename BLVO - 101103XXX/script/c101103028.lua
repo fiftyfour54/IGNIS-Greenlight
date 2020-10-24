e804c104579565eec9f38ba11a94f874ea5bd091
lrhel <lrhel@users.noreply.github.com>
2020-10-24 04:45:40 +0200

    edit "Live☆Twin Lilla Treat"
    Add listed series

--- BLVO - 101103XXX/script/c101103028.lua
+++ BLVO - 101103XXX/script/c101103028.lua
@@ -26,6 +26,7 @@ function s.initial_effect(c)
 	e2:SetOperation(s.operation)
 	c:RegisterEffect(e2)
 end
+s.listed_series={0x153,0x155}
 function s.spcon(e,c)
 	if c==nil then return true end
 	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
