23fde6579c6b8f70b5ceb38d32586a78bcfbd2f5
Dylam De La Torre <dyxel04@gmail.com>
2020-10-30 14:50:32 +0100

    add Electro-Magnetic Railgun (tested)

---
+++ BLVO - 101103XXX/script/c101103078.lua
@@ -0,0 +1,38 @@
+--Ｅ．Ｍ．Ｒ．
+--Electro-Magnetic Railgun
+--Scripted by DyXel
+
+local s,id=GetID()
+function s.initial_effect(c)
+	--Tribute and destroy based on og ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(TIMING_ATTACK,TIMINGS_CHECK_MONSTER_E+TIMING_ATTACK)
+	e1:SetCost(s.descost)
+	e1:SetTarget(s.destarget)
+	e1:SetOperation(s.desact)
+	c:RegisterEffect(e1)
+end
+function s.costfilter(c)
+	return c:IsMonster() and c:IsRace(RACE_MACHINE) and c:GetBaseAttack()>=1000
+end
+function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,s.costfilter,1,nil) end
+	local g=Duel.SelectReleaseGroupCost(tp,s.costfilter,1,1,false,nil,nil)
+	local mtgc=math.floor(g:GetFirst():GetBaseAttack()/1000)
+	Duel.Release(g,REASON_COST)
+	e:SetLabel(mtgc)
+end
+function s.destarget(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,e:GetLabel(),nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+end
+function s.desact(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetTargetCards(e)
+	if #g>0 then Duel.Destroy(g,REASON_EFFECT) end
+end
