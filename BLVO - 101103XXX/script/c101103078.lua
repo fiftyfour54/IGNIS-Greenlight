d0d51af882c6d49e4e9403aa593b6fccb8e2b179
Dylam De La Torre <dyxel04@gmail.com>
2020-10-30 15:36:57 +0100

    update Electro-Magnetic Railgun
    add chkc check
    move cost to target and make cost check if it was skipped

--- BLVO - 101103XXX/script/c101103078.lua
+++ BLVO - 101103XXX/script/c101103078.lua
@@ -2,6 +2,8 @@
 --Electro-Magnetic Railgun
 --Scripted by DyXel
 
+DIDNT_SKIP_COST=0xDEADBEEF
+
 local s,id=GetID()
 function s.initial_effect(c)
 	--Tribute and destroy based on og ATK
@@ -12,7 +14,7 @@ function s.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(TIMING_ATTACK,TIMINGS_CHECK_MONSTER_E+TIMING_ATTACK)
 	e1:SetCost(s.descost)
-	e1:SetTarget(s.destarget)
+	e1:SetTarget(s.destg)
 	e1:SetOperation(s.desact)
 	c:RegisterEffect(e1)
 end
@@ -20,16 +22,21 @@ function s.costfilter(c)
 	return c:IsMonster() and c:IsRace(RACE_MACHINE) and c:GetBaseAttack()>=1000
 end
 function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckReleaseGroup(tp,s.costfilter,1,nil) end
-	local g=Duel.SelectReleaseGroupCost(tp,s.costfilter,1,1,false,nil,nil)
-	local mtgc=g:GetFirst():GetBaseAttack()//1000
-	Duel.Release(g,REASON_COST)
-	e:SetLabel(mtgc)
+	e:SetLabel(DIDNT_SKIP_COST)
+	if chk==0 then return true end
 end
-function s.destarget(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return Duel.IsExistingTarget(nil,tp,0,LOCATION_ONFIELD,1,nil) end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) end
+	if chk==0 then
+		if e:GetLabel()~=DIDNT_SKIP_COST then return false end
+		return Duel.CheckReleaseGroup(tp,s.costfilter,1,nil) and
+		       Duel.IsExistingTarget(nil,tp,0,LOCATION_ONFIELD,1,nil)
+	end
+	local tg=Duel.SelectReleaseGroupCost(tp,s.costfilter,1,1,false,nil,nil)
+	local mtgc=tg:GetFirst():GetBaseAttack()//1000
+	Duel.Release(tg,REASON_COST)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,nil,tp,0,LOCATION_ONFIELD,1,e:GetLabel(),nil)
+	local g=Duel.SelectTarget(tp,nil,tp,0,LOCATION_ONFIELD,1,mtgc,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
 end
 function s.desact(e,tp,eg,ep,ev,re,r,rp)
