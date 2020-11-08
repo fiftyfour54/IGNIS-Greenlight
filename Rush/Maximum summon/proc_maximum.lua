65304052532b01443360a59ebf2ce883e1ab6864
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-11-08 01:23:42 +0100

    Update proc_maximum.lua
    made the maximum attack handler impossible to disable

--- Rush/Maximum summon/proc_maximum.lua
+++ Rush/Maximum summon/proc_maximum.lua
@@ -84,7 +84,7 @@ end
 function Card.AddMaximumAtkHandler(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCondition(aux.IsMaximumMode)--supposition, to be checked when we get more information
 	e1:SetCode(EFFECT_SET_BASE_ATTACK)
