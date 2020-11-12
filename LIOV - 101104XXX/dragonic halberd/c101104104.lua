628c2664304aadfb468a4818101cc4e576f6e9aa
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-11-12 23:20:49 +0100

    added dragonic utopia prototype

---
+++ LIOV - 101104XXX/dragonic halberd/c101104104.lua
@@ -0,0 +1,56 @@
+-- Dragonic Utopia Ray
+local s,id=GetID()
+function s.initial_effect(c)
+	--xyz summon
+	Xyz.AddProcedure(c,nil,5,3)
+	c:EnableReviveLimit()
+	--Equip
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_EQUIP)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetTarget(s.eqtg)
+	e1:SetOperation(s.eqop)
+	c:RegisterEffect(e1)
+	aux.AddEREquipLimit(c,nil,s.eqval,s.equipop,e1)
+	
+end
+
+function s.eqval(ec,c,tp)
+	return ec:IsControler(tp) and ec:IsSetCard(0x7e)
+end
+function s.filter(c)
+	local eff={c:GetCardEffect(89785779)}
+	if not (c:IsSetCard(0x7e) and not c:IsForbidden()) then return false end
+	for _,te in ipairs(eff) do
+		if te:GetValue()(ec,c,tp) then return true end
+	end
+	return false
+end
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK+LOCATION_HAND,0,1,nil,e:GetHandler()) end
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_DECK+LOCATION_HAND)
+end
+function s.equipop(c,e,tp,tc)
+	aux.EquipByEffectAndLimitRegister(c,e,tp,tc,nil,true)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_EQUIP_LIMIT)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetValue(true)
+	e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+	tc:RegisterEffect(e2)
+end
+function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 or c:IsFacedown() or not c:IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK+LOCATION_HAND,0,1,1,nil,c)
+	local tc=g:GetFirst()
+	if tc then
+		local eff=tc:GetCardEffect(89785779)
+		eff:GetOperation()(tc,eff:GetLabelObject(),tp,c)
+	end
+end
