ffac2f182bc9f18fe5053441ef35db28704b8c33
edo9300 <edoardo762@gmail.com>
2020-11-13 12:55:45 +0100

    Add Radiant Seiyaryu
    From windows 2000 with love :flushed:

---
+++ Selection 10/script/c100272041.lua
@@ -0,0 +1,90 @@
+--聖夜に煌めく竜
+--Radiant Seiyaryu
+local s,id=GetID()
+function s.initial_effect(c)
+	--If normal or special summoned from hand, destroy 1
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DAMAGE)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	-- e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCondition(s.descon)
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	--Cannot be destroyed by battle with DARKs
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e3:SetValue(s.tglimit)
+	c:RegisterEffect(e3)
+	--Cannot be destroyed by DARKs' effects
+	local e4=e3:Clone()
+	e4:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	c:RegisterEffect(e4)
+	--banish until ep and chain attack
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,1))
+	e5:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
+	e5:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCountLimit(1)
+	e5:SetCondition(s.bancon)
+	e5:SetOperation(s.banop)
+	c:RegisterEffect(e5)
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_HAND)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() end
+	if chk==0 then return Duel.IsExistingTarget(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
+end
+function s.tglimit(e,c)
+	return c and c:IsAttribute(ATTRIBUTE_DARK)
+end
+function s.bancon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local d=c:GetBattleTarget()
+	return c==Duel.GetAttacker() and d and d:IsFaceup() and not d:IsControler(tp) and d:IsAbleToRemove()
+end
+function s.banop(e,tp,eg,ep,ev,re,r,rp)
+	local d=Duel.GetAttacker():GetBattleTarget()
+	if d and d:IsRelateToBattle() and d:IsFaceup() then
+		if Duel.Remove(d,0,REASON_EFFECT+REASON_TEMPORARY)~=0 then
+			d:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			e1:SetCode(EVENT_PHASE+PHASE_END)
+			e1:SetReset(RESET_PHASE+PHASE_END)
+			e1:SetLabelObject(d)
+			e1:SetCountLimit(1)
+			e1:SetCondition(s.retcon)
+			e1:SetOperation(s.retop)
+			Duel.RegisterEffect(e1,tp)
+			if c:CanChainAttack() then
+				Duel.ChainAttack()
+			end
+		end
+	end
+end
+function s.retcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetLabelObject():GetFlagEffect(id)~=0
+end
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.ReturnToField(e:GetLabelObject())
+end
