fa74aa20a31c17300b2b1682a0598139a6755e0e
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-11-16 16:47:12 +0100

    yggdrago left/right reverse

--- Rush/Maximum summon/c160202010.lua
+++ Rush/Maximum summon/c160202010.lua
@@ -11,7 +11,6 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
-	c:AddSideMaximumHandler(e1)
 end
 function s.maxCon(e)
 	--maximum mode check to do
