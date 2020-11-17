701728fe4b42921a6e8349efa0cda1ca8900ff45
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-11-17 15:24:47 +0100

    Update proc_maximum.lua

--- Rush/Maximum summon/proc_maximum.lua
+++ Rush/Maximum summon/proc_maximum.lua
@@ -323,3 +323,12 @@ function Duel.GetFieldGroupCountRush(player, p1, p2)
 	local maxi=Duel.GetMatchingGroupCount(Card.IsMaximumModeSide,player,p1,p2,nil)
 	return Duel.GetFieldGroupCount(player,p1,p2)-maxi
 end
+
+--function used to register an effect on all part of a Maximum monster instead of just a part (for ex, if you want to update the atk, you use that effect to register the EFFECT_UPDATE_ATTACK to the 3 part of the monster)
+function Card.RegisterEffectRush(c,eff)
+	local g=Duel.GetMatchingGroup(Card.IsMaximumMode,c:GetControler(),LOCATION_MZONE,0,nil)
+	local tc=g:GetFirst()
+	for tc in aux.Next(g) do
+		tc:RegisterEffect(eff)
+	end
+end
