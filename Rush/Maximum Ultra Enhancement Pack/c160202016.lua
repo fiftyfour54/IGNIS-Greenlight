5f414a74e131ff2404b316db5554b7ac6322188c
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-11-18 14:55:48 +0100

    added Zeptron

--- Rush/Maximum Ultra Enhancement Pack/c160202016.lua
+++ Rush/Maximum Ultra Enhancement Pack/c160202016.lua
@@ -1,5 +1,5 @@
---アトロン 
---Attron
+--ゼプトロン  
+--Zeptron
 local s,id=GetID()
 function s.initial_effect(c)
 	--Special Summon
@@ -14,18 +14,19 @@ function s.initial_effect(c)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
-s.listed_names={160202014}
+s.listed_names={160202014,160202014}
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==1 
 	and Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_GRAVE,0,1,nil,160202014)
+	and Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_GRAVE,0,1,nil,160202015)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
 	Duel.SetTargetPlayer(tp)
-	Duel.SetTargetParam(1)
+	Duel.SetTargetParam(2)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
@@ -33,7 +34,12 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if Duel.SendtoGrave(c,REASON_COST)>0 then
 		--effect
-		local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-		Duel.Draw(p,d,REASON_EFFECT)
+		if Duel.Draw(tp,2,REASON_EFFECT)>0 then
+			Duel.Hint(HINT_SELECTMSG,p,HINTMSG_TODECK)
+			local dg=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_HAND,0,2,2,nil)
+			local opt=Duel.SelectOption(tp,aux.Stringid(id,0),aux.Stringid(id,1))
+			Duel.SendtoDeck(dg,nil,SEQ_DECKBOTTOM,REASON_EFFECT)
+			Duel.SortDeckbottom(tp,tp,#dg)
+		end
 	end
 end
