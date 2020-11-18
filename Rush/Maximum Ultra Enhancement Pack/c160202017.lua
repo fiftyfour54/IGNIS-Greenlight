3862a758ba1d04780ecc9e779bd31ac4288efb70
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-11-18 15:03:31 +0100

    added Yoctron

--- Rush/Maximum Ultra Enhancement Pack/c160202017.lua
+++ Rush/Maximum Ultra Enhancement Pack/c160202017.lua
@@ -1,5 +1,5 @@
---ゼプトロン  
---Zeptron
+--ヨクトロン 
+--Yoctron
 local s,id=GetID()
 function s.initial_effect(c)
 	--Special Summon
@@ -14,31 +14,30 @@ function s.initial_effect(c)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
-s.listed_names={160202014,160202014}
+s.listed_names={160202014,160202015,160202016}
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==1 
-	and Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_GRAVE,0,1,nil,160202014)
+	return Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_GRAVE,0,1,nil,160202014)
 	and Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_GRAVE,0,1,nil,160202015)
+	and Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_GRAVE,0,1,nil,160202016)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
+if chk==0 then return Duel.IsPlayerCanDraw(tp,3) end
 	Duel.SetTargetPlayer(tp)
-	Duel.SetTargetParam(2)
-	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+	Duel.SetTargetParam(3)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,3)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local c=e:GetHandler()
 	if Duel.SendtoGrave(c,REASON_EFFECT)>0 then
 		--effect
-		if Duel.Draw(tp,2,REASON_EFFECT)>0 then
+		if Duel.Draw(tp,3,REASON_EFFECT)>0 then
 			Duel.Hint(HINT_SELECTMSG,p,HINTMSG_TODECK)
-			local dg=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_HAND,0,2,2,nil)
-			Duel.SendtoDeck(dg,nil,SEQ_DECKBOTTOM,REASON_EFFECT)
-			Duel.SortDeckbottom(tp,tp,#dg)
+			local dg=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,LOCATION_HAND,0,3,3,nil)
+			Duel.SendtoGrave(dg,REASON_EFFECT)
 		end
 	end
 end
