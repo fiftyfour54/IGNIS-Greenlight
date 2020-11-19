ca6d10f8776425568ae6737d602dc406d809f2c1
Dylam De La Torre <dyxel04@gmail.com>
2020-11-19 13:34:18 +0100

    update "Contract With the Void"

--- 21PP - 100274XXX/c100274012.lua
+++ 21PP - 100274XXX/c100274012.lua
@@ -1,6 +1,7 @@
 --煉獄の契約
 --Contract with the Void
 --Scripted by Rundas
+
 local s,id=GetID()
 function s.initial_effect(c)
 	--Discard + Special Summon
@@ -9,28 +10,32 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_HANDES)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(s.spcon)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 end
 s.listed_series={0xb}
-function s.filter(c,e,tp)
-	return (c:IsSetCard(0xb) or (c:IsType(TYPE_SYNCHRO) and c:IsAttribute(ATTRIBUTE_DARK) and c:IsLevel(8) and c:IsRace(RACE_DRAGON))) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetMatchingGroupCount(nil,tp,LOCATION_HAND,0,e:GetHandler())>=3
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
-	g:RemoveCard(e:GetHandler())
-	if chk==0 then return #g>=3 end
-	Duel.SetOperationInfo(0,CATEGORY_HANDES,g,1,0,0)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_HANDES,Duel.GetFieldGroup(tp,LOCATION_HAND,0),1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,0,tp,1)
 end
+function s.spfilter(c,e,tp)
+	return c:IsType(TYPE_MONSTER) and
+	       (c:IsSetCard(0xb) or (c:IsType(TYPE_SYNCHRO) and c:IsAttribute(ATTRIBUTE_DARK) and c:IsLevel(8) and c:IsRace(RACE_DRAGON))) and
+	       c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
-	if Duel.SendtoGrave(g,REASON_EFFECT+REASON_DISCARD)==0 or (not Duel.IsExistingMatchingCard(aux.NecroValleyFilter(s.filter),tp,LOCATION_GRAVE,0,1,nil,e,tp)) then return end
+	if Duel.SendtoGrave(Duel.GetFieldGroup(tp,LOCATION_HAND,0),REASON_EFFECT+REASON_DISCARD)==0 or
+	   (not Duel.IsExistingMatchingCard(aux.NecroValleyFilter(s.spfilter),tp,LOCATION_GRAVE,0,1,nil,e,tp)) then return end
 	if Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
 		Duel.BreakEffect()
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local tc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.filter),tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+		local tc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 		if tc then Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP) end
 	end
 end
