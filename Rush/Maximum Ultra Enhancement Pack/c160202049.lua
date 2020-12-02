08279a0276f3a4e4c4e563b503785b579105094c
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-12-02 15:33:49 +0100

    Update c160202049.lua

--- Rush/Maximum Ultra Enhancement Pack/c160202049.lua
+++ Rush/Maximum Ultra Enhancement Pack/c160202049.lua
@@ -5,7 +5,7 @@ function s.initial_effect(c)
 	--Destroy
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
