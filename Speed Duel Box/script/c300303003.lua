4d756300270b5816584f4ef11f51491bcf4fdc0d
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-12-14 00:13:05 +0100

    added Hieratic Chant

---
+++ Speed Duel Box/script/c300303003.lua
@@ -0,0 +1,40 @@
+--Hieratic Chant
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop,1)
+end
+function s.filter(c)
+	return c:IsCode(CARD_RA) and c:IsAbleToHand()
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--opt check
+	if Duel.GetFlagEffect(ep,id)>0 then return end
+	--condition
+	return aux.CanActivateSkill(tp)
+	and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) 
+	and Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)>0
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	local g=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
+	if Duel.SendtoGrave(g,REASON_COST+REASON_DISCARD)>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+		if #g>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetDescription(aux.Stringid(id,2))
+			e1:SetType(EFFECT_TYPE_FIELD)
+			e1:SetTargetRange(LOCATION_HAND,0)
+			e1:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+			e1:SetValue(0x1)
+			e1:SetReset(RESET_PHASE+PHASE_END)
+			Duel.RegisterEffect(e1,tp)
+			local e2=e1:Clone()
+			e2:SetCode(EFFECT_EXTRA_SET_COUNT)
+			Duel.RegisterEffect(e2,tp)
+		end
+	end
+end
