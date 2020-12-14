a352c8b540207eea5dd9959e4dcf7c029fdb800c
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-12-14 16:04:16 +0100

    Hieratic Chant update
    It is flipping itself at the start of the duel

--- Speed Duel Box/script/c300303003.lua
+++ Speed Duel Box/script/c300303003.lua
@@ -2,6 +2,20 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop,1)
+	--flip at start of the duel
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_STARTUP)
+	e1:SetCountLimit(1)
+	e1:SetRange(0x5f)
+	e1:SetLabel(0)
+	e1:SetOperation(s.flipop2)
+	c:RegisterEffect(e1)
+end
+function s.flipop2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
 end
 function s.filter(c)
 	return c:IsCode(CARD_RA) and c:IsAbleToHand()
