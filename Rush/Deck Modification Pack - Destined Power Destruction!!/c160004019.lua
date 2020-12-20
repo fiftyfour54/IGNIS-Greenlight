f622a4f9124d741d9976cafcfe420dcc22f268fa
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2020-12-20 10:58:29 -0500

    Updated Rush Duel scripts Tested and applied changes.

--- Rush/Deck Modification Pack - Destined Power Destruction!!/c160004019.lua
+++ Rush/Deck Modification Pack - Destined Power Destruction!!/c160004019.lua
@@ -1,8 +1,9 @@
 --祭神モドリーナ
 --Enshrined Deity Modorina
+
 local s,id=GetID()
 function s.initial_effect(c)
-	--To Hand
+	--Return 1 level 7+ warrior monster to hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TOHAND)
 	e1:SetRange(LOCATION_MZONE)
@@ -16,15 +17,12 @@ function s.filter(c)
 	return c:IsLevelAbove(7) and c:IsRace(RACE_WARRIOR) and c:IsAbleToHand()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,e:GetHandler()) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil) end
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and c:IsFaceup() then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
-		if #g>0 then
-			Duel.SendtoHand(g,nil,REASON_EFFECT)
-		end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
 	end
 end
--- Rush/Tournament pack 1/c160406003.lua
+++ Rush/Tournament pack 1/c160406003.lua
@@ -1,20 +1,21 @@
 --ネクメイド・ナナ
 --Necmaid Nana
+
 local s,id=GetID()
 function s.initial_effect(c)
-	---Draw
+	--Return 1 level 7 monster from GY to deck, draw 2
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DRAW+CATEGORY_TODECK)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetCountLimit(1)
-    e1:SetCost(s.cost)
+	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
+	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.tdfilter,tp,LOCATION_GRAVE,0,1,e:GetHandler()) end
@@ -28,11 +29,11 @@ end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	--Requirement
-    Duel.SendtoGrave(c,REASON_COST)
-    --Effect
-    if c:IsRelateToEffect(e) then
+	if Duel.SendtoGrave(c,REASON_COST)~=0 then
+	--Effect
 		local g=Duel.SelectMatchingCard(tp,s.tdfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-		if Duel.SendtoDeck(g,nil,1,REASON_COST)>0 then
+		Duel.HintSelection(g)
+		if Duel.SendtoDeck(g,nil,SEQ_DECKBOTTOM,REASON_COST)>0 then
 			local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 			Duel.Draw(p,d,REASON_EFFECT)
 		end
