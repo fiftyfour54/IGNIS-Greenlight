465abd8fb68d0f2df28d6c7841b5c5e766b5fea8
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2020-12-25 06:57:55 -0800

    Update "Holy Night Ciel Script"

--- LIOV - 101104XXX/script/c101104019.lua
+++ LIOV - 101104XXX/script/c101104019.lua
@@ -1,12 +1,12 @@
 --ホーリーナイツ・シエル
 --Holy Night Ciel
---Scripted by The 
+--Scripted by The Razgriz
 local s,id=GetID()
 function s.initial_effect(c)
 	--Return 1 Level 7 LIGHT Dragon or Holy Night monster to SS this card
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DAMAGE)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCountLimit(1,id)
@@ -60,16 +60,6 @@ end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) then 
-		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
-	end
-end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
-end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
 		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
