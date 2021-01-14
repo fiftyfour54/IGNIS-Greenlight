21301e84abcd94856a13634ce5acc89937f63619
EerieCode <eeriecode@gmail.com>
2021-01-14 21:09:04 +0100

    Updated procedure for "Bearcti Big Dipper"

--- Deck Build Pack - Ancient Guardiants - 100416XXX/script/c100416038.lua
+++ Deck Build Pack - Ancient Guardiants - 100416XXX/script/c100416038.lua
@@ -11,13 +11,14 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--tribute substitute
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_EXTRA_RELEASE_NONSUM)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EFFECT_RELEASE_REPLACE)
 	e2:SetRange(LOCATION_FZONE)
 	e2:SetTargetRange(LOCATION_GRAVE,0)
 	e2:SetCountLimit(1)
-	e2:SetTarget(s.reltg)
-	e2:SetValue(s.relval)
+	e2:SetTarget(s.reptg)
+	e2:SetValue(s.repval)
+	e2:SetOperation(s.repop)
 	c:RegisterEffect(e2)
 	--add counter
 	local e3=Effect.CreateEffect(c)
@@ -42,13 +43,26 @@ function s.initial_effect(c)
 end
 s.listed_series={0x25b }
 s.counter_place_list={0x204 }
-function s.reltg(e,c)
-	return c:IsSetCard(0x25b) and c:IsLevelAbove(7)
-end
-function s.relval(e,re,r,rp)
+function s.repfilter(c,tp,re)
 	local rc=re:GetHandler()
-	return re:IsActivated() and re:IsActiveType(TYPE_MONSTER) 
-		and rc and rc:IsSetCard(0x25b) and (r&REASON_COST)~=0
+	return re:IsActivated() and re:IsActiveType(TYPE_MONSTER) and rc and rc:IsSetCard(0x25b) 
+		and c:IsReason(REASON_COST) and c:IsType(TYPE_MONSTER) and not c:IsReason(REASON_REPLACE)
+end
+function s.repcfilter(c)
+	return c:IsSetCard(0x25b) and c:IsLevelAbove(7) and c:IsAbleToRemoveAsCost()
+end
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return eg:IsExists(s.repfilter,1,nil,tp,re)
+		and Duel.IsExistingMatchingCard(s.repcfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	return Duel.SelectEffectYesNo(tp,e:GetHandler())
+end
+function s.repval(e,c)
+	return s.repfilter(c,e:GetHandlerPlayer(),c:GetReasonEffect())
+end
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.repcfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.Remove(g,POS_FACEUP,REASON_COST+REASON_REPLACE)
 end
 function s.countop(e,tp,eg,ep,ev,re,r,rp)
 	e:GetHandler():AddCounter(0x204,1)
