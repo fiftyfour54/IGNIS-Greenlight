fdfa520cc85451b0a28ebbb751c87d40c3d1fd45
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-01-14 17:37:09 +0100

    Update bear_proc.lua

--- Deck Build Pack - Ancient Guardiants - 100416XXX/script/bear_proc.lua
+++ Deck Build Pack - Ancient Guardiants - 100416XXX/script/bear_proc.lua
@@ -1,7 +1,7 @@
 --c:card
---comc:card to compare
+--compc:card to compare
 --val: value required
 --return if the level of c-comc or comc-c is equal to val 
 function Card.DifferLevel(c,compc,val)
-	return (c:GetLevel()-comc:GetLevel())==val or (comc:GetLevel()-c:GetLevel())==val
+	return math.abs((c:GetLevel()-compc:GetLevel()))==val
 end
