8bfcb65a1416143a1944bbf8498eedf39bdda103
EerieCode <eeriecode@gmail.com>
2021-01-14 20:05:59 +0100

    Added summon proedure for "Bearcti" Synchro monsters

--- Deck Build Pack - Ancient Guardiants - 100416XXX/script/bear_proc.lua
+++ Deck Build Pack - Ancient Guardiants - 100416XXX/script/bear_proc.lua
@@ -1,7 +1,54 @@
---c:card
---compc:card to compare
---val: value required
---return if the level of c-comc or comc-c is equal to val 
-function Card.DifferLevel(c,compc,val)
-	return math.abs((c:GetLevel()-compc:GetLevel()))==val
+--Special Summon procedure for the "Bearcti" Synchro monsters from DBAG, applied directly to the card.
+function Auxiliary.AddBearctiSummonProcedure(c)
+	c:EnableReviveLimit()
+	--Must be special summoned by its own method
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	c:RegisterEffect(e1)
+	--Special summon procedure (from extra deck)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_SPSUMMON_PROC)
+	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e2:SetRange(LOCATION_EXTRA)
+	e2:SetCondition(Auxiliary.BearctiSummonCondition)
+	e2:SetTarget(Auxiliary.BearctiSummonTarget)
+	e2:SetOperation(Auxiliary.BearctiSummonOperation)
+	c:RegisterEffect(e2)
+end
+function Auxiliary.BearctiSummonCostFilter(c)
+	return c:IsFaceup() and c:IsHasLevel() and c:IsAbleToGraveAsCost()
+end
+function Auxiliary.BearctiSummonCostCheck(lv)
+	return	function(sg,e,tp,mg)
+				if #sg==2 and sg:IsExists(Card.IsType,1,nil,TYPE_TUNER) and sg:IsExists(Card.IsNotTuner,1,nil) and Duel.GetLocationCountFromEx(tp,tp,sg,e:GetHandler())>0 then
+					local tc1=sg:GetFirst()
+					local tc2=sg:GetNext()
+					return math.abs(tc1:GetLevel()-tc2:GetLevel())==lv
+				else return false end
+			end
+end
+function Auxiliary.BearctiSummonCondition(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local lv=e:GetHandler():GetLevel()
+	local g=Duel.GetMatchingGroup(Auxiliary.BearctiSummonCostFilter,tp,LOCATION_MZONE,0,nil)
+	return aux.SelectUnselectGroup(g,e,tp,2,2,Auxiliary.BearctiSummonCostCheck(lv),0)
+end
+function Auxiliary.BearctiSummonTarget(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.GetMatchingGroup(Auxiliary.BearctiSummonCostFilter,tp,LOCATION_MZONE,0,nil)
+	local sg=aux.SelectUnselectGroup(g,e,tp,2,2,Auxiliary.BearctiSummonCostCheck(lv),1,tp,HINTMSG_TOGRAVE)
+	if #sg==2 then
+		sg:KeepAlive()
+		e:SetLabelObject(sg)
+		return true
+	end
+	return false
+end
+function s.sprop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.SendtoGrave(g,REASON_COST)
 end
--- Deck Build Pack - Ancient Guardiants - 100416XXX/script/c100416033.lua
+++ Deck Build Pack - Ancient Guardiants - 100416XXX/script/c100416033.lua
@@ -2,24 +2,7 @@
 -- Bearcti - Polari 
 local s,id=GetID()
 function s.initial_effect(c)
-	--Must be properly summoned before reviving
-	c:EnableReviveLimit()
-	--Must be special summoned by its own method
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-	c:RegisterEffect(e1)
-	--Special summon procedure (from extra deck)
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_SPSUMMON_PROC)
-	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e2:SetRange(LOCATION_EXTRA)
-	e2:SetCondition(s.sprcon)
-	e2:SetTarget(s.sprtg)
-	e2:SetOperation(s.sprop)
-	c:RegisterEffect(e2)
+	aux.AddBearctiSummonProcedure(c)
 	--activate Big Dipper from deck
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
@@ -32,47 +15,6 @@ function s.initial_effect(c)
 end
 s.listed_series={0x25b}
 s.listed_names={100416038}
-function s.sprfilter(c)
-	return c:IsFaceup() and c:IsAbleToGraveAsCost()
-end
-function s.sprfilter1(c,tp,g,sc)
-	local lv=c:GetLevel()
-	local g=Duel.GetMatchingGroup(s.sprfilter,tp,LOCATION_MZONE,0,nil)
-	return c:IsType(TYPE_TUNER) and g:IsExists(s.sprfilter2,1,c,tp,c,sc)
-end
-function s.sprfilter2(c,tp,mc,sc)
-	local sg=Group.FromCards(c,mc)
-	return c:DifferLevel(mc,1) and not c:IsType(TYPE_TUNER) and Duel.GetLocationCountFromEx(tp,tp,sg,sc)>0
-end
-function s.sprcon(e,c)
-	if c==nil then return true end
-	local tp=c:GetControler()
-	local g=Duel.GetMatchingGroup(s.sprfilter,tp,LOCATION_MZONE,0,nil)
-	return g:IsExists(s.sprfilter1,1,nil,tp,g,c)
-end
-function s.sprtg(e,tp,eg,ep,ev,re,r,rp,c)
-	local c=e:GetHandler()
-	local g=Duel.GetMatchingGroup(s.sprfilter,tp,LOCATION_MZONE,0,nil)
-	local g1=g:Filter(s.sprfilter1,nil,tp,g,c)
-	local mg1=aux.SelectUnselectGroup(g1,e,tp,1,1,nil,1,tp,HINTMSG_TOGRAVE,nil,nil,true)
-	if #mg1>0 then
-		local mc=mg1:GetFirst()
-		local g2=g:Filter(s.sprfilter2,mc,tp,mc,c,mc:GetLevel())
-		local mg2=aux.SelectUnselectGroup(g2,e,tp,1,1,nil,1,tp,HINTMSG_TOGRAVE,nil,nil,true)
-		mg1:Merge(mg2)
-	end
-	if #mg1==2 then
-		mg1:KeepAlive()
-		e:SetLabelObject(mg1)
-		return true
-	end
-	return false
-end
-function s.sprop(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=e:GetLabelObject()
-	if not g then return end
-	Duel.SendtoGrave(g,REASON_COST)
-end
 --activate Bearcti Big Dipper
 function s.accon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,tp)
@@ -85,8 +27,4 @@ function s.acop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
 	local tc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.filter),tp,LOCATION_DECK,0,1,1,nil,tp):GetFirst()
 	aux.PlayFieldSpell(tc,e,tp,eg,ep,ev,re,r,rp)
-end
---to delete later, part of the proc
-function Card.DifferLevel(c,compc,val)
-	return math.abs((c:GetLevel()-compc:GetLevel()))==val
 end
