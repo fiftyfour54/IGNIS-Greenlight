ec412c2e475f6af2104fe36b772b334bc9d3e1a7
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-01-19 18:41:18 +0100

    War Rock Skyler fix
    forgot TARGET flag

--- BLVO - 101103XXX/script/c101103096.lua
+++ BLVO - 101103XXX/script/c101103096.lua
@@ -12,6 +12,7 @@ function s.initial_effect(c)
 	--If your Earth Warrior monster battles
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
 	e2:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
