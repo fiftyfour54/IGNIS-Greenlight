6e3baffb8fc59964e6d87a7b8e94fa4923892a94
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-01-22 17:06:29 +0100

    added pendulum encore

---
+++ BLVO - 101103XXX/script/c101103091.lua
@@ -0,0 +1,33 @@
+--Pendulum Encore
+local s,id=GetID()
+function s.initial_effect(c)
+	--synchro effect
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_BATTLE_START+TIMING_BATTLE_END)
+	e1:SetCondition(s.pencon)
+	e1:SetCost(s.pencost)
+	e1:SetTarget(s.pentg)
+	e1:SetOperation(s.penop)
+	c:RegisterEffect(e1)
+end
+function s.pencon(e,tp,eg,ep,ev,re,r,rp)
+	local ph=Duel.GetCurrentPhase()
+	return Duel.GetTurnPlayer()==1-tp and (ph==PHASE_MAIN1 or ph==PHASE_MAIN2)
+end
+function s.pencost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+end
+function s.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanPendulumSummon(tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA+LOCATION_HAND)
+end
+function s.penop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsPendulumSummonable,tp,LOCATION_EXTRA+LOCATION_HAND,0,nil,nil)
+	if #g>0 then
+		Duel.PendulumSummon(tp)
+	end
+end
