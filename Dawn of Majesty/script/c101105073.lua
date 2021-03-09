1b26c4a2dc6ec2afe6e507c3a1730de24d4fd4c1
XyleN5967 <73727154+XyleN5967@users.noreply.github.com>
2021-03-09 18:35:47 +0800

    fix

--- Dawn of Majesty/script/c101105073.lua
+++ Dawn of Majesty/script/c101105073.lua
@@ -16,7 +16,7 @@ function s.initial_effect(c)
 end
 s.listed_series={SET_MAGIKEY}
 function s.cfilter(c) 
-	return c:IsFaceup() and (c:IsSetCard(SET_MAGIKEY) and c:IsType(TYPE_RITUAL)
+	return c:IsFaceup() and (c:IsType(TYPE_MONSTER)
 		or c:IsSetCard(SET_MAGIKEY) and c:IsType(TYPE_MONSTER) and c:IsSummonLocation(LOCATION_EXTRA))
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
@@ -31,27 +31,23 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
 	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re)
-		and Duel.Destroy(eg,REASON_EFFECT)~=0
-		and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		and Duel.Destroy(eg,REASON_EFFECT)~=0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATTRIBUTE)
 		local att=Duel.AnnounceAttribute(tp,1,0xffff)
 		e:SetLabel(att)
-		--all face-up monster become the declared attribute
-		s.operation(e,tp,eg,ep,ev,re,r,rp)
+		local g=Duel.GetMatchingGroup(aux.FilterFaceupFunction(Card.IsType,TYPE_MONSTER),tp,0,LOCATION_MZONE,nil)
+		local tc=g:GetFirst() 
+		if tc then
+			for tc in aux.Next(g) do
+				local e1=Effect.CreateEffect(c)
+				e1:SetType(EFFECT_TYPE_SINGLE)
+				e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+				e1:SetValue(e:GetLabel())
+				e1:SetReset(RESET_PHASE+PHASE_END)
+				tc:RegisterEffect(e1) 
+			end
+		end
 	end
-end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
-	e1:SetTargetRange(0,LOCATION_MZONE)
-	e1:SetValue(e:GetLabel())
-	e1:SetTarget(s.attg)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1,tp)
-end
-function s.attg(e,c)
-	return c:IsFaceup() and c:IsType(TYPE_MONSTER)
 end
