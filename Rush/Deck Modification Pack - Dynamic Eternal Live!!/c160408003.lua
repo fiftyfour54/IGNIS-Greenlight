4910e54125c53ca57b6abc8cc373c17f5cae6f01
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-03-12 17:05:05 +0100

    Update c160408003.lua

--- Rush/Deck Modification Pack - Dynamic Eternal Live!!/c160408003.lua
+++ Rush/Deck Modification Pack - Dynamic Eternal Live!!/c160408003.lua
@@ -3,7 +3,7 @@
 local s,id=GetID()
 function s.initial_effect(c)	
 	Card.Alias(c,74848038)
-	--Make 1 monster be able to inflict piercing damage
+	--add 1 monster from the grave to the hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCategory(CATEGORY_TOHAND)
@@ -23,7 +23,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,1,e:GetHandler())
@@ -35,14 +35,6 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		if #g>0 then
 			Duel.SendtoHand(g,nil,REASON_EFFECT)
 			Duel.ConfirmCards(1-tp,g)
-			--Cannot attack directly this turn
-			local e1=Effect.CreateEffect(e:GetHandler())
-			e1:SetType(EFFECT_TYPE_FIELD)
-			e1:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
-			e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-			e1:SetTargetRange(LOCATION_MZONE,0)
-			e1:SetReset(RESET_PHASE+PHASE_END)
-			Duel.RegisterEffect(e1,tp)
 		end
 	end
 end
