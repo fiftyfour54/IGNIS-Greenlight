e4ef8215fadeed08d8e0cfa09234331c9afaeaf8
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2021-03-16 15:47:18 -0700

    Update "Flower Fang Spores"

--- Rush/Deck Modification Pack - Dynamic Eternal Live!!/c160005048.lua
+++ Rush/Deck Modification Pack - Dynamic Eternal Live!!/c160005048.lua
@@ -8,6 +8,7 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(s.condition)
 	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
@@ -16,6 +17,9 @@ end
 function s.cfilter(c)
 	return c:IsType(TYPE_MONSTER+TYPE_NORMAL) and c:IsRace(RACE_PLANT) and c:IsLevelBelow(4)
 end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,1,nil) end
+end
 function s.spfilter(c,e,tp)
 	return s.cfilter(c) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
@@ -23,14 +27,13 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,1) end
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	--requirement
 	if Duel.DiscardDeck(tp,1,REASON_COST)>0 then
 		--effect
-		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 		local c=e:GetHandler()
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
