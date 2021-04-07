ec03df0adb9c0c2b2bbadf66751b274abae401dd
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-04-07 14:49:29 +0200

    added Acetic Acid Trap Hole and Majinrai, the Magical Lightning

--- Rush/Deck Modification Pack - Dynamic Eternal Live!!/c160005050.lua
+++ Rush/Deck Modification Pack - Dynamic Eternal Live!!/c160005050.lua
@@ -1,26 +1,50 @@
---Royal Demon's Live Arena
+-- 魔迅雷 
+-- Majinrai, the Magical Lightning
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_HANDES+CATEGORY_DAMAGE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCondition(s.actcond)
+	e1:SetCost(s.cost)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	--Decrease ATK
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_UPDATE_ATTACK)
-	e2:SetRange(LOCATION_FZONE)
-	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e2:SetTarget(aux.NOT(aux.TargetBoolFunction(Card.IsAttribute,ATTRIBUTE_WATER)))
-	e2:SetValue(-300)
-	c:RegisterEffect(e2)
-	--Decrease DEF
-	local e3=e2:Clone()
-	e3:SetCode(EFFECT_UPDATE_DEFENSE)
-	c:RegisterEffect(e3)
+	
 end
-function s.actcond(e,tp,eg,ep,ev,re,r,rp)
-	 return Duel.GetMatchingGroupCountRush(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)>1
+s.listed_names={CARD_SUMMONED_SKULL}
+function s.costfilter(c)
+	return c:IsType(TYPE_NORMAL) and c:IsDiscardable() and c:IsAbleToGraveAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+end
+function s.filter(c)
+	return c:IsRace(RACE_FIED) and c:IsLevelAbove(7) and c:IsType(TYPE_NORMAL) and c:IsFaceup() 
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return 
+	Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsCode,CARD_SUMMONED_SKULL),tp,LOCATION_MZONE,0,1,nil)
+	or
+	Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil)
+end
+
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(1300)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,1300)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	-- requirement
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	local ct=Duel.SendtoGrave(g,REASON_COST)
+	if ct>0 then
+		--Effect
+		local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+		Duel.Damage(p,d,REASON_EFFECT)
+	end
 end
