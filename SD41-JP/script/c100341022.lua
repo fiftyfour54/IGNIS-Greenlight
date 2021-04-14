a4dda0b1eefdd5b1d9d4344ce77bd8a47485de89
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2021-04-13 17:03:30 -0700

    Update "Eternal Cyber"

--- SD41-JP/script/c100341022.lua
+++ SD41-JP/script/c100341022.lua
@@ -5,10 +5,11 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Return to Deck/SS
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TODECK)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOEXTRA)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
@@ -17,6 +18,7 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EFFECT_DESTROY_REPLACE)
 	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,id+100)
 	e2:SetTarget(s.reptg)
 	e2:SetValue(s.repval)
 	e2:SetOperation(s.repop)
@@ -24,27 +26,25 @@ function s.initial_effect(c)
 end
 s.listed_series={0x93}
 function s.cybfilter(c)
-  return c:IsType(TYPE_FUSION) and c:IsMonster() and c:IsRace(RACE_MACHINE) and c:IsSetCard(0x93)
+	return c:IsType(TYPE_FUSION) and c:IsMonster() and c:IsRace(RACE_MACHINE) and c:IsSetCard(0x93)
 end
-function s.filter(c,e,tp,ft)
-	return s.cybfilter(c) and (c:IsAbleToDeck() or (ft>0 and c:IsCanBeSpecialSummoned(e,0,tp,true,false)))
+function s.filter(c,e,tp)
+	return s.cybfilter(c) and (c:IsAbleToDeck() or c:IsCanBeSpecialSummoned(e,0,tp,true,false))
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.filter(chkc,e,tp,ft) end
-	if chk==0 then return ft>0 and Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp,ft) end
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.filter(chkc,e,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,ft)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON+CATEGORY_TODECK,g,1,0,0)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if tc and tc:IsRelateToEffect(e) then
-		Duel.BreakEffect()
-		if tc:IsAbleToDeck() and (not tc:IsCanBeSpecialSummoned(e,0,tp,true,false) or ft<=0 or Duel.SelectYesNo(tp,aux.Stringid(id,0))) then
+		local op=Duel.SelectOption(tp,aux.Stringid(id,0),aux.Stringid(id,1))
+		if tc:IsAbleToDeck() and (not tc:IsCanBeSpecialSummoned(e,0,tp,true,false) or ft<=0 or op==0) then
 			Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
-		elseif ft>0 and tc:IsCanBeSpecialSummoned(e,0,tp,true,false) and (not tc:IsAbleToDeck() or Duel.SelectYesNo(tp,aux.Stringid(id,1))) then
+		elseif ft>0 and tc:IsCanBeSpecialSummoned(e,0,tp,true,false) and (not tc:IsAbleToDeck() or op==1) then
 			Duel.SpecialSummon(tc,0,tp,tp,true,false,POS_FACEUP)
 		end
 	end
