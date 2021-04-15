c82f173b8da8d9ca27c5eb2e8464b4b72cfc6fff
Xy❤️ <80439628+XyleN5967@users.noreply.github.com>
2021-04-15 11:52:31 +0800

    add エコール・ド・ゾーン

---
+++ AC01-JP/c100278021.lua
@@ -0,0 +1,90 @@
+--エコール・ド・ゾーン
+--Script by XyLeN
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e0=Effect.CreateEffect(c) 
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
+	--token
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_TOKEN)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetRange(LOCATION_FZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.tkcon)
+	e1:SetTarget(s.tktg)
+	e1:SetOperation(s.tkop)
+	c:RegisterEffect(e1)
+	--destroy
+	local e2=Effect.CreateEffect(c) 
+	e2:SetDescription(aux.Stringid(id,1)) 
+	e2:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetCode(EVENT_LEAVE_FIELD)
+	e2:SetCondition(s.descon)
+	e2:SetTarget(s.destg)
+	e2:SetOperation(s.desop)
+	c:RegisterEffect(e2)
+end
+function s.tkcon(e,tp,eg,ep,ev,re,r,rp)
+	if #eg~=1 then return false end
+	local tc=eg:GetFirst()
+	return tc~=e:GetHandler() and tc:IsFaceup() and (tc:GetAttack()>0 and tc:GetDefense()>0) and (tc:IsSummonPlayer(1-tp) or tc:IsSummonPlayer(tp))
+end
+function s.tktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	--not sure the operation info here
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,tp,0)
+end
+function s.tkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=eg:GetFirst()
+	if tc:IsFaceup() and tc:IsLocation(LOCATION_MZONE) and not tc:IsImmuneToEffect(e) then
+		local atk=tc:GetAttack() 
+		local def=tc:GetDefense()
+		local p=tc:GetControler()
+		if Duel.Destroy(tc,REASON_EFFECT)==0 then return end
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsPlayerCanSpecialSummonMonster(tp,id+100,0,TYPES_TOKEN,0,0,1,RACE_SPELLCASTER,ATTRIBUTE_DARK) then
+			local token=Duel.CreateToken(tp,id+100) 
+			Duel.SpecialSummonStep(token,0,p,p,false,false,POS_FACEUP)
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_SET_ATTACK)
+			e1:SetValue(atk)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD)
+			token:RegisterEffect(e1)
+			local e2=e1:Clone()
+			e2:SetCode(EFFECT_SET_DEFENSE)
+			e2:SetValue(def)
+			token:RegisterEffect(e2)
+			local e3=Effect.CreateEffect(c)
+			e3:SetDescription(3206)
+			e3:SetType(EFFECT_TYPE_SINGLE)
+			e3:SetCode(EFFECT_CANNOT_ATTACK)
+			e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_OATH+EFFECT_FLAG_CLIENT_HINT)
+			e3:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD)
+			token:RegisterEffect(e3)
+		end
+		Duel.SpecialSummonComplete()
+	end
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousPosition(POS_FACEUP) and c:IsPreviousLocation(LOCATION_ONFIELD)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(Card.IsCode,tp,LOCATION_MZONE,LOCATION_MZONE,0,nil,id+100)
+	if chk==0 then return #g>0 end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsCode,tp,LOCATION_MZONE,LOCATION_MZONE,0,nil,id+100)
+	if #g>0 then
+		Duel.Destroy(g,REASON_EFFECT)
+	end
+end
