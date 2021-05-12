679ce758fcd9b97a3f290ce05786bc3e78aea93a
EerieCode <eeriecode@gmail.com>
2021-05-12 10:01:52 +0200

    Effect update

--- AC01-JP/c100278012.lua
+++ AC01-JP/c100278012.lua
@@ -27,13 +27,46 @@ end
 function s.op(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
+	local race=e:GetLabel()
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CHANGE_RACE)
 	e1:SetRange(LOCATION_SZONE)
 	e1:SetTargetRange(LOCATION_GRAVE,LOCATION_GRAVE)
-	e1:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_MONSTER))
-	e1:SetValue(e:GetLabel())
+	e1:SetTarget(s.rctg)
+	e1:SetValue(race)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 	c:RegisterEffect(e1)
+	--id chk
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(id)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetTargetRange(1,1)
+	e2:SetValue(s.val(race))
+	c:RegisterEffect(e2)
+end
+function s.rctg(e,c)
+	if c:GetFlagEffect(1)==0 then
+		c:RegisterFlagEffect(1,0,0,0)
+		local eff
+		if c:IsLocation(LOCATION_MZONE) then
+			eff={Duel.GetPlayerEffect(c:GetControler(),EFFECT_NECRO_VALLEY)}
+		else
+			eff={c:GetCardEffect(EFFECT_NECRO_VALLEY)}
+		end
+		c:ResetFlagEffect(1)
+		for _,te in ipairs(eff) do
+			local op=te:GetOperation()
+			if not op or op(e,c) then return false end
+		end
+	end
+	return true
+end
+function s.val(race)
+	return  function(e,c,re,chk)
+				if chk==0 then return true end
+				return race
+			end
 end
