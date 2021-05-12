fe96a9cbba49f53fde653c576064f9259e84e72d
EerieCode <eeriecode@gmail.com>
2021-05-12 09:33:45 +0200

    AC01-JP012 --輪廻独断

---
+++ AC01-JP/c100278012.lua
@@ -0,0 +1,39 @@
+--輪廻独断
+--Declaration of Rebirth
+--Scripted by Eerie Code
+local s,id=GetID()
+function s.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--change race
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(s.tg)
+	e2:SetOperation(s.op)
+	c:RegisterEffect(e2)
+end
+function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local rc=Duel.AnnounceRace(tp,1,RACE_ALL)
+	e:SetLabel(rc)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CHANGE_RACE)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetTargetRange(LOCATION_GRAVE,LOCATION_GRAVE)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_MONSTER))
+	e1:SetValue(e:GetLabel())
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	c:RegisterEffect(e1)
+end
