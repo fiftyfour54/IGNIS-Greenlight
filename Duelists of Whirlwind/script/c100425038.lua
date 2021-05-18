4aeecf116485dbc0c0d4fe99c1ca1d0e2b42f1f4
EerieCode <eeriecode@gmail.com>
2021-05-18 14:16:17 +0200

    DP25-JP038 --LL－バード・コール

---
+++ Duelists of Whirlwind/script/c100425038.lua
@@ -0,0 +1,39 @@
+--LL－バード・コール
+--Lyrilusc - Bird Call
+--Scripted by Eerie Code
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_names={id}
+s.listed_series={0x121}
+function s.filter(c)
+	return c:IsSetCard(0x121) and not c:IsCode(id) and (c:IsAbleToHand() or c:IsAbleToGrave())
+end
+function s.spfilter(c,e,tp,tc)
+	return c:IsSetCard(0x121) and not c:IsCode(tc:GetCode()) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+	local tc=g:GetFirst()
+	aux.ToHandOrElse(tc,tp)
+	local sg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND,0,nil,e,tp,tc)
+	if #sg>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.BreakEffect()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sc=sg:Select(tp,1,1,nil):GetFirst()
+		Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
