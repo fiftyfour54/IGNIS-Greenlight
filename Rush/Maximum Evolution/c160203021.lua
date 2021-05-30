963de2af5e611910fa87fc433ff29864f661bade
JSY1728 <34511983+JSY1728@users.noreply.github.com>
2021-05-30 11:14:19 +0900

    Add "Seedclotron Brusseln"
    c160203021.lua = RD/MAX2-JP021
    
    please check.

---
+++ Rush/Maximum Evolution/c160203021.lua
@@ -0,0 +1,43 @@
+--シードクロトロン・ブラッセルン
+--Seedclotron Brusseln
+local s,id=GetID()
+function s.initial_effect(c)
+	--Send to Grave
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOGRAVE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.tgcon)
+	e1:SetTarget(s.tgtg)
+	e1:SetOperation(s.tgop)
+	c:RegisterEffect(e1)
+end
+function s.tgcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsStatus(STATUS_SUMMON_TURN)
+end
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,1) end
+end
+function s.thfilter(c)
+	return c:IsRace(RACE_CYBERSE) and c:IsDefense(0) and c:IsAbleToHand()
+end
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+	--General Effect
+	local c=e:GetHandler()
+	Duel.DiscardDeck(tp,1,REASON_EFFECT)>0
+	local g=Duel.GetOperatedGroup()
+	local ct=g:GetFirst()
+	if ct then
+		--Select Effect (Yes or No)
+		if ct:IsType(TYPE_MONSTER) and ct:IsRace(RACE_CYBERSE) and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+			local ct2=Duel.GetMatchingGroupCount(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+			local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter),tp,LOCATION_GRAVE,0,1,ct2,nil)
+			Duel.HintSelection(g)
+			if #g>0 then
+				Duel.SendtoHand(g,nil,REASON_EFFECT)
+			end
+		end
+	end
+end
