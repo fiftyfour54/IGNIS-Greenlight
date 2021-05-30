23054f5f052f8191ecc6d7b1b2a4fcd962900510
JSY1728 <34511983+JSY1728@users.noreply.github.com>
2021-05-30 11:37:31 +0900

    Update c160203021.lua
    Script Final Fixed

--- Rush/Maximum Evolution/c160203021.lua
+++ Rush/Maximum Evolution/c160203021.lua
@@ -4,7 +4,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Send to Grave
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOGRAVE)
+	e1:SetCategory(CATEGORY_DECKDES+CATEGORY_TOGRAVE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
@@ -18,27 +18,23 @@ function s.tgcon(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,1) end
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,0,nil,tp,1)
 end
 function s.thfilter(c)
 	return c:IsRace(RACE_CYBERSE) and c:IsDefense(0) and c:IsAbleToHand()
 end
 function s.tgop(e,tp,eg,ep,ev,re,r,rp)
-	--General Effect
-	local c=e:GetHandler()
-	Duel.DiscardDeck(tp,1,REASON_EFFECT)>0
+	if Duel.DiscardDeck(tp,1,REASON_EFFECT)==0 then return end
 	local g=Duel.GetOperatedGroup()
-	local ct=g:GetFirst()
-	if ct then
-		--Select Effect (Yes or No)
-		if ct:IsType(TYPE_MONSTER) and ct:IsRace(RACE_CYBERSE) and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-			local ct2=Duel.GetMatchingGroupCount(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-			local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter),tp,LOCATION_GRAVE,0,1,ct2,nil)
-			Duel.HintSelection(g)
-			if #g>0 then
-				Duel.SendtoHand(g,nil,REASON_EFFECT)
-				Duel.ConfirmCards(1-tp,g)
-			end
+	local ct=Duel.GetMatchingGroupCount(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+	if g:IsExists(Card.IsRace,1,nil,RACE_CYBERSE) and ct>0
+		and Duel.IsExistingMatchingCard(aux.NecroValleyFilter(s.thfilter),tp,LOCATION_GRAVE,0,1,nil)
+		and Duel.SelectYesNo(id,0) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter),tp,LOCATION_GRAVE,0,1,ct,nil)
+		if #sg>0 then
+			Duel.SendtoHand(sg,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,sg)
 		end
 	end
 end
