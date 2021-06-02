79d559cec4e1be105bc910538710f8f37261b27c
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-06-02 19:07:32 +0200

    Update c160203025.lua

--- Rush/Maximum Evolution/c160203025.lua
+++ Rush/Maximum Evolution/c160203025.lua
@@ -6,7 +6,7 @@ function s.initial_effect(c)
 	--Send the top 2 cards of deck to GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_DECKDES)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_DECKDES+CATEGORY_RECOVER)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
 	e1:SetCost(s.cost)
@@ -46,6 +46,8 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 			if #g>0 then
 				Duel.SendtoHand(g,nil,REASON_EFFECT)
 				Duel.ConfirmCards(1-tp,g)
+				local rec=g:GetFirst():GetAttack()
+				Duel.Recover(tp,rec,REASON_EFFECT)
 			end
 		end
 	end
