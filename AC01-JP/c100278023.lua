550cb79a5475204162ab94bb16a15e067c24101f
Hatter <sanity.sierota@protonmail.com>
2021-06-03 07:10:55 +0800

    Update c100278023.lua
    removed DAMAGE category

--- AC01-JP/c100278023.lua
+++ AC01-JP/c100278023.lua
@@ -5,7 +5,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	-- Add monster to hand
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_DAMAGE)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCondition(s.condition)
