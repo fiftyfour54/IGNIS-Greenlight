1a35cfbe008e40d8b7b0c3bbb4a6b5e504372247
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2021-06-06 09:07:48 -0400

    Added "Ice Mirror" script

---
+++ AC01-JP/c100278024.lua
@@ -0,0 +1,54 @@
+--アイス・ミラー
+--Ice Mirror
+--Logical Nonsense
+
+--Substitute ID
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special summon 1 level 3 or lower WATER monster from deck
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+	--Check for a level 3 or lower WATER monster
+function s.filter(c,e,tp)
+	return c:IsFaceup() and c:IsLevelBelow(3) and c:IsAttribute(ATTRIBUTE_WATER)
+	and Duel.GetFirstMatchingCard(s.filter2,tp,LOCATION_DECK,0,nil,c:GetCode(),e,tp)
+end
+	--Check if copy of filter can be special summoned
+function s.filter2(c,code,e,tp)
+	return c:IsCode(code) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+	--Activation legality
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil,e,tp)end
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,0))
+	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+	--Special summon 1 level 3 or lower WATER monster from deck
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local tc=g:GetFirst()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sg=Duel.GetFirstMatchingCard(s.filter2,tp,LOCATION_DECK,0,nil,tc:GetCode(),e,tp)
+		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e1:SetAbsoluteRange(tp,1,0)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		sg:RegisterEffect(e1,true)
+	end
+end
