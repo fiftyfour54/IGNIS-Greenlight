35d285fa56c6a9807d9d95f1527b9b15f0a4afb1
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-06-09 21:48:12 +0300

    chimera updates

--- VJUMP/c5370235.lua
+++ VJUMP/c5370235.lua
@@ -22,23 +22,23 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_TO_GRAVE)
 	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-	e2:SetCountLimit(1,id+100)
+	e2:SetCountLimit(1,id+1)
 	e2:SetTarget(s.tgtg)
 	e2:SetOperation(s.tgop)
 	c:RegisterEffect(e2)
 end
-s.listed_names={37630732,id}
+s.listed_names={37630732}
 s.listed_series={0x93,0x4093}
 --Search + Extra Fusion Material
 function s.dcfilter(c)
-	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsDiscardable() and c:IsAbleToGraveAsCost()
-end
-function s.thfilter(c)
-	return c:IsCode(37630732) and c:IsAbleToHand()
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsDiscardable()
 end
 function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.dcfilter,tp,LOCATION_HAND,0,1,nil) end
 	Duel.DiscardHand(tp,s.dcfilter,1,1,REASON_COST+REASON_DISCARD)
+end
+function s.thfilter(c)
+	return c:IsCode(37630732) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
@@ -51,18 +51,19 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 	end
+	local c=e:GetHandler()
 	--Extra Fusion Material
-	local e1=Effect.CreateEffect(e:GetHandler())
+	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_EXTRA_FUSION_MATERIAL)
 	e1:SetTargetRange(LOCATION_GRAVE,0)
+	e1:SetCountLimit(1)
 	e1:SetValue(1)
-	e1:SetCountLimit(1,id+101)
-	e1:SetReset(RESET_PHASE+PHASE_END)
 	e1:SetOperation(Fusion.BanishMaterial)
+	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 	--Material Restriction
-	local e2=Effect.CreateEffect(e:GetHandler())
+	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_CANNOT_BE_FUSION_MATERIAL)
 	e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_IGNORE_IMMUNE)
@@ -73,6 +74,7 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.RegisterEffect(e2,tp)
 end
 function s.fuslimit(e,c)
+	if not c then return false end
 	return c:IsControler(e:GetHandlerPlayer())
 end
 function s.fustg(e,c)
