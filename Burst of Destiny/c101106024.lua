1a9cf199de9906bd428fe303ba490f775e53578f
Neo Yuno <82581124+NeoYuno@users.noreply.github.com>
2021-06-11 17:30:44 +0200

    Fix "Penguin Champion"
    The level reducing part should not reset during the end phase

--- Burst of Destiny/c101106024.lua
+++ Burst of Destiny/c101106024.lua
@@ -72,7 +72,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
         e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
         e1:SetType(EFFECT_TYPE_SINGLE)
         e1:SetCode(EFFECT_UPDATE_LEVEL)
-        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD)
         e1:SetValue(-lv)
         c:RegisterEffect(e1)
     end
