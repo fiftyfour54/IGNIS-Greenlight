a9d168a358606ae27a7a60da21cea06f24f7b37d
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2021-06-16 11:33:08 -0400

    Updated "Puppet Queen"
    ATK boost should last until end of next turn.

--- VJUMP/c100200204.lua
+++ VJUMP/c100200204.lua
@@ -69,7 +69,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 			e1:SetCode(EFFECT_UPDATE_ATTACK)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,2)
 			e1:SetValue(1000)
 			tc:RegisterEffect(e1)
 		end
