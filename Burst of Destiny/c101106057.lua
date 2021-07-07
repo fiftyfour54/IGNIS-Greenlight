ea2b33ee411016fb6f8207122a9c41457ba4871f
Rundas <38040866+Rundas01@users.noreply.github.com>
2021-07-07 21:37:04 +0200

    Update c101106057.lua

--- Burst of Destiny/c101106057.lua
+++ Burst of Destiny/c101106057.lua
@@ -4,7 +4,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--To hand + potential Fusion Summon
-	local fparams={aux.FilterBoolFunction(Card.IsLevelAbove,8),Fusion.InHandMat(Card.IsAbleToRemove),s.fextra,Fusion.BanishMaterial}
+	local fparams={aux.FilterBoolFunction(Card.IsLevelAbove,8),Fusion.InHandMat(Card.IsAbleToRemove),s.fextra,Fusion.BanishMaterial,nil,s.stage2}
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON+CATEGORY_TOHAND)
@@ -15,6 +15,8 @@ function s.initial_effect(c)
 	e1:SetTarget(s.thtg)
 	e1:SetOperation(s.thop(Fusion.SummonEffTG(table.unpack(fparams)),Fusion.SummonEffOP(table.unpack(fparams))))
 	c:RegisterEffect(e1)
+	if not GhostBelleTable then GhostBelleTable={} end
+	table.insert(GhostBelleTable,e1)
 end
 s.listed_names={CARD_ALBAZ}
 s.listed_series={0x160}
@@ -45,5 +47,17 @@ function s.thop(fustg,fusop)
 			Duel.BreakEffect()
 			fusop(e,tp,eg,ep,ev,re,r,rp)
 		end
+	end
+end
+function s.stage2(e,tc,tp,mg,chk)
+	if chk==1 then
+		--Cannot attack directly this turn
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetDescription(3207)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
 	end
 end
