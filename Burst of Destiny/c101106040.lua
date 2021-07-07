ccf1544c7b828baceaf1bd191872eddd57934959
EerieCode <eeriecode@gmail.com>
2021-07-07 18:58:14 +0200

    Small fix

--- Burst of Destiny/c101106040.lua
+++ Burst of Destiny/c101106040.lua
@@ -4,6 +4,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
+	c:AddSetcodesRule(SET_URSARCTIC,SET_DRYTRON)
 	--Special summon condition
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
