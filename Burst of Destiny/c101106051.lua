c9ef72c1a8ba99f8376733ad3d9675ee83cfaede
Hatter <sanity.sierota@protonmail.com>
2021-07-11 17:42:40 +0800

    Update c101106051.lua

--- Burst of Destiny/c101106051.lua
+++ Burst of Destiny/c101106051.lua
@@ -43,7 +43,7 @@ function s.spfilter(c,e,tp)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
-		return (Duel.GetLocationCount(tp,LOCATION_MZONE)>0 or e:GetHandler():IsInMainZone())
+		return (Duel.GetLocationCount(tp,LOCATION_MZONE)>0 or e:GetHandler():IsInMainMZone())
 			and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
