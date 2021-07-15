36ab89c01a6850ebb34500dbe0640140c5ca3fea
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-07-15 13:33:56 +0200

    Update c160006020.lua

--- Rush/Deck Modification Pack - Fierce Thunder Storm!!/c160006020.lua
+++ Rush/Deck Modification Pack - Fierce Thunder Storm!!/c160006020.lua
@@ -16,13 +16,13 @@ function s.posfilter(c)
 	return c:IsPosition(POS_FACEUP_ATTACK) and c:IsRace(RACE_WARRIOR) and c:IsCanChangePosition()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.posfilter,tp,LOCATION_MZONE,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.posfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_POSITION,nil,1,0,0)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	--Effect
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local g=Duel.SelectMatchingCard(tp,s.posfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.posfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 	Duel.HintSelection(g)
 	if #g>0 then
 		Duel.ChangePosition(g,0,0,POS_FACEUP_DEFENSE,POS_FACEUP_DEFENSE)
