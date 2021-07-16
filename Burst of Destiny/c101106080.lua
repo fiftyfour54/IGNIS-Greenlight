7e6270ddb18aab014be9c99ccf1944c831d859da
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-07-16 22:51:07 +0300

    Update c101106080.lua

--- Burst of Destiny/c101106080.lua
+++ Burst of Destiny/c101106080.lua
@@ -8,7 +8,6 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(0,TIMING_SUMMON+TIMING_SPSUMMON)
-	e1:SetTarget(s.target)
 	c:RegisterEffect(e1)
 	--to grave
 	local e2=Effect.CreateEffect(c)
