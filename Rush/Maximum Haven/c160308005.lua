f718f82b043a51fca2c8e1dcd5722cc8673cde10
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-08-05 16:37:03 +0200

    added Psychic Trap Hole

--- Rush/Maximum Haven/c160308005.lua
+++ Rush/Maximum Haven/c160308005.lua
@@ -21,11 +21,10 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 
 function s.filter(c)
-	return c:IsRace(RACE_PSYCHIC) and c:IsFaceup()
+	return c:IsRace(RACE_CYBERSE) and c:IsFaceup()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,0,LOCATION_MZONE,1,nil)
-		and Duel.GetMatchingGroupCount(s.filter,tp,LOCATION_MZONE,0,1,nil)>0
+	if chk==0 then return Duel.GetMatchingGroupCount(s.filter,tp,LOCATION_MZONE,0,1,nil)>0
 	end
 end
 function s.cfilter(c)
@@ -38,17 +37,17 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_GRAVE,0,2,2,nil,tp)
 	Duel.HintSelection(g)
 	if #g>0 and Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST)>0 then
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
-			local tc=Duel.SelectMatchingCard(tp,s.cfilter,tp,0,LOCATION_MZONE,1,1,nil):GetFirst()
-			Duel.HintSelection(Group.FromCards(tc))
-			local atk=Duel.GetMatchingGroupCount(s.filter,tp,LOCATION_MZONE,0,nil)
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_UPDATE_ATTACK)
-			e1:SetProperty(EFFECT_FLAG_COPY_INHERIT)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
-			e1:SetValue(atk*-300)
-			tc:RegisterEffectRush(e1)
-		end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
+		local tc=Duel.SelectMatchingCard(tp,s.cfilter,tp,0,LOCATION_MZONE,1,1,nil):GetFirst()
+		Duel.HintSelection(Group.FromCards(tc))
+		local atk=Duel.GetMatchingGroupCount(s.filter,tp,LOCATION_MZONE,0,nil)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetProperty(EFFECT_FLAG_COPY_INHERIT)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
+		e1:SetValue(atk*-300)
+		tc:RegisterEffectRush(e1)
 	end
+	
 end
