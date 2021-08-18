9379cc76f0f4e910ad51af3edaddf142921365d4
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-08-18 14:26:37 +0200

    Added new Rush Fusion
    - Magical Knight - Sevens Palladion (second effect to do)
    - Supergeardragon Dragearstar F

---
+++ Rush/Extra Transcend Enhancement Pack/c160204015.lua
@@ -0,0 +1,46 @@
+-- Magical Knight - Sevens Palladion
+
+-- Sevens Road Magician + Roadstar Swordsman
+-- Requirement: Send the top card of your Deck to the GY.
+-- Chosen Effect: - Until the end of this turn, this card gains 400 ATK for each Attribute in both players' GYs.
+-- - Return 1 monster from your GY to the Deck. Then, draw 1 card.
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,CARD_SEVENS_ROAD_MAGICIAN,160204021)
+		--ATK increase
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCountLimit(1,0,EFFECT_COUNT_CODE_SINGLE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,1) end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil,TYPE_MONSTER) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--requirement
+	if Duel.DiscardDeck(tp,1,REASON_COST)>0 then
+		--effect
+		if c:IsRelateToEffect(e) and c:IsFaceup() then
+			local att=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_GRAVE,LOCATION_GRAVE,nil,TYPE_MONSTER):GetClassCount(Card.GetAttribute)
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetProperty(EFFECT_FLAG_COPY_INHERIT)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
+			e1:SetValue(att*400)
+			c:RegisterEffect(e1)
+		end
+	end
+end
