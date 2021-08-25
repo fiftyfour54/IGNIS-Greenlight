974fb6a903f552e44c38b5597fceade358166548
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-08-25 16:53:21 +0300

    Pax fix

--- DBGC-JP/script/c100417021.lua
+++ DBGC-JP/script/c100417021.lua
@@ -9,13 +9,14 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+	e1:SetHintTiming(0,TIMING_MAIN_END+TIMINGS_CHECK_MONSTER)
 	e1:SetCondition(function()return Duel.IsMainPhase()end)
 	e1:SetCost(s.thcost)
 	e1:SetTarget(s.thtg)
 	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
 end
+s.listed_names={id}
 s.listed_series={0x270}
 function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.CheckLPCost(tp,800) end
@@ -29,7 +30,7 @@ function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.listfilter(c,sc)
-	return c:IsSetCard(0x270) and c:IsMonster() and aux.IsCodeListed(sc,c:GetCode())
+	return c:IsSetCard(0x270) and c:IsFaceup() and c:IsMonster() and aux.IsCodeListed(sc,c:GetCode())
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
@@ -39,7 +40,7 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.ConfirmCards(1-tp,sc)
 	if sc:IsMonster() and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
 		and sc:IsCanBeSpecialSummoned(e,0,tp,false,false)
-		and Duel.IsExistingMatchingCard(s.listfilter,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,1,nil,sc) 
+		and Duel.IsExistingMatchingCard(s.listfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,nil,sc) 
 		and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
 		Duel.BreakEffect()
 		Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)
