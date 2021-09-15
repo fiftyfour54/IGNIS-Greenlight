b3f3d70a95436c9b4198af77b922aaa4610a26b8
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-09-15 13:03:02 +0200

    added Qixing Long Yuan, the Swordsoul Catastrophe

---
+++ BACH-JP/script/c101107041.lua
@@ -0,0 +1,104 @@
+-- 相剣大邪－七星龍淵
+-- Qixing Long Yuan, the Swordsoul Catastrophe
+local s,id=GetID()
+function s.initial_effect(c)
+	--synchro summon
+	c:EnableReviveLimit()
+	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTunerEx(Card.IsRace,RACE_WYRM),1,99)
+	--Draw
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,{id,1})
+	e1:SetCondition(s.drcon)
+	e1:SetTarget(s.drtg)
+	e1:SetOperation(s.drop)
+	c:RegisterEffect(e1)
+	--Banish monster
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_REMOVE)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(s.rmcon)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+	--Banish Spell/Trap
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_REMOVE)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,{id,3})
+	e3:SetCondition(s.rmcon2)
+	e3:SetTarget(s.rmtg2)
+	e3:SetOperation(s.rmop2)
+	c:RegisterEffect(e3)
+	aux.DoubleSnareValidity(c,LOCATION_MZONE)
+end
+s.listed_series={0x16d}
+
+function s.cfilter(c,e,tp)
+	return c:IsFaceup() and c:IsType(TYPE_SYNCHRO) and c:IsSummonType(SUMMON_TYPE_SYNCHRO)
+		and c:IsRace(RACE_WYRM)
+		and c:IsControler(tp)
+end
+function s.drcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,e:GetHandler(),e,tp)
+end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+end
+--banish monster
+function s.filter(c,tp)
+	return c:IsSummonPlayer(tp) and c:IsAbleToRemove()
+end
+function s.rmcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.filter,1,nil,1-tp) and rp~=tp
+end
+function s.remtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return eg:IsExists(s.filter,1,nil,1-tp) end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,eg,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,1200)
+end
+function s.remop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local tc=eg:FilterSelect(tp,s.filter,1,1,nil,tp)
+	if tc and Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)~=0 then
+		Duel.Damage(1-tp,1200,REASON_EFFECT)
+	end
+end
+--banish spell/trap
+function s.remcon2(e,tp,eg,ep,ev,re,r,rp)
+	return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED)
+		and ep==1-tp and re:IsActiveType(TYPE_SPELL+TYPE_TRAP) 
+end
+function s.remtg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return re:GetHandler():IsAbleToRemove() end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,rg,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,1200)
+end
+function s.remop2(e,tp,eg,ep,ev,re,r,rp)
+	if re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:GetHandler():IsRelateToEffect(re) then
+		local ct=Duel.Remove(eg,POS_FACEUP,REASON_EFFECT)
+		if ct>0 then
+			Duel.Damage(1-tp,1200,REASON_EFFECT)
+		end
+	end
+end
