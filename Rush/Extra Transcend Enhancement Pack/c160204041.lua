1ec5ed42c92387079d073b666d3d7e3bbe30f1a2
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-09-15 13:05:44 +0300

    cannon soldier typo

--- Rush/Extra Transcend Enhancement Pack/c160204041.lua
+++ Rush/Extra Transcend Enhancement Pack/c160204041.lua
@@ -22,7 +22,7 @@ function s.damop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	--requirement
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_MZONE,0,1,1,nil))
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_MZONE,0,1,1,nil)
 	local ct=Duel.SendtoGrave(g,REASON_COST)
 	if ct>0 then
 		--effect
