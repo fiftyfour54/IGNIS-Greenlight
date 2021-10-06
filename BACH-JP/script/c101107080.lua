d00513364cd2315f483ad61c59d4ce62461d0ae0
Dylam De La Torre <dyxel04@gmail.com>
2021-10-06 14:50:13 +0200

    add End Of The Line

---
+++ BACH-JP/script/c101107080.lua
@@ -0,0 +1,33 @@
+--走魔灯
+--End of the Line
+--Scripted by DyXel
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local lp=Duel.GetLP(tp)
+		return lp<=100 and Duel.IsPlayerCanDraw(tp,lp<=10 and 4 or 2)
+	end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(2)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+	if Duel.GetLP(tp)<=10 and Duel.IsPlayerCanDraw(tp,2) then
+		Duel.BreakEffect()
+		Duel.Draw(tp,2,REASON_EFFECT)
+	end
+end
