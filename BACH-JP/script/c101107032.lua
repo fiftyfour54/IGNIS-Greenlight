54727218fc3d06dcbb6f38039087ba2bedb7a5dc
Hatter <sanity.sierota@protonmail.com>
2021-10-14 20:38:40 +0800

    Update c101107032.lua

--- BACH-JP/script/c101107032.lua
+++ BACH-JP/script/c101107032.lua
@@ -3,8 +3,8 @@
 --Scripted by Eerie Code
 local s,id=GetID()
 function s.initial_effect(c)
-	c:EnableCounterPermit(COUNTER_PEACE)
-	c:SetCounterLimit(COUNTER_PEACE,4)
+	c:EnableCounterPermit(0x20a)
+	c:SetCounterLimit(0x20a,4)
 	--battle indestructable
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
@@ -21,20 +21,20 @@ function s.initial_effect(c)
 	e2:SetOperation(s.ctop)
 	c:RegisterEffect(e2)
 end
-s.counter_place_list={COUNTER_PEACE }
+s.counter_place_list={0x20a}
 function s.cttg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	local bc=c:GetBattleTarget()
 	if chk==0 then return bc and c:IsLocation(LOCATION_MZONE) and c:IsRelateToBattle() 
-		and c:IsCanAddCounter(COUNTER_PEACE,1) end
+		and c:IsCanAddCounter(0x20a,1) end
 end
 function s.thfilter(c,is_monster)
 	return c:IsAbleToHand() and (not is_monster or c:IsMonster())
 end
 function s.ctop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and c:AddCounter(COUNTER_PEACE,1) then
-		local ct=c:GetCounter(COUNTER_PEACE)
+	if c:IsRelateToEffect(e) and c:AddCounter(0x20a,1) then
+		local ct=c:GetCounter(0x20a)
 		if ct==1 and Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_DECK,0,1,nil,TYPE_MONSTER) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
 			Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
 			local g=Duel.SelectMatchingCard(tp,Card.IsType,tp,LOCATION_DECK,0,1,1,nil,TYPE_MONSTER)
