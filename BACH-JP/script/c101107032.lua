fb9ddb15d27c07093fe1aacb2169144d89548eb0
EerieCode <eeriecode@gmail.com>
2021-10-14 14:01:47 +0200

    Added new cards
    BACH-JP032 --ピースリア
    SD43-JP020 --失烙印

---
+++ BACH-JP/script/c101107032.lua
@@ -0,0 +1,64 @@
+--ピースリア
+--Peacelia
+--Scripted by Eerie Code
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableCounterPermit(COUNTER_PEACE)
+	c:SetCounterLimit(COUNTER_PEACE,4)
+	--battle indestructable
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--place counter
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_COUNTER+CATEGORY_DRAW+CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_DAMAGE_STEP_END)
+	e2:SetTarget(s.cttg)
+	e2:SetOperation(s.ctop)
+	c:RegisterEffect(e2)
+end
+s.counter_place_list={COUNTER_PEACE }
+function s.cttg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local bc=c:GetBattleTarget()
+	if chk==0 then return bc and c:IsLocation(LOCATION_MZONE) and c:IsRelateToBattle() 
+		and c:IsCanAddCounter(COUNTER_PEACE,1) end
+end
+function s.thfilter(c,is_monster)
+	return c:IsAbleToHand() and (not is_monster or c:IsMonster())
+end
+function s.ctop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:AddCounter(COUNTER_PEACE,1) then
+		local ct=c:GetCounter(COUNTER_PEACE)
+		if ct==1 and Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_DECK,0,1,nil,TYPE_MONSTER) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+			Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
+			local g=Duel.SelectMatchingCard(tp,Card.IsType,tp,LOCATION_DECK,0,1,1,nil,TYPE_MONSTER)
+			local tc=g:GetFirst()
+			if tc then
+				Duel.ShuffleDeck(tp)
+				Duel.MoveSequence(tc,0)
+				Duel.ConfirmDecktop(tp,1)
+			end
+		end
+		if ct==2 and Duel.IsPlayerCanDraw(tp,1) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+			Duel.Draw(tp,1,REASON_EFFECT)
+		end
+		if ct==3 and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,true) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+			local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,true)
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+		if ct==4 and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,false) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+			local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,false)
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	end
+end
