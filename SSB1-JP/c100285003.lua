0559468d18fa176c3a5a1d4266cc5ccce645116a
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-11-12 20:13:00 +0200

    SSB1 updates

--- SSB1-JP/c100285003.lua
+++ SSB1-JP/c100285003.lua
@@ -1,10 +1,9 @@
--- たるひのまやかし－ゆきおんな
+-- 垂氷の魔妖－雪女
 -- Yuki-Onna, the Icicle Mayakashi
-
 local s,id=GetID()
 function s.initial_effect(c)
 	c:SetUniqueOnField(1,0,id)
-	--Link summon procedure
+	--Link Summon procedure
 	c:EnableReviveLimit()
 	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_ZOMBIE),2)
 	--Negate effects
@@ -12,38 +11,44 @@ function s.initial_effect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DISABLE)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
 	e1:SetCountLimit(1,{id,0})
 	e1:SetTarget(s.negtg)
 	e1:SetOperation(s.negop)
 	c:RegisterEffect(e1)
-	--spsummon
+	--Special Summon
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_GRAVE)
+	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
 	e2:SetCountLimit(1,{id,1})
 	e2:SetCost(aux.bfgcost)
 	e2:SetTarget(s.sptg)
 	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
 end
---negate
+s.listed_names={id}
+--Negate
+function s.negfilter(c)
+	return c:IsType(TYPE_EFFECT) and aux.disfilter1(c)
+end
 function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and aux.disfilter1(chkc) and chkc:IsControler(1-tp) end
-	if chk==0 then return Duel.IsExistingTarget(aux.disfilter1,tp,0,LOCATION_MZONE,1,nil) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.negfilter(chkc) and chkc:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.negfilter,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
-	local g=Duel.SelectTarget(tp,aux.disfilter1,tp,0,LOCATION_MZONE,1,1,nil)
+	local g=Duel.SelectTarget(tp,s.negfilter,tp,0,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
 end
 function s.negop(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if tc and aux.disfilter1(tc) and tc:IsRelateToEffect(e) then
+	if tc and tc:IsRelateToEffect(e) and aux.disfilter1(tc) then
+		local c=e:GetHandler()
+		--Negate its effects
+		Duel.NegateRelatedChain(tc,RESET_TURN_SET)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_DISABLE)
@@ -52,23 +57,25 @@ function s.negop(e,tp,eg,ep,ev,re,r,rp,chk)
 		local e2=Effect.CreateEffect(c)
 		e2:SetType(EFFECT_TYPE_SINGLE)
 		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetValue(RESET_TURN_SET)
 		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e2)
 	end
 end
---special summon
+--Special Summon
 function s.spfilter(c,e,tp)
-	return c:IsRace(RACE_ZOMBIE) and c:IsType(TYPE_SYNCHRO) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsFaceup() and c:IsRace(RACE_ZOMBIE) and c:IsType(TYPE_SYNCHRO)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE+LOCATION_REMOVED)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e,tp)
 	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
--- SSB1-JP/c100285004.lua
+++ SSB1-JP/c100285004.lua
@@ -1,72 +1,69 @@
 -- 逢華妖麗譚－魔妖不知火語
 -- Ghost Meets Girl - A Mayakashi and Shiranui's Tale
--- Scripted by Nellag 
+-- Scripted by Nellag
 local s,id=GetID()
-
--- Shiranui + Mayakashi
-s.listed_series={0xd9, 0x121}
-
 function s.initial_effect(c)
 	-- Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCost(s.pspcost)
-	e1:SetOperation(s.pspactivate)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER)
 	e1:SetCountLimit(1,id)
+	e1:SetCost(s.cost)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	-- Graveyard Effect to Return 1 Banished Zombie to GY
+	-- Return 1 of your banished Zombies to the GY
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOGRAVE)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCost(aux.bfgcost)
-	e2:SetOperation(s.tgop)
-	e2:SetTarget(s.tgtarget)
+	e2:SetHintTiming(0,TIMING_END_PHASE)
 	e2:SetCountLimit(1,id)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.tgtg)
+	e2:SetOperation(s.tgop)
 	c:RegisterEffect(e2)
 end
-function s.pspcostfilter(c)
-	return 
-		 (c:IsSetCard(0xd9) or c:IsSetCard(0x121))
-		and (c:IsType(TYPE_SYNCHRO) or c:IsType(TYPE_LINK))
+-- "Shiranui" + "Mayakashi"
+s.listed_series={0xd9,0x121}
+function s.cfilter(c)
+	return (c:IsSetCard(0xd9) or c:IsSetCard(0x121)) and (c:IsType(TYPE_SYNCHRO) or c:IsType(TYPE_LINK))
 end
-function s.pspcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if (chk==0) then return Duel.CheckReleaseGroupCost(tp,s.pspcostfilter,1,false,nil,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TRIBUTE)
-	local g=Duel.SelectReleaseGroupCost(tp,s.pspcostfilter,1,1,false,nil,nil)
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.cfilter,1,false,nil,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local g=Duel.SelectReleaseGroupCost(tp,s.cfilter,1,1,false,nil,nil)
 	Duel.Release(g,REASON_COST)
 end
-function s.pspfilter(e,c,tp)
-	return c:IsLocation(LOCATION_HAND) or c:IsLocation(LOCATION_EXTRA) or c:IsLocation(LOCATION_DECK)
-end
-function s.pspactivate(e,tp,eg,ep,ev,re,r,rp)
-	local c = e:GetHandler();
-	local e0 = Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_SINGLE)
-	e0:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e0:SetCategory(CATEGORY_DISABLE_SUMMON)
-	e0:SetDescription(aux.Stringid(id, 1))
-	e0:SetTargetRange(1,1)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	-- Cannot Special Summon from the hand, Deck, or Extra Deck
+	local e0=Effect.CreateEffect(e:GetHandler())
+	e0:SetDescription(aux.Stringid(id,2))
+	e0:SetType(EFFECT_TYPE_FIELD)
 	e0:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
-	e0:SetReset(RESET_EVENT+RESET_PHASE+PHASE_END)
-	e0:SetTarget(s.pspfilter)
-	Duel.RegisterEffect(e0, tp)
+	e0:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e0:SetTargetRange(1,1)
+	e0:SetTarget(s.splimit)
+	e0:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e0,tp)
 end
-function s.tgfilter(c,e,tp)
-	return c:IsRace(RACE_ZOMBIE)
+function s.splimit(e,c,tp)
+	return c:IsLocation(LOCATION_HAND+LOCATION_DECK+LOCATION_EXTRA)
 end
-function s.tgtarget(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_REMOVED) and s.tgfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_REMOVED) and chkc:IsControler(tp) and chkc:IsFaceup() and chkc:IsRace(RACE_ZOMBIE) end
+	if chk==0 then return Duel.IsExistingTarget(aux.FilterFaceupFunction(Card.IsRace,RACE_ZOMBIE),tp,LOCATION_REMOVED,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g = Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
+	local g = Duel.SelectTarget(tp,aux.FilterFaceupFunction(Card.IsRace,RACE_ZOMBIE),tp,LOCATION_REMOVED,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
 end
 function s.tgop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) then
-		Duel.SendtoGrave(tc, REASON_EFFECT+REASON_RETURN)
+		Duel.SendtoGrave(tc,REASON_EFFECT+REASON_RETURN)
 	end
 end
