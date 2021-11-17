7b858caebbf4f4b7e6d7ad3e29b2c79413dd9a19
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-11-17 14:44:28 +0100

    added new cards
    Onmyou Supreme Sage Douma
    Saikyo Battle Flag Gold Rush
     Onmyou Warrior Karatake

--- Rush/Gold Rush Pack/c160421053.lua
+++ Rush/Gold Rush Pack/c160421053.lua
@@ -4,7 +4,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Increase ATK
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_TODECK)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
