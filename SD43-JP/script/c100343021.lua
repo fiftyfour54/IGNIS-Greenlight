ab63915db50f6492c10450fd913330d773335108
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-11-29 20:09:06 +0200

    Branded Fusion updates

--- SD43-JP/script/c100343021.lua
+++ SD43-JP/script/c100343021.lua
@@ -4,23 +4,26 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	local e1=Fusion.CreateSummonEff(c,s.ffilter,nil,s.fextra,nil,nil,nil,2)
+	local e1=Fusion.CreateSummonEff(c,aux.FilterBoolFunction(aux.IsMaterialListCode,CARD_ALBAZ),nil,s.fextra,nil,nil,nil,2)
 	e1:SetCost(s.cost)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	c:RegisterEffect(e1)
 	if not AshBlossomTable then AshBlossomTable={} end
-	if not GhostBelleTable then GhostBelleTable={} end
 	table.insert(AshBlossomTable,e1)
-	table.insert(GhostBelleTable,e1)
 	Duel.AddCustomActivityCounter(id,ACTIVITY_SPSUMMON,s.counterfilter)
 end
 s.listed_names={CARD_ALBAZ}
+function s.fextra(e,tp,mg)
+	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToGrave),tp,LOCATION_DECK,0,nil)
+end
 function s.counterfilter(c)
-	return c:GetSummonLocation()~=LOCATION_EXTRA or c:IsType(TYPE_FUSION)
+	return not c:IsSummonLocation(LOCATION_EXTRA) or c:IsType(TYPE_FUSION)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0 end
-	local e1=Effect.CreateEffect(e:GetHandler())
+	local c=e:GetHandler()
+	--Cannot Special Summon from the Extra Deck, except Fusion Monsters
+	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH+EFFECT_FLAG_CLIENT_HINT)
@@ -29,18 +32,12 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e1:SetTarget(s.splimit)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
-	--Lizard check
-	aux.addTempLizardCheck(e:GetHandler(),tp,s.lizfilter)
-end
-function s.lizfilter(e,c)
-	return not c:IsOriginalType(TYPE_FUSION)
+	--Clock Lizard check
+	aux.addTempLizardCheck(c,tp,s.lizfilter)
 end
 function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
 	return not c:IsType(TYPE_FUSION) and c:IsLocation(LOCATION_EXTRA)
 end
-function s.ffilter(c)
-	return aux.IsCodeListed(c,CARD_ALBAZ)
-end
-function s.fextra(e,tp,mg)
-	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToGrave),tp,LOCATION_DECK,0,nil)
+function s.lizfilter(e,c)
+	return not c:IsOriginalType(TYPE_FUSION)
 end
