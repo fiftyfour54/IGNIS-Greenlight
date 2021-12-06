ae9ad1d61469afb60fff1f05bde00e6ad917d784
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-12-07 01:18:35 +0200

    "Yakusa-Ikazuchi-no-Kami" updates

--- VJMP Cards/c100200210.lua
+++ VJMP Cards/c100200210.lua
@@ -2,7 +2,7 @@
 --Yakusa-Ikazuchi-no-Kami
 local s,id=GetID()
 function s.initial_effect(c)
-	--special summon
+	--Special Summon procedure
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
@@ -12,31 +12,31 @@ function s.initial_effect(c)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	--return 1 Level/Rank 8 monster to activate effect
+	--Return 1 Level/Rank 8 monster and apply an effect
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW+CATEGORY_ATKCHANGE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetCountLimit(1,id)
 	e2:SetTarget(s.tdtg)
 	e2:SetOperation(s.tdop)
 	c:RegisterEffect(e2)
 end
 function s.spfilter(c,tp)
-	return (c:IsLevel(8) or c:IsRank(8)) and c:IsAbleToRemoveAsCost() and Duel.GetMZoneCount(tp,c)>0 and aux.SpElimFilter(c,true)
+	return (c:IsLevel(8) or c:IsRank(8)) and c:IsAbleToRemoveAsCost() and Duel.GetMZoneCount(tp,c)>0
+		and aux.SpElimFilter(c,true)
 end
 function s.spcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
-	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil,tp)
-	return aux.SelectUnselectGroup(g,e,tp,1,1,nil,0)
+	return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,nil,tp)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
-	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil,tp)
-	local rg=aux.SelectUnselectGroup(g,e,tp,1,1,nil,1,tp,HINTMSG_REMOVE,nil,nil,true)
-	if #rg>0 then
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local rg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,1,true,nil,tp)
+	if rg and #rg>0 then
 		rg:KeepAlive()
 		e:SetLabelObject(rg)
 		return true
@@ -49,34 +49,36 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
 	Duel.Remove(rg,POS_FACEUP,REASON_COST)
 	rg:DeleteGroup()
 end
-function s.tdfilter(c)
-	return (c:IsLevel(8) or c:IsRank(8)) and (c:IsType(TYPE_SYNCHRO) or c:IsType(TYPE_FUSION) or c:IsType(TYPE_RITUAL) or c:IsType(TYPE_XYZ))
+function s.tdfilter(c,tp)
+	return (c:IsLevel(8) or c:IsRank(8)) and (c:IsAbleToDeck() or c:IsAbleToExtra())
+		and ((c:IsType(TYPE_RITUAL|TYPE_FUSION) and Duel.IsPlayerCanDraw(tp,1)) or c:IsType(TYPE_SYNCHRO|TYPE_XYZ))
 end
 function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local c=e:GetHandler()
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and chkc:IsAbleToDeckAsCost() and s.tdfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.tdfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.tdfilter(chkc,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.tdfilter,tp,LOCATION_GRAVE,0,1,nil,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectTarget(tp,s.tdfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-	local tc=g:GetFirst()
-	if (tc:IsType(TYPE_RITUAL) or tc:IsType(TYPE_FUSION)) then
-		Duel.SetOperationInfo(0,CATEGORY_TODECK,tc,1,0,0)
+	local tc=Duel.SelectTarget(tp,s.tdfilter,tp,LOCATION_GRAVE,0,1,1,nil,tp):GetFirst()
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,tc,1,tp,LOCATION_GRAVE)
+	if tc:IsType(TYPE_RITUAL|TYPE_FUSION) then
 		Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
-	else
-		Duel.SetOperationInfo(0,CATEGORY_TODECK,tc,1,0,0)
-		Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,c,1,0,1000)
 	end
 end
 function s.tdop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
+	if not tc:IsRelateToEffect(e) or Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)==0 then return end
+	if tc:IsLocation(LOCATION_DECK) then Duel.ShuffleDeck(tp) end
 	local c=e:GetHandler()
-	if tc and tc:IsRelateToEffect(e) and (tc:IsType(TYPE_RITUAL) or tc:IsType(TYPE_FUSION)) and Duel.SendtoDeck(tc,nil,2,REASON_EFFECT)>0 then
+	if tc:IsType(TYPE_RITUAL|TYPE_FUSION) and tc:IsLocation(LOCATION_DECK+LOCATION_EXTRA) then
+		Duel.BreakEffect()
 		Duel.Draw(tp,1,REASON_EFFECT)
-	elseif tc and tc:IsRelateToEffect(e) and (tc:IsType(TYPE_SYNCHRO) or tc:IsType(TYPE_XYZ)) and Duel.SendtoDeck(tc,nil,2,REASON_EFFECT)>0 then
+	elseif tc:IsType(TYPE_SYNCHRO|TYPE_XYZ) and tc:IsLocation(LOCATION_EXTRA)
+		and c:IsFaceup() and c:IsRelateToEffect(e) then
+		Duel.BreakEffect()
+		--Increase ATK
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
 		e1:SetValue(1000)
 		c:RegisterEffect(e1)
 	end
