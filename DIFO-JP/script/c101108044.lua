33e661219d858fad4668a026378c1184a201d7fc
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-12-10 11:57:47 +0100

    added Heroic Champion - Claiomh Solais

---
+++ DIFO-JP/script/c101108044.lua
@@ -0,0 +1,107 @@
+-- Heroic Champion - Claiomh Solais
+-- Ｈ－Ｃ クレイヴソリッシュ
+local s,id=GetID()
+function s.initial_effect(c)
+	--Must be properly summoned before reviving
+	c:EnableReviveLimit()
+	--Xyz summon procedure
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_WARRIOR),4,2)
+	--Cannot target other monsters for attacks
+	local e3=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(0,LOCATION_MZONE)
+	e1:SetValue(s.tgtg)
+	c:RegisterEffect(e1)
+	--double attacks
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.condition)
+	e2:SetCost(s.cost)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
+	--Increase ATK
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_ATKCHANGE)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,{id,2})
+	e3:SetCondition(s.atkcon)
+	e3:SetCost(aux.dxmcostgen(1,1,nil))
+	e3:SetTarget(s.target)
+	e3:SetOperation(s.atkop)
+	c:RegisterEffect(e3)
+end
+--Cannot target other monsters for attacks
+function s.tgtg(e,c)
+	return c~=e:GetHandler()
+end
+--double atk
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLP(tp)>500 end
+	Duel.PayLPCost(tp,Duel.GetLP(tp)-500)
+end
+function s.doubleop(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PHASE+PHASE_BATTLE)
+	e1:SetCountLimit(1)
+	e1:SetReset(RESET_PHASE+PHASE_BATTLE)
+	e1:SetCondition(s.con)
+	e1:SetOperation(s.op)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.con(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATTACK)
+	local tc=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
+	if tc then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetValue(tc:GetAttack()*2)
+		tc:RegisterEffect(e1)
+	end
+end
+--atk
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	local d=a:GetBattleTarget()
+	if a:IsControler(1-tp) then a,d=d,a end
+	return a and a:IsFaceup() and a:IsRelateToBattle()
+		and d and d:IsFaceup() and d:IsRelateToBattle()
+		and a:GetControler()~=d:GetControler()
+end
+function s.filter(c)
+	return c:IsFaceup() and c:GetAttack()>0
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() and c:IsRelateToEffect(e) and c:IsFaceup() then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(tc:GetAttack())
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+		c:RegisterEffect(e1)
+	end
+end
