c0c98d2ef458b4f40046fe8e8c7911999c38cb91
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-12-10 11:59:34 +0100

    typo fix

--- DIFO-JP/script/c101108044.lua
+++ DIFO-JP/script/c101108044.lua
@@ -7,7 +7,7 @@ function s.initial_effect(c)
 	--Xyz summon procedure
 	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_WARRIOR),4,2)
 	--Cannot target other monsters for attacks
-	local e3=Effect.CreateEffect(c)
+	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
 	e1:SetRange(LOCATION_MZONE)
