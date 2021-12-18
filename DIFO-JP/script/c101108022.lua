f5acb52b6e775192ad308fb486b61588b71fb09f
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-12-19 01:57:40 +0200

    Use "aux.TargetBoolFunction" instead

--- DIFO-JP/script/c101108022.lua
+++ DIFO-JP/script/c101108022.lua
@@ -87,10 +87,7 @@ function s.spop2(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetTargetRange(1,0)
-	e1:SetTarget(s.splimit)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsCode,id))
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
-end
-function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
-	return c:IsCode(id)
 end
