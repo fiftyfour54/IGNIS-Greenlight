c215aa29b2d11e3e3378ac02596a079917be6606
that-hatter <sanity.sierota@outlook.com>
2022-01-08 22:44:09 +0800

    add "Icejade Curse"

---
+++ DIFO-JP/script/c101108056.lua
@@ -0,0 +1,63 @@
+-- 氷水呪縛
+-- Icejade Curse
+-- Scripted by Hatter
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	-- Cannot activate
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(0,LOCATION_MZONE)
+	e2:SetCondition(s.accon)
+	e2:SetValue(s.aclim)
+	c:RegisterEffect(e2)
+	-- Inflict damage
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_DAMAGE)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCode(EVENT_BATTLE_DESTROYED)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCountLimit(1,id)
+	e3:SetTarget(s.damtg)
+	e3:SetOperation(s.damop)
+	c:RegisterEffect(e3)
+end
+s.listed_names={7142724}
+s.listed_series={0x16e}
+function s.accon(e)
+	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,0x16e),0,LOCATION_MZONE,LOCATION_MZONE,1,nil)
+		and (Duel.IsEnvironment(7142724)
+		or Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsCode,7142724),0,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil))
+end
+function s.aclim(e,re,tp)
+	local status=STATUS_SUMMON_TURN+STATUS_FLIP_SUMMON_TURN+STATUS_SPSUMMON_TURN
+	return re:IsActiveType(TYPE_MONSTER) and re:GetHandler():IsStatus(status)
+end
+function s.damfilter(c,e,tp)
+	local rc=c:GetReasonCard()
+	return c:IsReason(REASON_BATTLE) and c:IsCanBeEffectTarget(e) and c:GetBaseAttack()>0
+		and ((c:IsPreviousControler(tp) and c:IsSetCard(0x16e))
+		or (rc and rc:IsPreviousControler(tp) and rc:IsSetCard(0x16e)))
+end
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return eg:IsExists(s.damfilter,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,1))
+	local tg=eg:FilterSelect(tp,s.damfilter,1,1,nil,e,tp)
+	Duel.SetTargetCard(tg)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,tg:GetFirst():GetBaseAttack())
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and tc:GetBaseAttack()>0 then
+		Duel.Damage(1-tp,tc:GetBaseAttack(),REASON_EFFECT)
+	end
+end
