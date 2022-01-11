49dd9563824f6a4148afb4217186ead96a058d7f
that-hatter <sanity.sierota@outlook.com>
2022-01-11 15:08:24 +0800

    add "Bridge of Salvation"

---
+++ HC01-JP/c100287010.lua
@@ -0,0 +1,64 @@
+-- 救いの架け橋
+-- Bridge of Salvation
+-- Scripted by Hatter
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_DUEL)
+	e1:SetCondition(s.tdcon)
+	e1:SetTarget(s.tdtg)
+	e1:SetOperation(s.tdop)
+	c:RegisterEffect(e1)
+	-- Search 1 "Crystal Beast" or Field Spell
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,{id,1},EFFECT_COUNT_CODE_DUEL)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0x1034}
+function s.tdcon(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(aux.FilterFaceupFunction(Card.IsLevelAbove,10),tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	return #g>1 and g:GetClassCount(Card.GetCode)>1
+end
+function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local loc=LOCATION_HAND+LOCATION_ONFIELD+LOCATION_GRAVE
+	local g=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,loc,loc,nil,e:GetHandler())
+	if chk==0 then return #g>0 end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,PLAYER_ALL,5)
+end
+function s.tdop(e,tp,eg,ep,ev,re,r,rp)
+	local loc=LOCATION_HAND+LOCATION_ONFIELD+LOCATION_GRAVE
+	local g=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,loc,loc,nil,e:GetHandler())
+	if #g>0 and Duel.SendtoDeck(g,nil,0,REASON_EFFECT)>0 then
+		Duel.BreakEffect()
+		Duel.Draw(tp,5,REASON_EFFECT)
+		Duel.Draw(1-tp,5,REASON_EFFECT)
+	end
+end
+function s.thfilter(c)
+	return (c:IsType(TYPE_FIELD) or (c:IsMonster() and c:IsSetCard(0x1034))) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
