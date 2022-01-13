870395eb07df7fa36a4708bf44430b679ad1c0ca
Hatter <sanity.sierota@protonmail.com>
2022-01-13 18:45:12 +0800

    Update c101108068.lua
    missing ends

--- DIFO-JP/script/c101108068.lua
+++ DIFO-JP/script/c101108068.lua
@@ -23,8 +23,8 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 		and (cl<3 or Duel.IsPlayerCanDiscardDeck(tp,1))
 		and (cl<4 or Duel.IsPlayerCanDraw(tp,1))
 	end
-	if cl>=3 then Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
-	if cl>=4 then Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+	if cl>=3 then Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK) end
+	if cl>=4 then Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1) end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local cl=Duel.GetCurrentChain()
