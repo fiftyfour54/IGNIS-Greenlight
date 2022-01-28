4c056f535dbb0fdab32ad5502155cb3c94ddc098
Hatter <sanity.sierota@outlook.com>
2022-01-29 07:36:44 +0800

    add "Libromancer Firestarter"

---
+++ BACH-EN/script/c101107088.lua
@@ -0,0 +1,81 @@
+-- 
+-- Libromancer Firestarter
+-- Scripted by Hatter
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	-- Check materials on Ritual Summon
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(EFFECT_MATERIAL_CHECK)
+	e0:SetValue(s.matcheck)
+	c:RegisterEffect(e0)
+	-- Cannot be destroyed by card effects
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(s.matcon)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	-- Cannot be banished by card effects
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCode(EFFECT_CANNOT_REMOVE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(1,1)
+	e2:SetTarget(function(e,c,tp,r) return c==e:GetHandler() and r==REASON_EFFECT end)
+	e2:SetCondition(s.matcon)
+	c:RegisterEffect(e2)
+	-- Gain ATK/DEF
+	local e3a=Effect.CreateEffect(c)
+	e3a:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3a:SetCode(EVENT_CHAINING)
+	e3a:SetRange(LOCATION_MZONE)
+	e3a:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3a:SetOperation(s.regop)
+	c:RegisterEffect(e3a)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_CHAIN_SOLVED)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(s.atkcon)
+	e3:SetOperation(s.atkop)
+	c:RegisterEffect(e3)
+end
+s.listed_series={0x277}
+function s.matcheck(e,c)
+	if c:GetMaterial():IsExists(Card.IsLocation,1,nil,LOCATION_MZONE) then
+		local reset=RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD
+		c:RegisterFlagEffect(id,reset,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(id,2))
+	end
+end
+function s.matcon(e)
+	return e:GetHandler():GetFlagEffect(id)>0
+end
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	if ep==1-tp then
+		e:GetHandler():RegisterFlagEffect(id+100,RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET+RESET_CHAIN,0,1)
+	end
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:GetFlagEffect(id+100)>0 and c:GetAttack()<3000
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,id)
+	local c=e:GetHandler()
+	-- Gain ATK/DEF
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetProperty(EFFECT_FLAG_COPY_INHERIT)
+	e1:SetValue(200)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e2)
+end
