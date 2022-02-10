c2039b279725d718424bf6865f24063d159e919c
Hatter <sanity.sierota@outlook.com>
2022-02-10 15:29:51 +0800

    add "Point of Mysterune" and utility function for "Mysterune" Quick-Play Spells

--- init.lua
+++ init.lua
@@ -1,2 +1,76 @@
-CARD_VALIANTS_SHINRABANSHO = 100427111
-CARD_VALIANTS_KOENIGWISSEN = 100427112
+Effect.CreateMysteruneQPEffect = (function()
+	local function spfilter(c,e,tp)
+		return c:IsSetCard(0x167) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+			and Duel.GetLocationCountFromEx(tp,tp,nil,c,0x60)>0
+	end
+
+	local function target(id,categ,tg1,rmcount)
+		return function(e,tp,eg,ep,ev,re,r,rp,chk)
+			local b1=tg1(e,tp,eg,ep,ev,re,r,rp,0) and Duel.IsPlayerCanRemove(tp)
+				and Duel.GetFieldGroupCount(tp,0,LOCATION_DECK)>=rmcount
+			local b2=Duel.IsExistingMatchingCard(spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp)
+			if chk==0 then return b1 or b2 end
+			local sel=aux.SelectEffect(tp,
+				{b1,aux.Stringid(id,0)},
+				{b2,aux.Stringid(id,1)})
+			e:SetLabel(sel)
+			if sel==1 then
+				e:SetCategory(categ)
+				tg1(e,tp,eg,ep,ev,re,r,rp,0)
+				Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,1-tp,LOCATION_DECK)
+			elseif sel==2 then
+				e:SetCategory(CATEGORY_SPECIAL_SUMMON)
+				Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+			else
+				e:SetCategory(0)
+				e:SetOperation(nil)
+			end
+		end
+	end
+
+	local function operation(op1,rmcount)
+		return function(e,tp,eg,ep,ev,re,r,rp)
+			local sel=e:GetLabel()
+			if sel==1 then
+				if op1(e,tp,eg,ep,ev,re,r,rp) and Duel.IsPlayerCanRemove(tp) then
+					local rg=Duel.GetDecktopGroup(1-tp,rmcount)
+					if #rg<1 then return end
+					Duel.DisableShuffleCheck()
+					Duel.BreakEffect()
+					Duel.Remove(rg,POS_FACEUP,REASON_EFFECT)
+				end
+			elseif sel==2 then
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+				local g=Duel.SelectMatchingCard(tp,spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+				if #g>0 then
+					Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP,0x60)
+				end
+			end
+			-- Skip next Battle Phase
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_FIELD)
+			e1:SetCode(EFFECT_SKIP_BP)
+			e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+			e1:SetTargetRange(1,0)
+			if Duel.IsTurnPlayer(tp) then
+				e1:SetLabel(Duel.GetTurnCount())
+				e1:SetCondition(function(e) return Duel.GetTurnCount()~=e:GetLabel() end)
+				e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,2)
+			else
+				e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,1)
+			end
+			Duel.RegisterEffect(e1,tp)
+		end
+	end
+
+	return function(c,id,categ,tg1,op1,rmcount)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_ACTIVATE)
+		e1:SetCode(EVENT_FREE_CHAIN)
+		e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+		e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+		e1:SetTarget(target(id,categ,tg1,rmcount))
+		e1:SetOperation(operation(op1,rmcount))
+		return e1
+	end
+end)()
