33854b2d34d13a1c258e9e624b831498a1c98a91
Hatter <sanity.sierota@outlook.com>
2022-02-10 16:09:15 +0800

    add "Mysterune of Destruction"

--- DBTM-JP/script/c100427031.lua
+++ DBTM-JP/script/c100427031.lua
@@ -1,5 +1,5 @@
--- 輝く炎の神碑
--- Mysterune of the Brilliant Flame
+-- 破壊の神碑
+-- Mysterune of Destruction
 -- Scripted by Hatter
 local s,id=GetID()
 function s.initial_effect(c)
@@ -9,9 +9,9 @@ function s.initial_effect(c)
 end
 s.listed_series={0x27b}
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return Duel.IsExistingTarget(Card.IsSummonType,tp,0,LOCATION_MZONE,1,nil,SUMMON_TYPE_SPECIAL) end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsType,tp,0,LOCATION_ONFIELD,1,nil,TYPE_SPELL+TYPE_TRAP) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,Card.IsSummonType,tp,0,LOCATION_MZONE,1,1,nil,SUMMON_TYPE_SPECIAL)
+	local g=Duel.SelectTarget(tp,Card.IsType,tp,0,LOCATION_ONFIELD,1,1,nil,TYPE_SPELL+TYPE_TRAP)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
