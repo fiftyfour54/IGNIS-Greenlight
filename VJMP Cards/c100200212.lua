e3e97646f62d1f48bb9e15948b1c06c96b45c676
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-03-18 22:19:29 +0200

    "Cassimolar" + promos updates

--- VJMP Cards/c100200212.lua
+++ VJMP Cards/c100200212.lua
@@ -8,11 +8,12 @@ function s.initial_effect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1,id)
 	e1:SetCondition(s.atkcon)
+	e1:SetTarget(s.atktg)
 	e1:SetOperation(s.atkop)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
@@ -23,20 +24,24 @@ function s.initial_effect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_TO_GRAVE)
 	e2:SetCountLimit(1,{id,1})
-	e2:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_TRIBUTE) end)
+	e2:SetCondition(s.tkcon)
 	e2:SetTarget(s.tktg)
 	e2:SetOperation(s.tkop)
 	c:RegisterEffect(e2)
 end
 s.listed_names={TOKEN_ENGINE}
 function s.atkconfilter(c,ec,tp)
-	return c:IsFaceup() and (ec==c and c:IsRace(RACE_MACHINE)) and c:IsControler(tp)
+	return c:IsFaceup() and (ec==c or c:IsRace(RACE_MACHINE)) and c:IsControler(tp) and c:GetBaseDefense()>0
 end
 function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.atkconfilter,1,nil,e:GetHandler(),tp)
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return eg:IsExists(Card.IsLocation,1,nil,LOCATION_MZONE) end
+	Duel.SetTargetCard(eg)
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local g=eg:Filter(Card.IsRelateToEffect,nil,e)
@@ -60,6 +65,10 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
 		tc:RegisterEffect(e1)
 	end
+end
+function s.tkcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsSummonType(SUMMON_TYPE_TRIBUTE)
 end
 function s.tktg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
