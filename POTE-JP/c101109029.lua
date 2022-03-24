a91fee96951098e38d1efa95face4757c21e15dd
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-03-24 19:00:51 +0200

    Krawler Suma updates

--- POTE-JP/c101109029.lua
+++ POTE-JP/c101109029.lua
@@ -1,5 +1,5 @@
 -- クローラー・ソゥマ
--- Krawler Soma
+-- Krawler Suma
 -- Scripted by Hatter
 local s,id=GetID()
 function s.initial_effect(c)
@@ -25,6 +25,7 @@ function s.initial_effect(c)
 	e2:SetOperation(s.lvop)
 	c:RegisterEffect(e2)
 end
+s.listed_series={0x104}
 function s.postg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and chkc:IsFaceup() and chkc:IsCanTurnSet() end
 	local c=e:GetHandler()
@@ -40,8 +41,8 @@ function s.posop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0
-		and tc:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsLocation(LOCATION_MZONE)
-		and tc:IsControler(tp) and Duel.ChangePosition(tc,POS_FACEDOWN_DEFENSE)>0 then
+		and tc:IsRelateToEffect(e) and tc:IsFaceup()
+		and Duel.ChangePosition(tc,POS_FACEDOWN_DEFENSE)>0 then
 		-- Cannot change its battle position
 		local e1=Effect.CreateEffect(c)
 		e1:SetDescription(3313)
@@ -55,11 +56,10 @@ end
 function s.lvfilter(c,e,tp)
 	return c:HasLevel() and c:IsSetCard(0x104) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_DEFENSE)
 end
-function s.lvrescon(c,mustlv)
+function s.lvrescon(mustlv)
 	return function(sg)
 		local sum=sg:GetSum(Card.GetLevel)
-		return (sum==2 or sum==4) and (not mustlv or mustlv==sum)
-			and c:IsLevelAbove(sum+1) and aux.dncheck(sg)
+		return sum==mustlv and aux.dncheck(sg)
 	end
 end
 function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -67,25 +67,30 @@ function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Duel.GetMatchingGroup(s.lvfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,nil,e,tp)
 	if chk==0 then
 		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-		if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=math.min(1,ft) end
-		return ft>0 and aux.SelectUnselectGroup(g,e,tp,1,ft,s.lvrescon(c),0)
+		if ft==0 or not c:HasLevel() or c:IsLevelBelow(2) then return false end
+		if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
+		if c:IsLevelAbove(3) and aux.SelectUnselectGroup(g,e,tp,1,ft,s.lvrescon(2),0) then return true end
+		if c:IsLevelAbove(5) and aux.SelectUnselectGroup(g,e,tp,1,ft,s.lvrescon(4),0) then return true end
+		return false
 	end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,0,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE)
 end
 function s.lvop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=math.min(1,ft) end
-	if ft<1 or c:IsFacedown() or not c:IsRelateToEffect(e) or c:IsImmuneToEffect(e) then return end
+	if ft==0 or c:IsFacedown() or not c:IsRelateToEffect(e) or c:IsImmuneToEffect(e) or c:IsLevelBelow(2) then return end
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
 	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.lvfilter),tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,nil,e,tp)
 	local lvs={}
-	if aux.SelectUnselectGroup(g,e,tp,1,ft,s.lvrescon(c,2),0) then table.insert(lvs,2) end
-	if aux.SelectUnselectGroup(g,e,tp,1,ft,s.lvrescon(c,4),0) then table.insert(lvs,4) end
+	if c:IsLevelAbove(3) and aux.SelectUnselectGroup(g,e,tp,1,ft,s.lvrescon(2),0) then table.insert(lvs,2) end
+	if c:IsLevelAbove(5) and aux.SelectUnselectGroup(g,e,tp,1,ft,s.lvrescon(4),0) then table.insert(lvs,4) end
 	if #lvs<1 then return end
 	local lv=Duel.AnnounceNumber(tp,table.unpack(lvs))
 	if c:UpdateLevel(-lv)~=-lv then return end
-	local tg=aux.SelectUnselectGroup(g,e,tp,1,ft,s.lvrescon(c,lv),1,tp,HINTMSG_SPSUMMON)
-	if #tg>0 then
-		Duel.SpecialSummon(tg,0,tp,tp,false,false,POS_DEFENSE)
+	local tg=aux.SelectUnselectGroup(g,e,tp,1,ft,s.lvrescon(lv),1,tp,HINTMSG_SPSUMMON,s.lvrescon(lv))
+	if #tg>0 and Duel.SpecialSummon(tg,0,tp,tp,false,false,POS_DEFENSE)>0 then
+		local fdg=Duel.GetOperatedGroup():Match(Card.IsFacedown,nil)
+		if #fdg==0 then return end
+		Duel.ConfirmCards(1-tp,fdg)
 	end
 end
