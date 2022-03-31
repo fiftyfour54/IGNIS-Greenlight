45a4a3f36c66be0f867ee59ebcba257f8d92e2d6
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-04-01 01:52:52 +0300

    Add files via upload

--- POTE-JP/c101109067.lua
+++ POTE-JP/c101109067.lua
@@ -3,7 +3,7 @@
 -- Scripted by Hatter
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Send to deck
+	-- Return to the Extra Deck
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TODECK)
@@ -15,12 +15,12 @@ function s.initial_effect(c)
 	e1:SetOperation(s.tdop)
 	c:RegisterEffect(e1)
 end
-function s.tdfilter(c,e,extype)
-	return c:IsFaceup() and c:IsType(extype) and c:IsAbleToDeck() and c:IsCanBeEffectTarget(e)
-end
 function s.tdcostfilter(c,e,tp)
 	return c:IsAbleToGraveAsCost()
 		and Duel.IsExistingMatchingCard(s.tdfilter,tp,0,LOCATION_MZONE,1,nil,e,c:GetType()&(TYPE_EXTRA|TYPE_PENDULUM))
+end
+function s.tdfilter(c,e,extype)
+	return c:IsFaceup() and c:IsType(extype) and c:IsAbleToDeck() and c:IsCanBeEffectTarget(e)
 end
 function s.tdcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.tdcostfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
@@ -34,14 +34,15 @@ function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c:IsControler(1-tp) and s.tdfilter(chkc,e,extype) end
 	if chk==0 then return true end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	Duel.SelectTarget(tp,s.tdfilter,tp,0,LOCATION_MZONE,1,1,nil,e,extype)
+	local g=Duel.SelectTarget(tp,s.tdfilter,tp,0,LOCATION_MZONE,1,1,nil,e,extype)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
 	if e:IsHasType(EFFECT_TYPE_ACTIVATE) then
 		Duel.SetChainLimit(function(e,ep,tp) return ep==tp or not e:IsActiveType(TYPE_MONSTER) end)
 	end
 end
 function s.tdop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsLocation(LOCATION_MZONE) then
+	if tc:IsRelateToEffect(e) then
 		Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
 	end
 end
