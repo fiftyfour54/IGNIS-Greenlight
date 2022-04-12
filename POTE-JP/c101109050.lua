21d6a7e27bfc04a18eeec6ce76cee868d3ea8533
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-04-12 23:43:47 +0300

    Scareclaw updates/fixes

--- POTE-JP/c101109050.lua
+++ POTE-JP/c101109050.lua
@@ -5,7 +5,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
 	Link.AddProcedure(c,s.matfilter,1,1)
-	-- Search 1 Pendulum Monster
+	--Search 1 "Lifobia, the Wanton and Willful World of Desire"
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
@@ -16,12 +16,13 @@ function s.initial_effect(c)
 	e1:SetTarget(s.thtg)
 	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
-	--special summon
+	--Special Summon itself from the GY
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCountLimit(1,id)
+	e2:SetCountLimit(1,id,EFFECT_COUNT_CODE_DUEL)
 	e2:SetCondition(s.spcon)
 	e2:SetTarget(s.sptg)
 	e2:SetOperation(s.spop)
@@ -30,8 +31,7 @@ end
 s.listed_names={CARD_VISAS_STAFROST,56063182}
 s.listed_series={0x17c}
 function s.matfilter(c,sc,st,tp)
-	return c:IsControler(tp) and c:IsInMainMZone()
-		and (c:IsSetCard(0x17c,sc,st,tp) or c:IsSummonCode(sc,st,tp,CARD_VISAS_STAFROST))
+	return c:IsInMainMZone(tp) and (c:IsSetCard(0x17c,sc,st,tp) or c:IsSummonCode(sc,st,tp,CARD_VISAS_STAFROST))
 end
 function s.thcon(e)
 	local c=e:GetHandler()
@@ -53,12 +53,12 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsCode,CARD_VISAS_STAFROST),tp,LOCATION_MZONE,0,1,nil)
+	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsCode,CARD_VISAS_STAFROST),tp,LOCATION_ONFIELD,0,1,nil)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
--- POTE-JP/c101109059.lua
+++ POTE-JP/c101109059.lua
@@ -6,26 +6,32 @@ function s.initial_effect(c)
 	-- Boost ATK of 1 "Scareclaw" monster or "Visas Stafrost"
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E+TIMING_MAIN_END)
-	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.atkcon)
 	e1:SetTarget(s.atktg)
 	e1:SetOperation(s.atkop)
 	c:RegisterEffect(e1)
 	-- Negate an effect that targets "Scareclaw" monster(s) and/or "Visas Stafrost"
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetType(EFFECT_TYPE_ACTIVATE)
 	e2:SetCategory(CATEGORY_DISABLE)
+	e2:SetType(EFFECT_TYPE_ACTIVATE)
 	e2:SetCode(EVENT_CHAINING)
+	e2:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e2:SetCondition(s.discon)
+	e2:SetTarget(s.distg)
 	e2:SetOperation(s.disop)
 	c:RegisterEffect(e2)
 end
 s.listed_names={CARD_VISAS_STAFROST}
 s.listed_series={0x17c}
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated()
+end
 function s.atkfilter(c,e,tp)
 	return c:IsFaceup() and c:IsCanBeEffectTarget(e) 
 		and (c:IsSetCard(0x17c) or c:IsCode(CARD_VISAS_STAFROST) or c:IsControler(1-tp))
@@ -46,6 +52,7 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local tc,ac=g:GetFirst(),g:GetNext()
 	if tc:IsControler(1-tp) then tc,ac=ac,tc end
 	if tc:IsFaceup() and tc:IsControler(tp) and ac:IsFaceup() and ac:IsControler(1-tp) then
+		--Increase ATK/DEF
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
@@ -58,13 +65,17 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function s.disconfilter(c,tp)
-	return c:IsFaceup() and c:IsMonster() and c:IsOnField() and c:IsControler(tp)
-		and (c:IsSetCard(0x17c) or c:IsCode(CARD_VISAS_STAFROST))
+	return c:IsFaceup() and c:IsOnField() and c:IsControler(tp)
+		and ((c:IsSetCard(0x17c) and c:IsMonster()) or c:IsCode(CARD_VISAS_STAFROST))
 end
 function s.discon(e,tp,eg,ep,ev,re,r,rp)
 	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
 	local tg=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
 	return tg and tg:IsExists(s.disconfilter,1,nil,tp) and Duel.IsChainDisablable(ev)
+end
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return not re:GetHandler():IsStatus(STATUS_DISABLED) end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
 end
 function s.disop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.NegateEffect(ev)
