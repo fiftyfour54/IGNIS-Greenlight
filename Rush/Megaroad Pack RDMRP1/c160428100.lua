7a25d223f487b09ac3be9d0493344d38023b50ec
Naim Santos <naimsantos45@gmail.com>
2022-05-22 09:30:21 -0300

    Fix Pot of Avarice (Rush)

--- Rush/Megaroad Pack RDMRP1/c160428100.lua
+++ Rush/Megaroad Pack RDMRP1/c160428100.lua
@@ -19,10 +19,10 @@ function s.filter(c)
 	return c:IsMonster() and c:IsAbleToDeckOrExtraAsCost()
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,3,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,5,nil) end
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
 	Duel.SetTargetPlayer(tp)
 	Duel.SetTargetParam(2)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
@@ -30,7 +30,7 @@ end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
 	--Requirement
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,0,3,3,nil)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,0,5,5,nil)
 	Duel.HintSelection(g)
 	if Duel.SendtoDeck(g,nil,5,REASON_COST)>0 then
 		Duel.ShuffleDeck(tp)
