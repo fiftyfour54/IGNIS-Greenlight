437f8ca88259fd16d392c668ec478b4fb4704f67
Naim Santos <naimsantos45@gmail.com>
2022-05-22 09:18:24 -0300

    Add Rush scripts

---
+++ Rush/Megaroad Pack RDMRP1/c160428030.lua
@@ -0,0 +1,42 @@
+--邪影ダーク・ルーカー
+--Wicked Shadow Dark Lurker
+--scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--Destroy 1 Spell/Trap, inflict 1000 damage and return itself to the hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE+CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,1) end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_SZONE,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,tp,LOCATION_SZONE)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,1,1-tp,1000)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,tp,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	if Duel.DiscardDeck(tp,1,REASON_COST)<1 then return end
+	--Effect
+	local c=e:GetHandler()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local dg=Duel.SelectMatchingCard(tp,aux.TRUE,tp,0,LOCATION_SZONE,1,1,nil)
+	if #dg>0 then
+		Duel.HintSelection(dg,true)
+		if Duel.Destroy(dg,REASON_EFFECT)>0 then
+			if Duel.Damage(1-tp,1000,REASON_EFFECT)==1000 and c:IsFaceup() anc c:IsRelateToEffect(e) then
+				Duel.BreakEffect()
+				Duel.SendToHand(c,REASON_EFFECT)
+			end
+		end
+	end
+end
