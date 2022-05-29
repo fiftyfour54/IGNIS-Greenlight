263bd3fdb85e3eeefe7ddceae4d487c42c9896d1
Naim Santos <naimsantos45@gmail.com>
2022-05-29 10:03:27 -0300

    RD/KP10-JP019 Dynamo Might

---
+++ Rush/Deck Modification Pack - Requiem of Destruction RDKP10/c160010119.lua
@@ -0,0 +1,11 @@
+--ダイナモ・マイト
+--Dynamo Might
+--scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+
+end
+[REQUIREMENT]
+During the turn you Normal Summoned this card.
+[EFFECT]
+Inflict 300 damage to your opponent.
