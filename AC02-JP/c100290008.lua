8f38e5322216bd4454797f889d2d9f2a9a3710f2
Naim Santos <naimsantos45@gmail.com>
2022-06-03 07:06:16 -0300

    Fix "EN Shuffle"

--- AC02-JP/c100290008.lua
+++ AC02-JP/c100290008.lua
@@ -3,7 +3,7 @@
 --Scripted by Eerie Code
 local s,id=GetID()
 function s.initial_effect(c)
-	--activate
+	--Special Summon 1 "Elemental HERO" or "Neo-Spacian" monster
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TODECK+CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -12,26 +12,26 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	--draw
+	--Shuffle cards then draw 1 card
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetCountLimit(1,{id,1})
-	e2:SetCost(s.bfgcost)
+	e2:SetCost(aux.bfgcost)
 	e2:SetTarget(s.drtg)
 	e2:SetOperation(s.drop)
 	c:RegisterEffect(e2)
 end
-s.listed_names={CARD_NEOS }
+s.listed_names={CARD_NEOS}
 s.listed_series={0x3008,0x1f}
 function s.tdfilter(c,e,tp,ft)
-	return (c:IsSetCard(0x3008) or c:IsSetCard(0x1f)) 
+	return (c:IsSetCard(0x3008) or c:IsSetCard(0x1f))
 		and c:IsAbleToDeck() and (ft>-1 or c:GetSequence()<5)
 		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp,c:GetCode())
 end
 function s.spfilter(c,e,tp,code)
-	return (c:IsSetCard(0x3008) or c:IsSetCard(0x1f)) and not c:IsCode(code) 
+	return (c:IsSetCard(0x3008) or c:IsSetCard(0x1f)) and not c:IsCode(code)
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -72,5 +72,5 @@ function s.drop(e,tp,eg,ep,ev,re,r,rp)
 	local sg=aux.SelectUnselectGroup(g,e,tp,1,2,s.rescon,1,tp,HINTMSG_DISCARD,s.rescon)
 	if #sg>0 and Duel.SendtoDeck(sg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)==#sg then
 		Duel.Draw(tp,1,REASON_EFFECT)
-	end	
+	end
 end
