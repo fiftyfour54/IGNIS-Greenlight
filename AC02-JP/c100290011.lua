72260a37158bc5f3dcd683717b08f3f8956e33a7
Naim Santos <naimsantos45@gmail.com>
2022-06-03 07:55:07 -0300

    Added missing effect

--- AC02-JP/c100290011.lua
+++ AC02-JP/c100290011.lua
@@ -19,8 +19,27 @@ function s.initial_effect(c)
 	e2:SetCondition(s.replacecon)
 	e2:SetOperation(s.replaceop)
 	c:RegisterEffect(e2)
+	--"Advanced Crystal Beast" monster can attack directly
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_DIRECT_ATTACK)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTargetRange(LOCATION_MZONE,0)
+	e3:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x5034))
+	c:RegisterEffect(e3)
+	--Battle damage is halved if attacking directly
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_CHANGE_BATTLE_DAMAGE)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetTargetRange(LOCATION_MZONE,0)
+	e4:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x5034))
+	e4:SetCondition(s.rdmgcond)
+	e4:SetValue(aux.ChangeBattleDamage(1,HALF_DAMAGE))
+	c:RegisterEffect(e4)
 end
 s.listed_names={CARD_ADVANCED_DARK}
+s.listed_series={0x5034} --same as the anime versions for now
 function s.tgcon(e)
 	return not Duel.IsEnvironment(CARD_ADVANCED_DARK)
 end
@@ -39,6 +58,9 @@ function s.replaceop(e,tp,eg,ep,ev,re,r,rp)
 	c:RegisterEffect(e1)
 	Duel.RaiseEvent(c,EVENT_CUSTOM+47408488,e,0,tp,0,0)
 end
---[[
-If "Advanced Dark" is not in the Field Zone, send this card to the GY. Your "Advanced Crystal Beast" monsters can attack directly, but when they do so using this effect, the battle damage inflicted to your opponent is halved. If this face-up card is destroyed in a Monster Zone, you can place it face-up in your Spell & Trap Zone as a Continuous Spell, instead of sending it to the GY.
-]]
+function s.rdmgcond(e)
+	local tp=e:GetHandlerPlayer()
+	local c=Duel.GetAttacker()
+	return Duel.GetAttackTarget()==nil and c:GetEffectCount(EFFECT_DIRECT_ATTACK)<2
+		and Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0 --if there are 0 monsters, it's not attacking directly using this effect
+end
