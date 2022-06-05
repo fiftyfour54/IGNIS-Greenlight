ae4e7c7bf454f85f61e7ccc5d15e776dadf07163
Naim Santos <naimsantos45@gmail.com>
2022-06-05 07:49:52 -0300

    Fix Melo Melo Meeeg☆Uuultra Beam

--- Rush/Deck Modification Pack - Requiem of Destruction RDKP10/c160010046.lua
+++ Rush/Deck Modification Pack - Requiem of Destruction RDKP10/c160010046.lua
@@ -3,6 +3,7 @@
 --scripted by Naim
 local s,id=GetID()
 function s.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
