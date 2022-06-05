30eb07424a2197c2dcb25a82ef5e5a838fab5e26
Naim Santos <naimsantos45@gmail.com>
2022-06-05 09:49:26 -0300

    Update Doodle Beast - Stego

--- AC02-JP/c100290032.lua
+++ AC02-JP/c100290032.lua
@@ -14,7 +14,7 @@ function s.initial_effect(c)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	--Search 1 "Doodle Beast" card then discard 1 card
+	--Search 1 "Doodlebook" card then discard 1 card
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_HANDES)
@@ -29,7 +29,7 @@ function s.initial_effect(c)
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e3)
 end
-s.listed_series={0x1518} --setcode used by the anime version for now
+s.listed_series={0x2282}
 function s.cfilter(c)
 	return c:IsFacedown() or not c:IsRace(RACE_DINOSAUR)
 end
@@ -49,7 +49,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function s.thfilter(c)
-	return c:IsSetCard(0x1518) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToHand()
+	return c:IsSetCard(0x2282) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
