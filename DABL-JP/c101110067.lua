5a1e61b3e7711ed63803d760a909cb85b680f50e
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-07-07 14:30:45 +0300

    Arametir updates/fixes

--- DABL-JP/c101110067.lua
+++ DABL-JP/c101110067.lua
@@ -15,18 +15,19 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetCountLimit(1,id)
+	e2:SetCost(s.tkcost)
 	e2:SetTarget(s.tktg)
 	e2:SetOperation(s.tkop)
 	c:RegisterEffect(e2)
 	--Special Summon 1 "Adventurer Token" if a monster is destroyed by battle or effect
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e3:SetCode(EVENT_DESTROYED)
 	e3:SetRange(LOCATION_SZONE)
-	e3:SetCountLimit(1,{id,id})
+	e3:SetCountLimit(1,{id,1})
 	e3:SetCondition(s.spcon)
 	e3:SetTarget(s.sptg)
 	e3:SetOperation(s.spop)
@@ -34,44 +35,46 @@ function s.initial_effect(c)
 	Duel.AddCustomActivityCounter(id,ACTIVITY_SPSUMMON,s.counterfilter)
 end
 s.listed_names={TOKEN_BRAVE}
+local token_stats={TOKEN_BRAVE,0,TYPES_TOKEN,2000,2000,4,RACE_FAIRY,ATTRIBUTE_EARTH}
 function s.counterfilter(c)
-	return (c:IsCode(TOKEN_BRAVE) or aux.IsCodeListed(c,TOKEN_BRAVE))
+	return c:IsCode(TOKEN_BRAVE) or (aux.IsCodeListed(c,TOKEN_BRAVE) and c:IsMonster())
 end
 function s.tkcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0 end
-	--Cannot Special Summon from the same turn, except "Adventurer Token" and monsters that mention it
+	--Cannot Special Summon, except "Adventurer Token" and monsters that mention it
 	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetDescription(aux.Stringid(id,2))
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_CLIENT_HINT+EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetTargetRange(1,0)
 	e1:SetTarget(s.splimit)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 end
-function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
-	return not (c:IsCode(TOKEN_BRAVE) or aux.IsCodeListed(c,TOKEN_BRAVE))
+function s.splimit(e,c)
+	return not (c:IsCode(TOKEN_BRAVE) or (aux.IsCodeListed(c,TOKEN_BRAVE) and c:IsMonster()))
 end
 function s.tktg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return (Duel.GetLocationCount(tp,LOCATION_MZONE)+Duel.GetLocationCount(1-tp,LOCATION_MZONE))>0
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,TOKEN_BRAVE,0,TYPES_TOKEN,2000,2000,4,RACE_FAIRY,ATTRIBUTE_EARTH)
-		and Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,LOCATION_HAND,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
+	local nm,arch,typ,atk,def,lvl,rc,attr=table.unpack(token_stats)
+	local b1=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsPlayerCanSpecialSummonMonster(tp,nm,arch,typ,atk,def,lvl,rc,attr)
+	local b2=Duel.GetLocationCount(1-tp,LOCATION_MZONE,tp)>0 and Duel.IsPlayerCanSpecialSummonMonster(tp,nm,arch,typ,atk,def,lvl,rc,attr,POS_FACEUP,1-tp)
+	if chk==0 then return (b1 or b2) and Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,LOCATION_HAND,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,tp,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_HAND)
 end
 function s.tkop(e,tp,eg,ep,ev,re,r,rp)
-	if not Duel.IsPlayerCanSpecialSummonMonster(tp,TOKEN_BRAVE,0,TYPES_TOKEN,2000,2000,4,RACE_FAIRY,ATTRIBUTE_EARTH) then return end
-	local b1=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-	local b2=Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0
+	local nm,arch,typ,atk,def,lvl,rc,attr=table.unpack(token_stats)
+	local b1=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsPlayerCanSpecialSummonMonster(tp,nm,arch,typ,atk,def,lvl,rc,attr)
+	local b2=Duel.GetLocationCount(1-tp,LOCATION_MZONE,tp)>0 and Duel.IsPlayerCanSpecialSummonMonster(tp,nm,arch,typ,atk,def,lvl,rc,attr,POS_FACEUP,1-tp)
 	if not (b1 or b2) then return end
 	local op=aux.SelectEffect(tp,
-		{b1,aux.Stringid(id,2)},
-		{b2,aux.Stringid(id,3)})
+		{b1,aux.Stringid(id,3)},
+		{b2,aux.Stringid(id,4)})
 	local player=op==1 and tp or 1-tp
-	local token=Duel.CreateToken(tp,id+100)
-	if Duel.SpecialSummon(token,0,tp,player,false,false,POS_FACEUP)>0 and Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,LOCATION_HAND,0,1,nil) then
+	local token=Duel.CreateToken(tp,TOKEN_BRAVE)
+	if Duel.SpecialSummon(token,0,tp,player,false,false,POS_FACEUP)>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 		local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,LOCATION_HAND,0,1,1,nil)
 		if #g>0 then
@@ -87,20 +90,22 @@ function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.cfilter,1,nil,tp)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return (Duel.GetLocationCount(tp,LOCATION_MZONE)+Duel.GetLocationCount(1-tp,LOCATION_MZONE))>0
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,TOKEN_BRAVE,0,TYPES_TOKEN,2000,2000,4,RACE_FAIRY,ATTRIBUTE_EARTH) end
-	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
+	local nm,arch,typ,atk,def,lvl,rc,attr=table.unpack(token_stats)
+	local b1=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsPlayerCanSpecialSummonMonster(tp,nm,arch,typ,atk,def,lvl,rc,attr)
+	local b2=Duel.GetLocationCount(1-tp,LOCATION_MZONE,tp)>0 and Duel.IsPlayerCanSpecialSummonMonster(tp,nm,arch,typ,atk,def,lvl,rc,attr,POS_FACEUP,1-tp)
+	if chk==0 then return b1 or b2 end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,tp,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	if not Duel.IsPlayerCanSpecialSummonMonster(tp,TOKEN_BRAVE,0,TYPES_TOKEN,2000,2000,4,RACE_FAIRY,ATTRIBUTE_EARTH) then return end
-	local b1=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-	local b2=Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0
+	local nm,arch,typ,atk,def,lvl,rc,attr=table.unpack(token_stats)
+	local b1=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsPlayerCanSpecialSummonMonster(tp,nm,arch,typ,atk,def,lvl,rc,attr)
+	local b2=Duel.GetLocationCount(1-tp,LOCATION_MZONE,tp)>0 and Duel.IsPlayerCanSpecialSummonMonster(tp,nm,arch,typ,atk,def,lvl,rc,attr,POS_FACEUP,1-tp)
 	if not (b1 or b2) then return end
 	local op=aux.SelectEffect(tp,
-		{b1,aux.Stringid(id,2)},
-		{b2,aux.Stringid(id,3)})
+		{b1,aux.Stringid(id,3)},
+		{b2,aux.Stringid(id,4)})
 	local player=op==1 and tp or 1-tp
-	local token=Duel.CreateToken(tp,id+100)
+	local token=Duel.CreateToken(tp,TOKEN_BRAVE)
 	Duel.SpecialSummon(token,0,tp,player,false,false,POS_FACEUP)
 end
