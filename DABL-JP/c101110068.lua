a8473da52f1de437112e5647cc26fda6ae7b0f74
EerieCode <eeriecode@gmail.com>
2022-07-12 10:36:16 +0200

    DABL-JP068 --VV－ソロアクティベート

---
+++ DABL-JP/c101110068.lua
@@ -0,0 +1,59 @@
+--VV－ソロアクティベート
+--Vaylantz Vake - Solo Activation
+--Scripted by Eerie Code
+local s,id=GetID()
+function s.initial_effect(c)
+	--to pzone
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,{id,0})
+	e1:SetTarget(s.pztg)
+	e1:SetOperation(s.pzop)
+	c:RegisterEffect(e1)
+	--move
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(function(e) return Duel.CheckLocation(tp,LOCATION_FZONE,0) end)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.mmvtg)
+	e2:SetOperation(s.mmvop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0x17e}
+function s.pzfilter(c)
+	return c:IsSetCard(0x17e) and c:IsType(TYPE_PENDULUM) and not c:IsForbidden()
+end
+function s.pztg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.pzfilter,tp,LOCATION_DECK,0,1,nil)
+		and (Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1)) end
+end
+function s.pzop(e,tp,eg,ep,ev,re,r,rp)
+	if not (Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1)) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local tc=Duel.SelectMatchingCard(tp,s.pzfilter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
+	if tc then
+		Duel.MoveToField(tc,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+	end
+end
+function s.mmvfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x17e) and c:CheckAdjacent()
+end
+function s.mmvtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.mmvfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.mmvfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,s.mmvfilter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function s.mmvop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then 
+		tc:MoveAdjacent()
+	end
+end
