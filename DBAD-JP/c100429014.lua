dbea718032c61b70ea5b5f133d9dcfaed5d33b95
Hatter <sanity.sierota@protonmail.com>
2022-08-09 21:09:55 +0800

    add Rank 2 "Purery" monsters

---
+++ DBAD-JP/c100429014.lua
@@ -0,0 +1,93 @@
+-- エピュアリィ・ハピネス
+-- Epurery Happiness
+-- Scripted by Hatter
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	-- 2 Level 2 monsters
+	Xyz.AddProcedure(c,nil,2,2)
+	-- Search 1 "Purery" card
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_DAMAGE_STEP_END)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+	-- Attack "Purery" Quick-Play Spell
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_TOHAND)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(3)
+	e3:SetCondition(s.qpovcon)
+	e3:SetTarget(s.qpovtg)
+	e3:SetOperation(s.qpovop)
+	c:RegisterEffect(e3)
+end
+s.listed_names={100429021}
+s.listed_series={0x289}
+function s.thfilter(c)
+	return c:IsSetCard(0x289) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.atkfilter(c)
+	return c:IsFaceup() and c:GetAttack()>0
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g==0 or Duel.SendtoHand(g,nil,REASON_EFFECT)==0 or not g:GetFirst():IsLocation(LOCATION_HAND) then return end
+	Duel.ConfirmCards(1-tp,g)
+	local c=e:GetHandler()
+	if c:GetOverlayGroup():IsExists(Card.IsCode,1,nil,100429021)
+		and Duel.IsExistingMatchingCard(s.atkfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
+		local tc=Duel.SelectMatchingCard(tp,s.atkfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil):GetFirst()
+		if not tc then return end
+		-- Halve ATK
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetValue(tc:GetAttack()//2)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+	end
+end
+function s.qpovcon(e,tp,eg,ep,ev,re,r,rp)
+	if rp~=tp or not re:IsHasType(EFFECT_TYPE_ACTIVATE) then return end
+	local rc=re:GetHandler()
+	return rc:IsSetCard(0x289) and rc:GetType()==TYPE_SPELL+TYPE_QUICKPLAY
+		and rc:IsOnField() and rc:IsCanBeXyzMaterial(e:GetHandler(),tc,REASON_EFFECT)
+end
+function s.qpovtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,1-tp,LOCATION_SZONE)
+end
+function s.qpovop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local rc=re:GetHandler()
+	if c:IsRelateToEffect(e) and rc:IsRelateToEffect(re)
+		and not c:IsImmuneToEffect(e) and not rc:IsImmuneToEffect(e)
+		and rc:IsCanBeXyzMaterial(c,tp,REASON_EFFECT) then
+		Duel.Overlay(c,rc)
+		if not c:GetOverlayGroup():IsContains(rc) then return end
+		rc:CancelToGrave()
+		if Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,0,LOCATION_SZONE,1,nil)
+			and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+			local g=Duel.SelectMatchingCard(tp,Card.IsAbleToHand,tp,0,LOCATION_SZONE,1,1,nil)
+			if #g==0 then return end
+			Duel.BreakEffect()
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+		end
+	end
+end
