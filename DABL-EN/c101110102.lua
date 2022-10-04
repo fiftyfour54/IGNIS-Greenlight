bc8f8de47f41470fb0c6761e2d5dca01a776310c
Naim <naimsantos45@gmail.com>
2022-10-04 05:33:23 -0300

    Rename function

--- DABL-EN/c101110102.lua
+++ DABL-EN/c101110102.lua
@@ -87,7 +87,7 @@ function s.cfilter(c)
 	return c:IsRace(RACE_FISH) and c:IsAbleToRemoveAsCost()
 		and aux.SpElimFilter(c,true)
 end
-function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
 	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,1,nil)
