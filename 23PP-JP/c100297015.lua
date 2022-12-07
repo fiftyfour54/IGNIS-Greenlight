89ac557a6d23465ecf8c559b76f20432223d8563
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-12-07 22:49:01 +0200

    Zero Paradox + Azalea updates/fixes

--- 23PP-JP/c100297015.lua
+++ 23PP-JP/c100297015.lua
@@ -12,21 +12,21 @@ function s.initial_effect(c)
 	e0:SetType(EFFECT_TYPE_SINGLE)
 	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e0:SetValue(0)
 	c:RegisterEffect(e0)
-	--Target 1 opponent's Pendulum card to Special Summon and place in your Pendulum Zone
+	--Special Summon this card and place an opponent's Pendulum Scale in your Pendulum Zone
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_CONTROL)
 	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_PZONE)
 	e1:SetCountLimit(1,id)
 	e1:SetTarget(s.pentg)
 	e1:SetOperation(s.penop)
 	c:RegisterEffect(e1)
-	--Special Summon and Destroy
+	--Special Summon this card and destroy all other cards on the field
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DESTROY)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
@@ -35,102 +35,99 @@ function s.initial_effect(c)
 	e2:SetTarget(s.sptg)
 	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
-	--ATK becomes 6000 if another "D/D/D" leaves field by Spell
+	--This card's ATK becomes 6000
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetDescription(aux.Stringid(id,2))
 	e3:SetCategory(CATEGORY_ATKCHANGE)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e3:SetCode(EVENT_LEAVE_FIELD)
 	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
 	e3:SetCondition(s.atkcon)
-	e3:SetTarget(s.atktg)
 	e3:SetOperation(s.atkop)
 	c:RegisterEffect(e3)
-	
 end
+s.listed_series={SET_DDD}
 function s.pentg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_PZONE) and chkc:IsControler(1-tp) end
-	if chk==0 then return Duel.CheckPendulumZones(tp)
-		and Duel.IsExistingTarget(Card.IsType,tp,LOCATION_PZONE,0,1,nil,TYPE_PENDULUM) end
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,0))
-	local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_PZONE,1,1,nil)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
+		and Duel.IsExistingTarget(Card.IsAbleToChangeControler,tp,0,LOCATION_PZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToChangeControler,tp,0,LOCATION_PZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_CONTROL,g,1,tp,0)
 end
 function s.penop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if not Duel.CheckPendulumZones(tp) then return end
+	if not c:IsRelateToEffect(e) or Duel.SpecialSummon(c,0,tp,tp,true,true,POS_FACEUP)==0 then return end
+	c:CompleteProcedure()
 	local tc=Duel.GetFirstTarget()
-	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
-		if tc:IsFaceup() and tc:IsRelateToEffect(e) and Duel.MoveToField(tc,tp,tp,LOCATION_PZONE,POS_FACEUP,true)>0 then
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-			e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-			e1:SetCode(EVENT_PHASE+PHASE_END)
-			e1:SetCondition(s.descon)
-			e1:SetOperation(s.desop)
-			e1:SetReset(RESET_PHASE+PHASE_END,2)
-			e1:SetCountLimit(1)
-			e1:SetLabel(Duel.GetTurnCount())
-			e1:SetLabelObject(tc)
-			Duel.RegisterEffect(e1,tp)
-		end
+	if not (tc:IsRelateToEffect(e) and Duel.CheckPendulumZones(tp)) then return end
+	if Duel.MoveToField(tc,tp,tp,LOCATION_PZONE,POS_FACEUP,true) then
+		tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
+		--Destroy it during the End Phase of the next turn
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e1:SetCode(EVENT_PHASE+PHASE_END)
+		e1:SetCountLimit(1)
+		e1:SetLabel(Duel.GetTurnCount())
+		e1:SetLabelObject(tc)
+		e1:SetCondition(function(e) return Duel.GetTurnCount()==e:GetLabel()+1 and e:GetLabelObject():GetFlagEffect(id)>0 end)
+		e1:SetOperation(function(e) Duel.Destroy(e:GetLabelObject(),REASON_EFFECT) end)
+		e1:SetReset(RESET_PHASE+PHASE_END,2)
+		Duel.RegisterEffect(e1,tp)
 	end
 end
-function s.descon(e,tp,eg,ep,ev,re,r,rp)
-	local tc=e:GetLabelObject()
-	return Duel.GetTurnCount()~=e:GetLabel() and tc:GetFlagEffect(id)~=0
-end
-function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=e:GetLabelObject()
-	Duel.Destroy(tc,REASON_EFFECT)
-end
-function s.filter(c,tp)
-	return c:IsMonster() and c:IsSummonPlayer(tp) and c:GetSummonType()&SUMMON_TYPE_PENDULUM~=0
+function s.spcfilter(c,tp)
+	return c:IsSummonType(SUMMON_TYPE_PENDULUM) and c:HasLevel() and c:IsSummonPlayer(tp) and c:IsFaceup()
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	local pg=Duel.GetFieldGroup(tp,LOCATION_PZONE,0):GetSum(Card.GetScale)
-	local sg=eg:Filter(s.filter,nil,tp):GetSum(Card.GetLevel)
-	return eg:IsExists(s.filter,1,nil,tp) and pg>sg
+	local sg=eg:Filter(s.spcfilter,nil,tp)
+	if #sg==0 then return false end
+	local pg=Duel.GetFieldGroup(tp,LOCATION_PZONE,0)
+	if #pg==0 then return false end
+	local total_scales=pg:GetSum(Card.GetScale)
+	local total_levels=sg:GetSum(Card.GetLevel)
+	return total_scales>total_levels
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	local g=Duel.GetFieldGroup(0,LOCATION_ONFIELD,LOCATION_ONFIELD)
+	if chk==0 then return #g>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and c:IsCanBeSpecialSummoned(e,0,tp,true,true) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,PLAYER_ALL,LOCATION_ONFIELD)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,PLAYER_ALL,LOCATION_ONFIELD)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) then return end
-	if Duel.SpecialSummon(c,0,tp,tp,true,false,POS_FACEUP)~=0 then
-		c:CompleteProcedure()
-		local sg=Duel.GetMatchingGroup(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
-		Duel.Destroy(sg,REASON_EFFECT)
+	if not c:IsRelateToEffect(e) or Duel.SpecialSummon(c,0,tp,tp,true,true,POS_FACEUP)==0 then return end
+	c:CompleteProcedure()
+	local g=Duel.GetMatchingGroup(nil,0,LOCATION_ONFIELD,LOCATION_ONFIELD,c)
+	if #g>0 and Duel.Destroy(g,REASON_EFFECT)>0 and Duel.CheckPendulumZones(tp)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
 		Duel.BreakEffect()
-		if Duel.SelectYesNo(tp,aux.Stringid(id,0)) and Duel.CheckPendulumZones(tp) then
-			Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
-		end
+		Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
 	end
 end
-function s.lvfilter(c,tp,rp,re)
-	return c:IsSetCard(0x10af) and c:GetPreviousControler()==tp and c:IsReason(REASON_EFFECT) and re:IsActiveType(TYPE_SPELL)
+function s.atkcfilter(c,tp)
+	return c:IsPreviousPosition(POS_FACEUP) and c:IsPreviousSetCard(SET_DDD) and c:IsPreviousControler(tp)
+		and c:IsReason(REASON_EFFECT) and c:GetReasonEffect():IsSpellEffect()
 end
 function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	return eg:IsExists(s.lvfilter,1,c,tp,rp,re)
-end
-function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,c,1,0,0)
+	return eg:IsExists(s.atkcfilter,1,nil,tp)
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsFaceup() and c:IsRelateToEffect(e) then
-		--Cannot attack this turn
+		--Its ATK becomes 6000
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		e1:SetValue(6000)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
 		c:RegisterEffect(e1)
 	end
 end
--- VJUMP/c100200228.lua
+++ VJUMP/c100200228.lua
@@ -1,64 +1,64 @@
 --閃刀姫－アザレア
---Sky Striker Ace - Azaela
+--Sky Striker Ace - Azalea
 --Scripted by The Razgriz
 local s,id=GetID()
 function s.initial_effect(c)
-	--link summon
-	Link.AddProcedure(c,s.matfilter,2,2)
 	c:EnableReviveLimit()
 	c:SetSPSummonOnce(id)
-	--must link summon
+	--Link Summon procedure
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_LIGHT|ATTRIBUTE_DARK),2,2)
+	--Must be Link Summoned
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
 	e1:SetValue(aux.lnklimit)
 	c:RegisterEffect(e1)
-	--Destroy 1 card/Send to GY if Spells in GY are less than 4
+	--Destroy 1 card on the field
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetCategory(CATEGORY_DESTROY+CATEGORY_TOGRAVE)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetTarget(s.destg)
 	e2:SetOperation(s.desop)
 	c:RegisterEffect(e2)
-	--Banish Spell to destroy monster at start of Damage Step
+	--Destroy an opponent's monster that battles this card
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_DESTROY)
-	e3:SetCode(EVENT_BATTLE_START)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_BATTLE_START)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetCountLimit(1)
+	e3:SetCondition(s.btcon)
 	e3:SetCost(s.btcost)
 	e3:SetTarget(s.bttg)
 	e3:SetOperation(s.btop)
 	c:RegisterEffect(e3)
 end
 s.listed_names={id}
-function s.matfilter(c)
-	return (c:IsAttribute(ATTRIBUTE_LIGHT) or c:IsAttribute(ATTRIBUTE_DARK)) and c:IsCanBeLinkMaterial()
-end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) end
-	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	if chkc then return chkc:IsOnField() end
+	if chk==0 then return Duel.IsExistingTarget(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_MZONE,1,1,nil)
-	local tc=g:GetFirst()
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,tc,1,0,0)
-	Duel.SetPossibleOperationInfo(0,CATEGORY_TOGRAVE,c,1,0,0)
+	local g=Duel.SelectTarget(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOGRAVE,e:GetHandler(),1,0,0)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
-		Duel.Destroy(tc,REASON_EFFECT)
-		if Duel.GetFieldGroup(tp,LOCATION_GRAVE,0):FilterCount(Card.IsSpell,nil)<4 then
-			Duel.SendtoGrave(c,REASON_EFFECT)
-		end
+	if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)>0 and c:IsRelateToEffect(e)
+		and Duel.GetMatchingGroupCount(Card.IsSpell,tp,LOCATION_GRAVE,0,nil)<=3 then
+		Duel.BreakEffect()
+		Duel.SendtoGrave(c,REASON_EFFECT)
 	end
+end
+function s.btcon(e,tp,eg,ep,ev,re,r,rp)
+	local bc=e:GetHandler():GetBattleTarget()
+	return bc and bc:IsControler(1-tp)
 end
 function s.cfilter(c)
 	return c:IsSpell() and c:IsAbleToRemoveAsCost()
@@ -70,17 +70,12 @@ function s.btcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
 function s.bttg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	local tc=Duel.GetAttacker()
-	if tc==c then tc=Duel.GetAttackTarget() end
-	if chk==0 then return tc and tc:IsControler(1-tp) end
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,tc,1,0,0)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler():GetBattleTarget(),1,0,0)
 end
 function s.btop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=Duel.GetAttacker()
-	if tc==c then tc=Duel.GetAttackTarget() end
-	if tc:IsRelateToBattle() and tc:IsControler(1-tp) then
-		Duel.Destroy(tc,REASON_EFFECT)
+	local bc=e:GetHandler():GetBattleTarget()
+	if bc:IsRelateToBattle() and bc:IsControler(1-tp) then
+		Duel.Destroy(bc,REASON_EFFECT)
 	end
 end
