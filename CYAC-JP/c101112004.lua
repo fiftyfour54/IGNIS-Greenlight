b4835b6d6154b4ea74d67ecdc3f0f8c9f92f0620
Hatter <sanity.sierota@protonmail.com>
2022-12-09 14:34:32 +0800

    fix indentation

--- CYAC-JP/c101112004.lua
+++ CYAC-JP/c101112004.lua
@@ -3,8 +3,8 @@
 -- Scripted by Satella
 local s,id=GetID()
 function s.initial_effect(c)
-        -- Special Summon from your hand
-        local e1=Effect.CreateEffect(c)
+	-- Special Summon from your hand
+	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
