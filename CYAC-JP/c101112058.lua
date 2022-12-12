be5db521315bdb0ab02cd7a4d01e480db4716cd5
Naim <naimsantos45@gmail.com>
2022-12-12 11:00:11 -0300

    Rescript "Primal Planet Kalarium" partially

--- CYAC-JP/c101112058.lua
+++ CYAC-JP/c101112058.lua
@@ -23,18 +23,29 @@ function s.initial_effect(c)
 	e2:SetValue(s.atkvalue)
 	c:RegisterEffect(e2)
 	--Special Summon 1 destroyed monster
-	local e3=Effect.CreateEffect(c)
-	e3:SetCategory(aux.Stringid(id,1))
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e3:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
-	e3:SetCode(EVENT_LEAVE_FIELD)
-	e3:SetRange(LOCATION_FZONE)
-	e3:SetCountLimit(1,id)
-	e3:SetCondition(s.spcond)
-	e3:SetTarget(s.sptg)
-	e3:SetOperation(s.spop)
-	c:RegisterEffect(e3)
+	local e3a=Effect.CreateEffect(c)
+	e3a:SetDescription(aux.Stringid(id,1))
+	e3a:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3a:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3a:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e3a:SetCode(EVENT_CUSTOM+id)
+	e3a:SetRange(LOCATION_FZONE)
+	e3a:SetCountLimit(1,id)
+	e3a:SetTarget(s.sptg)
+	e3a:SetOperation(s.spop)
+	c:RegisterEffect(e3a)
+	local g=Group.CreateGroup()
+	g:KeepAlive()
+	e3a:SetLabelObject(g)
+	--Register the destuction of monsters
+	local e3b=Effect.CreateEffect(c)
+	e3b:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3b:SetCode(EVENT_DESTROYED)
+	e3b:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3b:SetRange(LOCATION_FZONE)
+	e3b:SetLabelObject(e3a)
+	e3b:SetOperation(s.regop)
+	c:RegisterEffect(e3b)
 end
 s.listed_names={CARD_VISAS_STARFROST}
 s.listed_series={SET_MANADOME}
@@ -54,27 +65,48 @@ function s.atkvalue(e)
 	local tp=e:GetHandlerPlayer()
 	return Duel.GetMatchingGroupCount(aux.FaceupFilter(Card.IsType,TYPE_TUNER),tp,LOCATION_MZONE|LOCATION_GRAVE,0,nil)*100
 end
-function s.cfilter(c,tp,e)
-	return c:IsPreviousPosition(POS_FACEUP) and c:IsLocation(LOCATION_GRAVE|LOCATION_REMOVED) and c:IsFaceup()
+function s.tgfilter(c,tp,e)
+	return c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsPreviousPosition(POS_FACEUP)
+		and c:IsLocation(LOCATION_GRAVE|LOCATION_REMOVED) and c:IsFaceup()
 		and (c:IsType(TYPE_TUNER) or (c:GetPreviousTypeOnField()&TYPE_TUNER)>0)
-		and c:IsPreviousControler(tp) and c:IsReason(REASON_DESTROY) and c:IsReason(REASON_BATTLE+REASON_EFFECT)
+		and c:IsPreviousControler(tp) and c:IsReason(REASON_BATTLE+REASON_EFFECT)
 		and c:IsCanBeEffectTarget(e)
-end
-function s.spcond(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil,tp,e)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chkc then return eg:IsContains(chkc) and s.cfilter(chkc,tp,e) end
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and eg:IsExists(s.cfilter,1,nil,tp,e) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local tg=eg:FilterSelect(tp,s.cfilter,1,1,nil,tp,e)
-	Duel.SetTargetCard(tg)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,tg,#tg,tp,0)
+	local g=e:GetLabelObject():Filter(s.tgfilter,nil,tp,e)
+	if chkc then return g:IsContains(chkc) and s.tgfilter(chkc,tp,e) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and #g>0 end
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+	local tc=nil
+	if #g==1 then
+		tc=g:GetFirst()
+		Duel.SetTargetCard(tc)
+	else
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+		tc=g:Select(tp,1,1,nil)
+		Duel.SetTargetCard(tc)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,tc,1,tp,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFlagEffect(tp,id)>0 then return end
+	local tg=eg:Filter(s.tgfilter,nil,tp,e)
+	if #tg>0 then
+		for tc in tg:Iter() do
+			tc:RegisterFlagEffect(id,RESET_CHAIN,0,1)
+		end
+		local g=e:GetLabelObject():GetLabelObject()
+		if Duel.GetCurrentChain()==0 then g:Clear() end
+		g:Merge(tg)
+		g:Remove(function(c) return c:GetFlagEffect(id)==0 end,nil)
+		e:GetLabelObject():SetLabelObject(g)
+		Duel.RaiseSingleEvent(e:GetHandler(),EVENT_CUSTOM+id,e,0,tp,tp,0)
 	end
 end
