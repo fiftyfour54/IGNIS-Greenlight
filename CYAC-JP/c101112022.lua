2b1c05ae99cc6268f147f95d167ff699aec574c8
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-12-21 23:32:23 +0200

    Infinitrack + Libromancer updates/fixes

--- CYAC-JP/c101112022.lua
+++ CYAC-JP/c101112022.lua
@@ -3,29 +3,30 @@
 --scripted by Naim
 local s,id=GetID()
 function s.initial_effect(c)
-	--Special Summon itself if an EARTH Machine monster is tributed
+	--Special Summon itself if an EARTH Machine monster is Tributed
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_RELEASE)
 	e1:SetProperty(EFFECT_FLAG_DELAY,EFFECT_FLAG2_CHECK_SIMULTANEOUS)
-	e1:SetRange(LOCATION_HAND+LOCATION_GRAVE)
+	e1:SetCode(EVENT_RELEASE)
+	e1:SetRange(LOCATION_HAND|LOCATION_GRAVE)
+	e1:SetLabel(0)
 	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.spcond1)
+	e1:SetCondition(s.spcon)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 	--Special Summon itself if an EARTH Machine monster is banished face-up
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_REMOVE)
-	e2:SetCondition(s.spcond2)
+	e2:SetLabel(1)
 	c:RegisterEffect(e2)
 	-- If attached to a Machine Xyz monster, monsters your opponent controls are changed to Defense Position
 	local e3=Effect.CreateEffect(c)
-	e3:SetCode(EFFECT_SET_POSITION)
 	e3:SetType(EFFECT_TYPE_XMATERIAL+EFFECT_TYPE_FIELD)
 	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetCode(EFFECT_SET_POSITION)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetTargetRange(0,LOCATION_MZONE)
 	e3:SetCondition(s.poscond)
@@ -34,20 +35,25 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 	-- If attached to a Machine Xyz monster, monsters your opponent controls lose 1000 DEF
 	local e4=Effect.CreateEffect(c)
-	e4:SetCode(EFFECT_UPDATE_DEFENSE)
 	e4:SetType(EFFECT_TYPE_XMATERIAL+EFFECT_TYPE_FIELD)
 	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e4:SetCode(EFFECT_UPDATE_DEFENSE)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetTargetRange(0,LOCATION_MZONE)
 	e4:SetCondition(s.poscond)
 	e4:SetValue(-1000)
 	c:RegisterEffect(e4)
 end
-function s.cfilter(c)
-	return c:GetPreviousAttributeOnField()&ATTRIBUTE_EARTH>0 and c:GetPreviousRaceOnField()&RACE_MACHINE>0
+function s.cfilter(c,label)
+	if label==1 and c:IsFacedown() then return false end
+	if c:IsPreviousLocation(LOCATION_MZONE) then
+		return c:GetPreviousAttributeOnField()&ATTRIBUTE_EARTH>0 and c:GetPreviousRaceOnField()&RACE_MACHINE>0
+	else
+		return c:IsMonster() and c:IsOriginalAttribute(ATTRIBUTE_EARTH) and c:IsOriginalRace(RACE_MACHINE)
+	end
 end
-function s.spcond1(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil)
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil,e:GetLabel())
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
@@ -58,21 +64,18 @@ end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
-		--Banish it if it leaves the field
+		--Banish it when it leaves the field
 		local e1=Effect.CreateEffect(c)
 		e1:SetDescription(3300)
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
-		e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
+		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
 		e1:SetValue(LOCATION_REMOVED)
+		e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
 		c:RegisterEffect(e1,true)
 	end
 end
-function s.spcond2(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(aux.FaceupFilter(s.cfilter),1,nil)
-end
 function s.poscond(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:GetOriginalRace()==RACE_MACHINE and c:IsType(TYPE_XYZ)
+	return c:IsOriginalRace(RACE_MACHINE) and c:IsType(TYPE_XYZ)
 end
--- CYAC-JP/c101112063.lua
+++ CYAC-JP/c101112063.lua
@@ -5,29 +5,30 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetOperation(s.setop)
 	c:RegisterEffect(e1)
-	--"Libromancer" monsters gain ATK
+	--"Libromancer" Ritual Monsters gain ATK
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_UPDATE_ATTACK)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetTargetRange(LOCATION_MZONE,0)
-	e2:SetTarget(function(_,c) return c:IsSetCard(SET_LIBROMANCER) and c:HasLevel() end)
+	e2:SetTarget(function(_,c) return c:IsSetCard(SET_LIBROMANCER) and c:IsRitualMonster() end)
 	e2:SetValue(function(_,c) return c:GetLevel()*100 end)
 	c:RegisterEffect(e2)
-	--Destroy 1 Spell/Trap Card
+	--Destroy 1 Spell/Trap your opponent controls
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_DESTROY)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e3:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e3:SetRange(LOCATION_SZONE)
-	e3:SetCountLimit(1,id)
+	e3:SetCountLimit(1,{id,1})
 	e3:SetCondition(s.descon)
 	e3:SetTarget(s.destg)
 	e3:SetOperation(s.desop)
@@ -40,7 +41,7 @@ function s.setfilter(c)
 end
 function s.setop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(s.setfilter,tp,LOCATION_DECK,0,nil)
-	if #g==0 or not Duel.SelectYesNo(tp,aux.Stringid(id,1)) then return end
+	if #g==0 or not Duel.SelectYesNo(tp,aux.Stringid(id,0)) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
 	local sg=g:Select(tp,1,1,nil)
 	if #sg>0 then
