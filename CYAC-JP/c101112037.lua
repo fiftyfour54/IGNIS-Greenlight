460a664fdc5f25efaf230499b29040ccebf58390
Naim <naimsantos45@gmail.com>
2023-01-02 09:15:23 -0300

    Incorrect category

--- CYAC-JP/c101112037.lua
+++ CYAC-JP/c101112037.lua
@@ -29,7 +29,7 @@ function s.initial_effect(c)
 	--Special Summon 1 banished "Beetrooper" monster
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
-	e3:SetCategory(CATEGORY_TOHAND)
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
 	e3:SetCode(EVENT_REMOVE)
