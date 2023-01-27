549de44622e8ce6fa4f2f9ed93cb1a7d4d2d2d57
Naim <naimsantos45@gmail.com>
2023-01-27 17:58:58 -0300

    Update "Xyz Align"

--- PHHY-EN/c101111084.lua
+++ PHHY-EN/c101111084.lua
@@ -17,16 +17,24 @@ end
 function s.cfilter(c,e)
 	return c:IsFaceup() and c:HasLevel() and c:IsCanBeEffectTarget(e)
 end
-function s.rescon(sg,_,tp)
-	return sg:IsExists(Card.IsControler,1,nil,tp)
+function s.rescon(lvltable)
+	return function (sg,e,tp,mg)
+		return sg:IsExists(Card.IsControler,1,nil,tp) and not sg:IsExists(s.checklevels,1,nil,lvltable)
+	end
+end
+function s.rescon2(lv)
+	return function (sg,e,tp,mg)
+		return sg:IsExists(Card.IsControler,1,nil,tp) and not sg:IsExists(Card.IsLevel,1,nil,lv)
+	end
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end
 	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,e)
-	if chk==0 then return #g>=2 and aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0) end
+	local lvltable=s.GetDeclarableLevels(g)
+	if chk==0 then return #g>=2 and aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon(lvltable),0) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_LVRANK)
-	local lv=Duel.AnnounceLevel(tp,1,12)
-	local sg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,1,tp,HINTMSG_TARGET)
+	local lv=Duel.AnnounceNumber(tp,table.unpack(lvltable))
+	local sg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon2(lv),1,tp,HINTMSG_TARGET)
 	Duel.SetTargetCard(sg)
 	e:SetLabel(lv,sg:GetFirst():GetRace(),sg:GetNext():GetRace())
 	Duel.SetOperationInfo(0,CATEGORY_LVCHANGE,sg,#sg,0,0)
@@ -58,25 +66,29 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		tc:RegisterEffect(e1)
 	end
 end
---[[
-local function ValidDeclarableLevels(g)
-	local lvls={}
+function s.GetDeclarableLevels(g)
+	local res={}
 	for i=1,12 do
-		lvls[i] = i
+		res[i] = i
 	end
-	
-	if #g>=#g:GetClassCount(Card.GetLevel)-2 then --If at least 2 more monsters than different levels, can declare any level:
-		return lvls
-	else --otherwise, check which levels can be declared:
-		local temptable=g:GetClass(Card.GetLevel)
+	local levels=g:GetClass(Card.GetLevel)
+	local n_mon=#g
+	local n_levels=#levels
+	if n_levels==1 then --If all monsters have the same level, remove that level
+		table.remove(res,levels[#levels])
+		return res
+	elseif (n_mon==2 and n_levels==2) then--If 2 monsters with different levels, remove those levels
+		table.sort(levels)
+		for i=2,1,-1 do
+			table.remove(res,levels[i])
+		end
+	else
+		return res
 	end
-	
-	
-	return lvls
 end
-
-
-local lvls=ValidDeclarableLevels(g)
-local lv=Duel.AnnounceNumber(tp,table.unpack(lvls))
-Duel.PrintTable(temptable)
---]]
+function s.checklevels(c,t)
+	for _,level in ipairs(t) do
+		if c:GetLevel()==level then return true end
+	end
+	return false
+end
