427c9b2d4b4837a3ae27caf4560a40f12693c4b5
Naim <naimsantos45@gmail.com>
2023-02-09 06:00:32 -0300

    Added initial version of "Foie Glasya de Nouvellez"

---
+++ DBWS/c100430032 .lua
@@ -0,0 +1,89 @@
+--フォアグラシャ・ド・ヌーベルズ
+--Foie Glasya de Nouvellez
+--scripted by Naim
+local SET_RECIPE=0x193
+local SET_NOUVELLEZ=0x28e
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Shuffle cards from the GY into the Deck
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.tdtg)
+	e1:SetOperation(s.tdop)
+	c:RegisterEffect(e1)
+	--Tribute 2 monsters and Special Summon 1 Level 5/6 "Nouvellez" Ritual Monster from hand/Deck
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_RELEASE+CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_BE_BATTLE_TARGET)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetCondition(s.spcond)
+	c:RegisterEffect(e3)
+end
+s.listed_series={SET_RECIPE}
+function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsAbleToDeck() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToDeck,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToDeck,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,3,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,#g,0,0)
+end
+function s.tdop(e,tp,eg,ep,ev,re,r,rp)
+	local sg=Duel.GetTargetCards(e)
+	Duel.SendtoDeck(sg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+end
+function s.tgtfilter(c)
+	return c:IsMonster() and c:IsLocation(LOCATION_MZONE)
+end
+function s.spcond(e,tp,eg,ep,ev,re,r,rp)
+	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
+	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+	return g:IsExists(s.tgtfilter,1,nil)
+end
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(SET_NOUVELLEZ) and c:IsCanBeSpecialSummoned(e,183,tp,false,false)
+end
+function s.selfnouvfilter(c,tp)
+	return c:IsControler(tp) and c:IsSetCard(SET_NOUVELLEZ)
+end
+function s.cfilter(c,tp)
+	return c:IsReleasableByEffect() and (s.selfnouvfilter(c,tp) or c:IsAttackPos())
+end
+function s.rescon(sg,e,tp,mg)
+	return aux.ChkfMMZ(1)(sg,e,tp,mg) and sg:IsExists(s.atkposchk,2,nil,sg.tp)
+end
+function s.atkposchk(c,sg,tp)
+	return c:IsAttackPos() and sg:IsExists(s.selfnouvfilter,1,c)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,tp)
+	if chk==0 then return #g>=2 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND|LOCATION_DECK,nil,e,tp)
+		and aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0) end
+	Duel.SetOperationInfo(0,CATEGORY_RELEASE,g,2,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND|LOCATION_DECK)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,tp)
+	if #g<2 then return end
+	local rg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,1,tp,HINTMSG_RELEASE)
+	if Duel.Release(rg,REASON_EFFECT)==2 and  Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
+		if #g>0 then
+			Duel.SpecialSummon(g,183,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end
