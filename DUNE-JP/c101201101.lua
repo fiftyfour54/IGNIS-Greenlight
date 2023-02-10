0910d188f12a70186d67be4800d31a7af946773b
Naim <naimsantos45@gmail.com>
2023-02-10 09:13:37 -0300

    Rename functions

--- DUNE-JP/c101201101.lua
+++ DUNE-JP/c101201101.lua
@@ -71,13 +71,13 @@ end
 function s.synchfilter(c)
 	return c:IsFaceup() and c:IsType(TYPE_SYNCHRO) and c:IsLevelAbove(1)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.lvltg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.synchfilter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(s.synchfilter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_APPLYTO)
 	Duel.SelectTarget(tp,s.synchfilter,tp,LOCATION_MZONE,0,1,1,nil)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.lvlop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if not (tc:IsFaceup() and tc:IsRelateToEffect(e)) then return end
 	local lvl=tc:GetLevel()
@@ -92,10 +92,3 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetValue(-value)
 	tc:RegisterEffect(e1)
 end
-
-
-
---[[
-
-If this card you control is used as Synchro Material, you can treat it as a non-Tuner. You can only use each of the following effects of "Wheel Synchron" once per turn. During your Main Phase, you can: Immediately after this effect resolves, Normal Summon 1 Level 4 or lower monster, also you cannot Special Summon monsters from the Extra Deck for the rest of this turn, except Synchro Monsters. You can banish this card from your GY, then target 1 Synchro Monster you control; reduce its Level by up to 4.
-]]
