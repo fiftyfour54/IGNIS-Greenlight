69b4bbd0bc2463804a08a36c48faeeb3ecbe1dcd
user6174 <enne100724@gmail.com>
2023-02-12 12:14:26 +0100

    fixed FilterCount arguments (#201)

--- DBWS/c100430035.lua
+++ DBWS/c100430035.lua
@@ -89,7 +89,7 @@ function s.tdop(e,tp,eg,ep,ev,re,r,rp)
 	local tg=Duel.GetTargetCards(e)
 	if #tg==0 then return end
 	if Duel.SendtoDeck(tg,nil,SEQ_DECKBOTTOM,REASON_EFFECT)>0 then
-		local ct=Duel.GetOperatedGroup():FilterCount(Card.IsLocation,1,nil,LOCATION_DECK)
+		local ct=Duel.GetOperatedGroup():FilterCount(Card.IsLocation,nil,LOCATION_DECK)
 		if ct>0 then
 			Duel.SortDeckbottom(tp,tp,ct)
 			Duel.BreakEffect()
