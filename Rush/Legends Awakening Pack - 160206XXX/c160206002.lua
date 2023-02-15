0c0bf9ffc5569fa9588945a40d51955555c0ef33
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-02-15 14:19:14 +0100

    added Great-Tusked Mammoth

--- Rush/Legends Awakening Pack - 160206XXX/c160206002.lua
+++ Rush/Legends Awakening Pack - 160206XXX/c160206002.lua
@@ -7,6 +7,7 @@ function s.initial_effect(c)
 	local e1=aux.AddNormalSummonProcedure(c,true,true,1,1,SUMMON_TYPE_TRIBUTE,aux.Stringid(id,0),nil,s.otop)
 	--Add to hand
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
