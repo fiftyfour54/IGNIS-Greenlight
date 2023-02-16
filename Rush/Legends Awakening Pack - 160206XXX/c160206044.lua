1a266b1a06407e3f59cc22251bd0607c7f4babfb
Naim <naimsantos45@gmail.com>
2023-02-16 16:48:36 -0300

    fix 'Masenkou, the Lashing Light'

--- Rush/Legends Awakening Pack - 160206XXX/c160206044.lua
+++ Rush/Legends Awakening Pack - 160206XXX/c160206044.lua
@@ -5,7 +5,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
