b618f1ff20ba1f0ad55fafc16aa3d0be3cab8e8b
EerieCode <eeriecode@gmail.com>
2023-02-27 18:24:15 +0100

    MAZE-EN003 --Gate Guardians Combined

---
+++ MAZE-EN/c100431003.lua
@@ -0,0 +1,110 @@
+--Gate Guardians Combined
+--Scripted by Eerie Code
+local s,id=GetID()
+function s.initial_effect(c)
+    c:EnableReviveLimit()
+    if not c:IsStatus(STATUS_COPYING_EFFECT) then
+        Fusion.AddContactProc(c,s.contactfil,s.contactop,aux.FALSE)
+        if c.material_count==nil then
+            s.material_count=3
+            s.material=CARDS_GATE_GUARDIAN
+            s.min_material_count=3
+            s.max_material_count=3
+        end
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+        e1:SetCode(EFFECT_FUSION_MATERIAL)
+        e1:SetCondition(Fusion.ConditionMix(true,true,s.ffilter(CARD_KAZEJIN),s.ffilter(CARD_SUIJIN),s.ffilter(CARD_SANGA)))
+        e1:SetOperation(Fusion.OperationMix(true,true,s.ffilter(CARD_KAZEJIN),s.ffilter(CARD_SUIJIN),s.ffilter(CARD_SANGA)))
+        c:RegisterEffect(e1)
+    end
+    --Negate
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+    e2:SetType(EFFECT_TYPE_QUICK_O)
+    e2:SetCode(EVENT_CHAINING)
+    e2:SetCountLimit(3,id)
+    e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCondition(s.discon)
+    e2:SetTarget(s.distg)
+    e2:SetOperation(s.disop)
+    c:RegisterEffect(e2)
+    --Special Summon
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL+EFFECT_FLAG_DELAY)
+    e3:SetCode(EVENT_LEAVE_FIELD)
+    e3:SetCondition(s.spcon)
+    e3:SetTarget(s.sptg)
+    e3:SetOperation(s.spop)
+    c:RegisterEffect(e3)
+    aux.DoubleSnareValidity(c,LOCATION_MZONE)
+end
+s.listed_names=CARDS_GATE_GUARDIAN
+s.listed_series={SET_GATE_GUARDIAN }
+function s.ffilter(code)
+    return function(c,fc,sub,sub2,mg,sg,tp,contact)
+        if contact then
+            return c:IsOriginalCode(code)
+        else
+            return c:IsSummonCode(fc,SUMMON_TYPE_FUSION,tp,code) or (sub and c:CheckFusionSubstitute(fc)) or (sub2 and c:IsHasEffect(511002961))
+        end
+    end
+end
+function s.contactfil(tp)
+    return Duel.GetMatchingGroup(Card.IsAbleToRemoveAsCost,tp,LOCATION_ONFIELD+LOCATION_HAND+LOCATION_GRAVE,0,nil)
+end
+function s.contactop(g)
+    Duel.Remove(g,POS_FACEUP,REASON_COST+REASON_MATERIAL)
+end
+function s.tfilter(c,tp)
+    return c:IsOnField() and c:IsControler(tp)
+end
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
+    if rp==tp or e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) then return false end
+    if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
+    local tg=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+    return tg and tg:IsExists(s.tfilter,1,nil,tp) and Duel.IsChainNegatable(ev)
+end
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+    if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+        Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+    end
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+        Duel.Destroy(eg,REASON_EFFECT)
+    end
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    return c:GetReasonPlayer()==1-tp and c:IsPreviousControler(tp) and c:IsReason(REASON_EFFECT)
+        and c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsPreviousPosition(POS_FACEUP)
+        and c:IsSummonType(SUMMON_TYPE_SPECIAL)
+end
+function s.spfilter(c,e,tp)
+    if c:IsLocation(LOCATION_EXTRA) and Duel.GetLocationCountFromEx(tp,tp,nil,c)==0 then return false end
+    return c:IsLevelBelow(11) and c:IsSetCard(SET_GATE_GUARDIAN) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    local loc=LOCATION_EXTRA
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then loc=loc+LOCATION_DECK end
+    if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,loc,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,loc)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    local loc=LOCATION_EXTRA
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then loc=loc+LOCATION_DECK end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,loc,0,1,1,nil,e,tp)
+    if #g>0 then
+        Duel.SpecialSummon(g,0,tp,tp,true,false,POS_FACEUP)
+    end
+end
