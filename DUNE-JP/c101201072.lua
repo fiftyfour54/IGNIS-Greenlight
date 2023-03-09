fb808170bfe15d532b92b310177a5b1495d18a42
Larry126 <28985622+larry126@users.noreply.github.com>
2023-03-09 16:29:40 +0800

    Update c101201072.lua

--- DUNE-JP/c101201072.lua
+++ DUNE-JP/c101201072.lua
@@ -29,7 +29,7 @@ function s.initial_effect(c)
 end
 s.listed_series={SET_NOBLE_ARMS,SET_INFERNOBLE_KNIGHT,SET_NOBLE_KNIGHT}
 s.listed_names={CARD_INFERNOBLE_CHARLES}
-function s.cfilter(c,sft)
+function s.cfilter(c,sft,e,tp)
 	return c:IsSetCard(SET_NOBLE_ARMS) and c:IsEquipSpell() and not c:IsPublic()
 		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,nil,e,tp,c,sft)
 end
@@ -40,7 +40,7 @@ function s.spfilter(c,e,tp,eq,sft)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,nil,Duel.GetLocationCount(tp,LOCATION_SZONE)) end
+		and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,nil,Duel.GetLocationCount(tp,LOCATION_SZONE),e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND|LOCATION_DECK)
 	Duel.SetPossibleOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_HAND)
 	Duel.SetPossibleOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_HAND)
@@ -48,36 +48,38 @@ end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local sft=Duel.GetLocationCount(tp,LOCATION_SZONE)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-	local eq=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND,0,1,1,nil,sft):GetFirst()
+	local eq=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND,0,1,1,nil,sft,e,tp):GetFirst()
 	if not eq then return end
 	Duel.ConfirmCards(1-tp,eq)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local tc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,1,nil,eq,sft):GetFirst()
-	Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
-	local eqBool=sft>0 and eq:CheckEquipTarget(tc) and eq:CheckUniqueOnField(tp) and not eq:IsForbidden()
-	local tgBool=eq:IsAbleToGrave()
-	if eqBool or tgBool then
-		Duel.BreakEffect()
-		local op=Duel.SelectEffect({eqBool,aux.Stringid(id,2)},{tgBool,aux.Stringid(id,2)})
-		if op==1 then
-			Duel.Equip(tp,eq,tc)
-		else
-			Duel.SendtoGrave(eq,REASON_EFFECT)
+	local tc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,1,nil,e,tp,eq,sft):GetFirst()
+	if Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0 then
+		local eqBool=sft>0 and eq:CheckEquipTarget(tc) and eq:CheckUniqueOnField(tp) and not eq:IsForbidden()
+		local tgBool=eq:IsAbleToGrave()
+		if eqBool or tgBool then
+			Duel.BreakEffect()
+			local op=Duel.SelectEffect(tp,{eqBool,aux.Stringid(id,2)},{tgBool,aux.Stringid(id,3)})
+			if op==1 then
+				Duel.Equip(tp,eq,tc)
+			else
+				Duel.SendtoGrave(eq,REASON_EFFECT)
+			end
 		end
 	end
 end
-function s.eqfilter(c)
-	return not c:IsForbidden() and c:IsSetCard(SET_NOBLE_KNIGHT) and c:IsMonster()
+function s.eqfilter(c,p)
+	return c:IsSetCard(SET_NOBLE_KNIGHT) and c:IsMonster()
+		and c:CheckUniqueOnField(p) and not c:IsForbidden()
 end
 function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE)
 		and chkc:IsFaceup() and chkc:IsCode(CARD_INFERNOBLE_CHARLES) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
 		and Duel.IsExistingTarget(aux.FaceupFilter(Card.IsCode,CARD_INFERNOBLE_CHARLES),tp,LOCATION_MZONE,0,1,nil)
-		and Duel.IsExistingMatchingCard(s.eqfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,nil) end
+		and Duel.IsExistingMatchingCard(s.eqfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,nil,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,s.efilter,tp,LOCATION_MZONE,0,1,1,nil,tp)
+	Duel.SelectTarget(tp,aux.FaceupFilter(Card.IsCode,CARD_INFERNOBLE_CHARLES),tp,LOCATION_MZONE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_HAND|LOCATION_DECK)
 end
 function s.eqop(e,tp,eg,ep,ev,re,r,rp)
@@ -85,7 +87,7 @@ function s.eqop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsFacedown() or not tc:IsRelateToEffect(e) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	local eq=Duel.SelectMatchingCard(tp,s.eqfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,1,nil):GetFirst()
+	local eq=Duel.SelectMatchingCard(tp,s.eqfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,1,nil,tp):GetFirst()
 	if eq and Duel.Equip(tp,eq,tc,true) then
 		local c=e:GetHandler()
 		--Equip Limit
@@ -95,7 +97,7 @@ function s.eqop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetValue(function(_,_c) return _c==tc end)
 		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
 		eq:RegisterEffect(e1)
-		--Increase ATK
+		--Increase 500 ATK
 		local e2=Effect.CreateEffect(c)
 		e2:SetType(EFFECT_TYPE_EQUIP)
 		e2:SetCode(EFFECT_UPDATE_ATTACK)
