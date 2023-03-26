add785b996078c343ce61bcde6b14eea25d03efa
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-03-26 10:52:49 +0200

    added Voidvelgr Gigantomachia

--- Rush/Oblivion of the Flash - 160013XXX/c160013008.lua
+++ Rush/Oblivion of the Flash - 160013XXX/c160013008.lua
@@ -6,7 +6,7 @@ function s.initial_effect(c)
 	-- Mill
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_DECKDES)
+	e1:SetCategory(CATEGORY_DECKDES+CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
@@ -19,8 +19,10 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetFieldGroupCountRush(tp,0,LOCATION_MZONE)>1
 end
 function s.mltg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsLevelBelow,8),tp,0,LOCATION_MZONE,nil)
 	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,3) end
 	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,3)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
 end
 function s.cfilter(c)
 	return c:IsLocation(LOCATION_GRAVE) and c:IsRace(RACE_GALAXY) and c:IsAttribute(ATTRIBUTE_DARK)
