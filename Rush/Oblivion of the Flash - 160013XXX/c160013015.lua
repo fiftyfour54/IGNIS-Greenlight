8900dfdc4a5ad2fbdecb47ad2f1fd1ede615b6a5
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-04-18 21:02:13 +0200

    fix

--- Rush/Oblivion of the Flash - 160013XXX/c160013015.lua
+++ Rush/Oblivion of the Flash - 160013XXX/c160013015.lua
@@ -3,7 +3,7 @@
 --scripted by YoshiDuels
 local s,id=GetID()
 function s.initial_effect(c)
-	--Gain LP
+	--Special Summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
