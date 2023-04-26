ad85fc37ded2298faa7844b45bc54d90df298f7c
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-04-26 19:24:28 +0200

    added Charis Magic - Elegant Change

--- Rush/Oblivion of the Flash - 160013XXX/c160013010.lua
+++ Rush/Oblivion of the Flash - 160013XXX/c160013010.lua
@@ -3,7 +3,7 @@
 --scripted by YoshiDuels
 local s,id=GetID()
 function s.initial_effect(c)
-	--Special summon wyrm normal monster from GY
+	--Special summon 1 monster from GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
