e12f3c0d3670c24a6588288776ac27ddfb5c1a10
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-05-29 16:14:00 +0300

    kono powa

--- SD46-JP/c100346025.lua
+++ SD46-JP/c100346025.lua
@@ -5,77 +5,60 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCondition(s.condition)
+	e1:SetCondition(function() return Duel.IsAbleToEnterBP() or Duel.IsBattlePhase() end)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_names={CARD_RED_DRAGON_ARCHFIEND }
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetCurrentPhase()<PHASE_MAIN2
-end
-function s.filter(c)
-	return c:IsFaceup() and c:IsCode(CARD_RED_DRAGON_ARCHFIEND) and c:GetFlagEffect(id)==0
+s.listed_names={CARD_RED_DRAGON_ARCHFIEND}
+function s.tgfilter(c)
+	return c:IsFaceup() and c:IsCode(CARD_RED_DRAGON_ARCHFIEND) and not c:HasFlagEffect(id)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.tgfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_MZONE,0,1,1,nil)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
-		if tc:GetFlagEffect(id)==0 then
-			tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_FIELD)
-			e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-			e1:SetCode(EFFECT_CANNOT_ACTIVATE)
-			e1:SetRange(LOCATION_MZONE)
-			e1:SetTargetRange(0,1)
-			e1:SetCondition(s.actcon)
-			e1:SetValue(1)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-			tc:RegisterEffect(e1)
-			local e2=Effect.CreateEffect(c)
-			e2:SetType(EFFECT_TYPE_SINGLE)
-			e2:SetCode(EFFECT_PIERCE)
-			e2:SetCondition(s.effcon)
-			e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-			tc:RegisterEffect(e2)
-			local e3=Effect.CreateEffect(c)
-			e3:SetType(EFFECT_TYPE_SINGLE)
-			e3:SetCode(EFFECT_CHANGE_BATTLE_DAMAGE)
-			e3:SetCondition(s.damcon)
-			e3:SetValue(aux.ChangeBattleDamage(1,DOUBLE_DAMAGE))
-			e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-			tc:RegisterEffect(e3)
-			local e4=Effect.CreateEffect(c)
-			e4:SetType(EFFECT_TYPE_SINGLE)
-			e4:SetCode(EFFECT_UPDATE_ATTACK)
-			e4:SetCondition(s.atkcon)
-			e4:SetValue(1000)
-			e4:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-			tc:RegisterEffect(e4)
-		end
-	end
+	if not tc:IsRelateToEffect(e) or tc:HasFlagEffect(id) then return end
+	local c=e:GetHandler()
+	tc:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(id,1))
+	--Gains 1000 ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetCondition(s.effcon)
+	e1:SetValue(1000)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+	tc:RegisterEffect(e1)
+	--Opponent cannot activate cards or effects
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(0,1)
+	e2:SetCondition(s.effcon)
+	e2:SetValue(1)
+	e2:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+	tc:RegisterEffect(e2)
+	--Deals piercing damage
+	local e3=e1:Clone()
+	e3:SetCode(EFFECT_PIERCE)
+	tc:RegisterEffect(e3)
+	--Any battle damage the opponent takes is doubled
+	local e4=e1:Clone()
+	e4:SetCode(EFFECT_CHANGE_BATTLE_DAMAGE)
+	e4:SetValue(aux.ChangeBattleDamage(1,DOUBLE_DAMAGE))
+	tc:RegisterEffect(e4)
 end
 function s.effcon(e)
-	return e:GetOwnerPlayer()==e:GetHandlerPlayer()
-end
-function s.actcon(e)
 	local c=e:GetHandler()
-	return (Duel.GetAttacker()==c or Duel.GetAttackTarget()==c) and c:GetBattleTarget()~=nil
-		and s.effcon(e)
-end
-function s.damcon(e,tp,eg,ep,ev,re,r,rp)
-	return s.effcon(e) and e:GetHandler():GetBattleTarget()~=nil
-end
-function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetCurrentPhase()==PHASE_DAMAGE_CAL and s.actcon(e)
+	return c:IsRelateToBattle() and c:GetBattleTarget() and e:GetOwnerPlayer()==e:GetHandlerPlayer()
 end
