2007ca174526fb59bb8865a6ab52832f3423dbb1
Naim <naimsantos45@gmail.com>
2023-06-08 09:36:19 -0300

    update Over Future Fusion
    -fixed indentation
    -variable name

--- VJMP/c100200237.lua
+++ VJMP/c100200237.lua
@@ -37,7 +37,7 @@ function s.tgfilter(c,fc)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local fusparams={handler=e:GetHandler(),fusfilter=s.ffilter,matfilter=aux.FALSE,
-                     extrafil=s.fextra,extraop=Fusion.BanishMaterial,extratg=s.extratg}
+						extrafil=s.fextra,extraop=Fusion.BanishMaterial,extratg=s.extratg}
 	local b1=Duel.IsExistingMatchingCard(s.exfilter,tp,LOCATION_EXTRA,0,1,nil,tp) and Duel.GetFlagEffect(tp,id)==0
 	local b2=Fusion.SummonEffTG(fusparams)(e,tp,eg,ep,ev,re,r,rp,0) and Duel.GetFlagEffect(tp,id+100)==0
 	if chk==0 then return b1 or b2 end
@@ -56,6 +56,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	end
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
 	local op=e:GetLabel()
 	if op==1 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
@@ -66,7 +67,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		local tc=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil,cc):GetFirst()
 		if tc and Duel.SendtoGrave(tc,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_GRAVE) then
 			--Cannot Special Summon monsters with the same name
-			local e1=Effect.CreateEffect(e:GetHandler())
+			local e1=Effect.CreateEffect(c)
 			e1:SetDescription(aux.Stringid(id,2))
 			e1:SetType(EFFECT_TYPE_FIELD)
 			e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
@@ -83,7 +84,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 			Duel.RegisterEffect(e2,tp)
 	end
 	elseif op==2 then
-		local fusparams={handler=e:GetHandler(),fusfilter=s.ffilter,matfilter=aux.FALSE,extrafil=s.fextra,extraop=Fusion.BanishMaterial,extratg=s.extratg}
+		local fusparams={handler=c,fusfilter=s.ffilter,matfilter=aux.FALSE,extrafil=s.fextra,extraop=Fusion.BanishMaterial,extratg=s.extratg}
 		Fusion.SummonEffOP(fusparams)(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
