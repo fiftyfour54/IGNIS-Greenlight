9766178b49970af0c6c76865639d7bcf0a197410
Naim <naimsantos45@gmail.com>
2023-06-13 14:44:54 -0300

    Update "Concours de Cuisine"

--- AGOV-JP/c101202064.lua
+++ AGOV-JP/c101202064.lua
@@ -31,33 +31,27 @@ local locs=LOCATION_HAND|LOCATION_DECK|LOCATION_EXTRA
 function s.cfilter(c,archetyp)
 	return c:IsSetCard(archetyp) and c:IsType(TYPE_PENDULUM)
 end
-function s.spchk(c,e,tp,archtyp2)
-	local loc=false
-	if not c:IsLocation(LOCATION_EXTRA) then
-		loc=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-	else 
-		loc=Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
-	end
-	return c:IsSetCard(archtyp2) and loc and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,1-tp)
+function s.firstsummon(c,e,tp,sg)
+	return c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and ((c:IsLocation(LOCATION_EXTRA) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0)
+		or Duel.GetLocationCountFromEx(tp,tp,nil,c)>0)
+		and sg:IsExists(s.secondsummon,1,c,e,tp) --exclude 'c'
 end
-function s.spfilter(c,e,tp,archtyp1,archtyp2)
-	local loc=false
-	if not c:IsLocation(LOCATION_EXTRA) then
-		loc=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-	else 
-		loc=Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
-	end
-	return c:IsSetCard(archtyp1) and loc and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-		and Duel.IsExistingMatchingCard(s.spchk,tp,locs,0,1,nil,e,tp,archtyp2)
+function s.secondsummon(c,e,tp)
+	return c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,1-tp)
+		and ((not c:IsLocation(LOCATION_EXTRA) and Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0)
+		or Duel.GetLocationCountFromEx(1-tp,tp,nil,c)>0)
+end
+function s.rescon(sg,e,tp,mg)
+	return sg:FilterCount(Card.IsSetCard,nil,SET_PATISSCIEL)==1
+		and sg:FilterCount(Card.IsSetCard,nil,SET_NOUVELLES)==1
+		and sg:IsExists(s.firstsummon,1,nil,e,tp,sg)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g1=Duel.GetMatchingGroup(s.cfilter,tp,locs,0,nil,SET_NOUVELLES)
 	local g2=Duel.GetMatchingGroup(s.cfilter,tp,locs,0,nil,SET_PATISSCIEL)
 	local g=g1+g2
-	if chk==0 then return #g1>0 and #g2>0
-		and (g:IsExists(s.spfilter,1,nil,e,tp,SET_NOUVELLES,SET_PATISSCIEL)
-		or g:IsExists(s.spfilter,1,nil,e,tp,SET_PATISSCIEL,SET_NOUVELLES))
-	end
+	if chk==0 then return #g1>0 and #g2>0 and aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,locs)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
@@ -85,30 +79,15 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local g2=Duel.GetMatchingGroup(s.cfilter,tp,locs,0,nil,SET_PATISSCIEL)
 	if #g1==0 or #g2==0 then return end
 	local g=g1+g2
-	local op1=g:IsExists(s.spfilter,1,nil,e,tp,SET_NOUVELLES,SET_PATISSCIEL)
-	local op2=g:IsExists(s.spfilter,1,nil,e,tp,SET_PATISSCIEL,SET_NOUVELLES)
-	if not (op1 or op2) then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local sc1=g:FilterSelect(tp,s.spfilter,1,1,nil,e,tp,SET_NOUVELLES,SET_PATISSCIEL):GetFirst()
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local sc2=g:FilterSelect(tp,s.spchk,1,1,sc1,e,tp,SET_PATISSCIEL):GetFirst()
-	local b1=s.extrachk(sc1,e,tp) and s.extrachk(sc2,e,1-tp)
-	local b2=s.extrachk(sc1,e,1-tp) and s.extrachk(sc2,e,tp)
+	local sg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,1,tp,HINTMSG_SPSUMMON)
+	if #sg~=2 then return end
+	local sc1,sc2=sg:GetFirst(),sg:GetNext()
 	Duel.Hint(HINT_CARD,tp,sc1:GetCode())
 	local op=Duel.SelectEffect(tp,
-			{b1,aux.Stringid(id,1)}, --"summon to your field"
-			{b2,aux.Stringid(id,2)})-1 --"summon to the opponent's field
+			{true,aux.Stringid(id,1)}, --"summon to your field"
+			{true,aux.Stringid(id,2)})-1 --"summon to the opponent's field
 	Duel.SpecialSummon(sc1,0,tp,op,false,false,POS_FACEUP)
 	Duel.SpecialSummon(sc2,0,tp,1-op,false,false,POS_FACEUP)
-end
-function s.extrachk(c,e,tp)
-	local loc=false
-	if not c:IsLocation(LOCATION_EXTRA) then
-		loc=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-	else 
-		loc=Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
-	end
-	return loc and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,1-tp)
 end
 function s.sumlimit(e,c)
 	if not c then return false end
