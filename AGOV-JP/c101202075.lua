ea6f454ea9c9a00c9a6f94e588ff7e0a5851379a
Naim <naimsantos45@gmail.com>
2023-07-06 17:22:34 -0300

    Add "The Glaring Ruler Snake-Eyes"

---
+++ AGOV-JP/c101202075.lua
@@ -0,0 +1,82 @@
+--睨み統べるスネークアイズ
+--The Glaring Ruler Snake-Eyes
+--scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_END_PHASE)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	c:RegisterEffect(e1)
+end
+s.listed_series={SET_SNAKE_EYE}
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsSetCard,SET_SNAKE_EYE),tp,LOCATION_MZONE,0,nil)
+	return g:GetSum(Card.GetLevel)>=2
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local b1=s.sttg(e,tp,eg,ep,ev,re,r,rp,0,chkc)--place a monster in the S/T zone
+	local b2=s.sptg(e,tp,eg,ep,ev,re,r,rp,0,chkc)--special summon a monster that is a Continous Spell
+	if chk==0 then return b1 or b2 end
+	local op=Duel.SelectEffect(tp,
+		{b1,aux.Stringid(id,1)},
+		{b2,aux.Stringid(id,2)})
+	if op==1 then
+		s.sttg(e,tp,eg,ep,ev,re,r,rp,1,chkc)
+		e:SetOperation(s.stop)
+	elseif op==2 then
+		s.sptg(e,tp,eg,ep,ev,re,r,rp,1,chkc)
+		e:SetOperation(s.spop)
+	end
+end
+function s.tgfilter(c)
+	local p=c:GetOwner()
+	return c:IsFaceup() and Duel.GetLocationCount(p,LOCATION_SZONE)>0
+		and c:CheckUniqueOnField(p,LOCATION_SZONE)
+		and (c:IsLocation(LOCATION_MZONE) or not c:IsForbidden())
+end
+function s.sttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE|LOCATION_GRAVE) and s.tgfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.tgfilter,tp,0,LOCATION_MZONE|LOCATION_GRAVE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,s.tgfilter,tp,0,LOCATION_MZONE|LOCATION_GRAVE,1,1,nil)
+end
+function s.spfilter(c,e,tp)
+	return c:IsFaceup() and c:IsOriginalType(TYPE_MONSTER) and c:IsContinuousSpell()
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_SZONE) and s.spfilter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_SZONE,LOCATION_SZONE,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_SZONE,LOCATION_SZONE,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function s.stop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e)
+		and Duel.MoveToField(tc,tp,tc:GetOwner(),LOCATION_SZONE,POS_FACEUP,true) then
+		--Treat it as a Continuous Spell
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EFFECT_CHANGE_TYPE)
+		e1:SetValue(TYPE_SPELL|TYPE_CONTINUOUS)
+		e1:SetReset(RESET_EVENT|(RESETS_STANDARD&~RESET_TURN_SET))
+		tc:RegisterEffect(e1)
+	end
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then 
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
