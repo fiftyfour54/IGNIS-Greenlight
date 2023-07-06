c2ab43defe3a9febcddb375d49e3d8f5905a21ff
Naim <naimsantos45@gmail.com>
2023-07-06 17:24:48 -0300

    Fix category

--- AGOV-JP/c101202075.lua
+++ AGOV-JP/c101202075.lua
@@ -6,7 +6,6 @@ function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
@@ -33,6 +32,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 		e:SetOperation(s.stop)
 	elseif op==2 then
 		s.sptg(e,tp,eg,ep,ev,re,r,rp,1,chkc)
+		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
 		e:SetOperation(s.spop)
 	end
 end
