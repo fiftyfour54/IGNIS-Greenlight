414f1e7ecbf6f2e82cb2558630e6e1ebbb3d5fbb
Naim <naimsantos45@gmail.com>
2023-07-07 13:03:26 -0300

    Add "Shinobaron Shade Peacock"

---
+++ AGOV-JP/c101202029.lua
@@ -0,0 +1,99 @@
+--霊魂鳥影－彦孔雀
+--Shinobaron Shade Peacock
+--scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--This card's name become "Shinobaron Peacock"
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_CHANGE_CODE)
+	e1:SetRange(LOCATION_MZONE|LOCATION_HAND)
+	e1:SetValue(52900000)
+	c:RegisterEffect(e1)
+	--Search 1 Ritual monster and 1 Ritual Spell
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:Setcondition(function(e,tp) return e:GetHandler():IsSummonType(SUMMON_TYPE_RITUAL) end)
+	e2:SetCountLimit(1,id)
+	e2:SetCost(aux.selfreleasecost)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+	--Special Summon itself after being banished
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e3:SetRange(LOCATION_REMOVED)
+	e3:SetCountLimit(1)
+	e3:SetCondition(s.selfspcond)
+	e3:SetTarget(s.selfsptg)
+	e3:SetOperation(s.selfspop)
+	c:RegisterEffect(e3)
+	--Spirit return
+	local sme,soe=Spirit.AddProcedure(c,EVENT_SPSUMMON_SUCCESS)
+	--Mandatory return
+	sme:SetCategory(CATEGORY_TOHAND)
+	sme:SetTarget(s.mrettg)
+	sme:SetOperation(s.retop)
+	--Optional return
+	soe:SetCategory(CATEGORY_TOHAND)
+	soe:SetTarget(s.orettg)
+	soe:SetOperation(s.retop)
+end
+s.listed_names={73055622,52900000} --Shinobird's Calling, Shinobaron Peacock
+function s.thfilter(c)
+	return c:IsAbleTohand() and (c:IsRitualSpell() or  c:IsType(TYPE_SPIRIT))
+end
+function s.rescon(sg,e,tp,mg)
+	return sg:FilterCount(Card.IsRitualSpell,nil)==1
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
+	if chk==0 then return #g>=2 and aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
+	if #g<2 then return end
+	local thg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,1,tp,HINTMSG_ATOHAND)
+	if #thg>0 then
+		Duel.SendtoHand(thg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,thg)
+	end
+end
+function s.selfspcond(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnCount()==e:GetHandler():GetTurnID()+1
+end
+function s.selfsptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,LOCATION_REMOVED)
+end
+function s.selfspop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.mrettg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Spirit.MandatoryReturnTarget(e,tp,eg,ep,ev,re,r,rp,1)
+end
+function s.orettg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Spirit.OptionalReturnTarget(e,tp,eg,ep,ev,re,r,rp,0)  end
+	Spirit.OptionalReturnTarget(e,tp,eg,ep,ev,re,r,rp,1)
+end
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SendtoHand(c,nil,REASON_EFFECT)
+	end
+end
