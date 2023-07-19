9c9facc74dd15312eb877abce860a1df62ade076
Larry126 <28985622+larry126@users.noreply.github.com>
2023-07-19 21:14:34 +0800

    Minor clean-ups

--- AGOV-JP/c101202042.lua
+++ AGOV-JP/c101202042.lua
@@ -7,17 +7,18 @@ function s.initial_effect(c)
 	Xyz.AddProcedure(c,nil,12,2,s.xyzfilter,aux.Stringid(id,0),2,s.xyzop)
 	--prevent activation
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,2))
+	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetRange(LOCATION_MZONE)
 	e1:SetTargetRange(1,1)
 	e1:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ) end)
 	e1:SetValue(s.actlimit)
 	c:RegisterEffect(e1)
 	--to hand
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,3))
+	e2:SetDescription(aux.Stringid(id,2))
 	e2:SetCategory(CATEGORY_TOHAND)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
@@ -44,14 +45,14 @@ function s.initial_effect(c)
 	end)
 end
 function s.checkfilter(c,tp)
-	return c:IsPreviousLocation(LOCATION_EXTRA) and c:IsSummonPlayer(tp)
+	return c:IsSummonLocation(LOCATION_EXTRA) and c:IsSummonPlayer(tp)
 end
 function s.checkop(e,tp,eg,ep,ev,re,r,rp)
 	for p=0,1 do
 		local gc=eg:FilterCount(s.checkfilter,nil,p)
 		s.sum_count[p]=s.sum_count[p]+gc
-		if s.sum_count[p]>=2 and Duel.GetFlagEffect(1-p,id)==0 then
-			Duel.RegisterFlagEffect(1-p,id,RESET_PHASE|PHASE_END,0,2)
+		if s.sum_count[p]>=2 and not Duel.HasFlagEffect(p,id) then
+			Duel.RegisterFlagEffect(p,id,RESET_PHASE|PHASE_END,0,2)
 		end
 	end
 end
@@ -62,24 +63,23 @@ function s.xyzfilter(c,tp,xyzc)
 	return c:IsFaceup() and not Duel.IsExistingMatchingCard(s.xyzfilter2,tp,LOCATION_MZONE,0,1,nil,c:GetAttack())
 end
 function s.xyzop(e,tp,chk)
-	if chk==0 then return Duel.GetFlagEffect(tp,id)>0 and Duel.GetFlagEffect(tp,id+100)==0 end
-	Duel.RegisterFlagEffect(tp,id+100,RESET_PHASE+PHASE_END,0,1)
+	if chk==0 then return Duel.HasFlagEffect(1-tp,id) end
+	local c=e:GetHandler()
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetCode(EFFECT_CANNOT_SUMMON)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetTargetRange(1,0)
-	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetReset(RESET_PHASE|PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 	local e2=e1:Clone()
 	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	Duel.RegisterEffect(e2,tp)
+	aux.RegisterClientHint(c,nil,tp,1,0,aux.Stringid(id,3),nil)
 	return true
 end
 function s.actlimit(e,re,tp)
-	return re:IsActiveType(TYPE_MONSTER) and re:GetHandler():IsAttackAbove(3000)
+	return re:IsMonsterEffect() and re:GetHandler():IsAttackAbove(3000)
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
