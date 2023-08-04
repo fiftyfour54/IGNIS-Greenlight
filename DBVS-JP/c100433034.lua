c0f630dada7033bb6e57e7212fc252cdb8c99878
EerieCode <eeriecode@gmail.com>
2023-08-04 08:57:57 +0200

    DBVS-JP034 --ヴァルモニカの神異－ゼブフェーラ

---
+++ DBVS-JP/c100433034.lua
@@ -0,0 +1,84 @@
+--ヴァルモニカの神異－ゼブフェーラ
+--Divine Oddity of Valmonica – Zebufera
+--Scripted by Eerie Code
+local s,id=GetID()
+function s.initial_effect(c)
+	c:SetSPSummonOnce(id)
+	c:EnableReviveLimit()
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_EFFECT),1,1)
+	--splimit
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SPSUMMON_COST)
+	e1:SetCost(s.spcost)
+	c:RegisterEffect(e1)
+	--Destruction replacement
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_DESTROY_REPLACE)
+	e2:SetTarget(s.reptg)
+	e2:SetOperation(s.repop)
+	c:RegisterEffect(e2)
+	--copy effect
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetCondition(function(e) Duel.GetTurnPlayer()~=e:GetHandlerPlayer() end)
+	e3:SetTarget(s.cptg)
+	e3:SetOperation(s.cpop)
+	c:RegisterEffect(e3)
+end
+s.listed_series={SET_VALMONICA }
+function s.spcfilter(c)
+	return c:IsFaceup() and c:IsOriginalRace(RACE_FIEND) and c:GetCounter(COUNTER_RESONANCE)>=3
+end
+function s.spcost(e,c,tp,st)
+	if (st&SUMMON_TYPE_LINK)~=SUMMON_TYPE_LINK then return true end
+	return Duel.IsExistingMatchingCard(s.spcfilter,tp,LOCATION_PZONE,0,1,nil)
+end
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return not c:IsReason(REASON_REPLACE) and Duel.IsCanRemoveCounter(tp,LOCATION_PZONE,0,COUNTER_RESONANCE,3,REASON_EFFECT) end
+	return Duel.SelectEffectYesNo(tp,c,96)
+end
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.RemoveCounter(tp,LOCATION_PZONE,0,COUNTER_RESONANCE,3,REASON_EFFECT)
+end
+function s.cpfilter(c)
+	return c:IsSetCard(SET_VALMONICA) and (c:IsNormalSpell() or c:IsNormalTrap()) 
+		and (c:IsFaceup() or not c:IsLocation(LOCATION_REMOVED))
+		and c:CheckActivateEffect(false,true,false)~=nil
+end
+function s.cptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE|LOCATION_REMOVED) and chkc:IsControler(tp) and s.cpfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.cpfilter,tp,LOCATION_GRAVE|LOCATION_REMOVED,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,s.cpfilter,tp,LOCATION_GRAVE|LOCATION_REMOVED,0,1,1,nil)
+end
+function s.cpop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if not (tc and tc:IsRelateToEffect(e)) then return end
+	local te,ceg,cep,cev,cre,cr,crp=tc:CheckActivateEffect(false,true,true)
+	if not te then return end
+	local tg=te:GetTarget()
+	local op=te:GetOperation()
+	if tg then tg(te,tp,Group.CreateGroup(),PLAYER_NONE,0,e,REASON_EFFECT,PLAYER_NONE,1) end
+	Duel.BreakEffect()
+	tc:CreateEffectRelation(te)
+	Duel.BreakEffect()
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	for etc in aux.Next(g) do
+		etc:CreateEffectRelation(te)
+	end
+	if op then op(te,tp,Group.CreateGroup(),PLAYER_NONE,0,e,REASON_EFFECT,PLAYER_NONE,1) end
+	tc:ReleaseEffectRelation(te)
+	for etc in aux.Next(g) do
+		etc:ReleaseEffectRelation(te)
+	end
+end
