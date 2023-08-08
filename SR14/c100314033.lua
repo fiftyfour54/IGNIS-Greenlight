7c4e9d67c1b921cf603177efdf279b2989572df7
Hatter <sanity.sierota@protonmail.com>
2023-08-08 15:48:32 +0800

    add "Fire King Avatar Hanuman"

--- SR14/c100314033.lua
+++ SR14/c100314033.lua
@@ -23,6 +23,7 @@ function s.initial_effect(c)
 	e2:SetOperation(s.chlimop)
 	c:RegisterEffect(e2)
 end
+s.listed_series={SET_FIRE_KING}
 function s.spfilter(c,e,tp)
 	return c:IsAttribute(ATTRIBUTE_FIRE) and c:IsRace(RACES_BEAST_BWARRIOR_WINGB)
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
