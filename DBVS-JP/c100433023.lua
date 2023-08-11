a802c38869b86a2841703f4c438a4e63b12ba7fa
Naim <naimsantos45@gmail.com>
2023-08-11 07:25:08 -0300

    Add "Truth Centurion"

--- DBVS-JP/c100433023.lua
+++ DBVS-JP/c100433023.lua
@@ -5,7 +5,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Banish 1 Monster card from the field
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_REMOVE+CATEGORY_SPECIAL_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
