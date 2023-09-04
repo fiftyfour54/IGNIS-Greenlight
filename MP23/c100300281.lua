0c69196ff8c6f67e2caee77014507b60cb09a4b5
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-09-04 13:13:03 +0300

    MP23 updates/fixes

--- MP23/c100300281.lua
+++ MP23/c100300281.lua
@@ -10,17 +10,16 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--Destruction replacement for "Noble Knight" cards
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EFFECT_DESTROY_REPLACE)
 	e2:SetRange(LOCATION_FZONE)
 	e2:SetTarget(s.reptg)
 	e2:SetValue(s.repval)
 	e2:SetOperation(s.repop)
 	c:RegisterEffect(e2)
-	--Banish this card, place 1 Noble Knights of the Round Table" in your Field Zone and Special summon or add to hand 1 "Artorigus"/"Noble Arms" card
+	--Banish this card, place 1 "Noble Knights of the Round Table" in your Field Zone and Special summon or add to hand 1 "Artorigus"/"Noble Arms" card
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetDescription(aux.Stringid(id,0))
 	e3:SetCategory(CATEGORY_REMOVE+CATEGORY_SPECIAL_SUMMON+CATEGORY_SEARCH+CATEGORY_TOHAND)
 	e3:SetType(EFFECT_TYPE_IGNITION)
 	e3:SetRange(LOCATION_FZONE)
@@ -71,32 +70,44 @@ function s.tofieldfilter(c)
 end
 function s.tofieldtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return c:IsAbleToRemove() and Duel.IsExistingMatchingCard(s.tofieldfilter,tp,LOCATION_DECK,0,1,nil) end
+	if chk==0 then return c:IsAbleToRemove() and Duel.IsExistingMatchingCard(s.tofieldfilter,tp,LOCATION_HAND|LOCATION_DECK|LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,c,1,tp,0)
 	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK|LOCATION_GRAVE)
 	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK|LOCATION_GRAVE)
 end
 function s.cfilter(c,e,tp,ft)
 	return (c:IsSetCard(SET_NOBLE_ARMS) and c:IsAbleToHand())
-		or (ft>0 and c:IsMonster() and c:IsSetCard(SET_ARTORIGUS) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP))
+		or (ft>0 and c:IsSetCard(SET_ARTORIGUS) and c:IsCanBeSpecialSummoned(e,0,tp,false,false))
 end
 function s.tofieldop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and aux.RemoveUntil(c,nil,REASON_EFFECT,PHASE_STANDBY,id,e,tp,s.returnop) and c:IsLocation(LOCATION_REMOVED) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
-		local tc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.tofieldfilter),tp,LOCATION_DECK|LOCATION_HAND|LOCATION_GRAVE,0,1,1,nil):GetFirst()
+		local tc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.tofieldfilter),tp,LOCATION_HAND|LOCATION_DECK|LOCATION_GRAVE,0,1,1,nil):GetFirst()
 		if not tc or not Duel.MoveToField(tc,tp,tp,LOCATION_FZONE,POS_FACEUP,true) then return end
 		--Special Summon 1 "Artorigus" monster, or add to your hand 1 "Noble Arms" card, from your Deck or GY
 		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-		if Duel.IsExistingMatchingCard(aux.NecroValleyFilter(s.cfilter),tp,LOCATION_DECK|LOCATION_GRAVE,0,1,nil,e,tp,ft) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		if Duel.IsExistingMatchingCard(aux.NecroValleyFilter(s.cfilter),tp,LOCATION_DECK|LOCATION_GRAVE,0,1,nil,e,tp,ft) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+			Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
 			local sc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.cfilter),tp,LOCATION_DECK|LOCATION_GRAVE,0,1,1,nil,e,tp,ft):GetFirst()
 			if not sc then return end
+			local b1=ft>0 and sc:IsSetCard(SET_ARTORIGUS) and sc:IsCanBeSpecialSummoned(e,0,tp,false,false)
+			local b2=sc:IsSetCard(SET_NOBLE_ARMS) and sc:IsAbleToHand()
 			Duel.BreakEffect()
-			aux.ToHandOrElse(sc,tp,
-							function(sc) return ft>0 and sc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP) end,
-							function(sc) Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)end,
-							aux.Stringid(id,3)
-			)
+			if b1 and b2 then
+				aux.ToHandOrElse(sc,tp,
+					function(sc) return ft>0 and sc:IsCanBeSpecialSummoned(e,0,tp,false,false) end,
+					function(sc) Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP) end,
+					aux.Stringid(id,3)
+				)
+			else
+				if b1 then
+					Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)
+				elseif b2 then
+					Duel.SendtoHand(sc,nil,REASON_EFFECT)
+					Duel.ConfirmCards(1-tp,sc)
+				end
+			end
 		end
 	end
 end
