b8c51b9a0c3e4a6ef4a7405a4d64a5bd0f69a6e8
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-09-11 23:00:18 +0300

    Saffira + Sauravis + Authority updates/fixes

--- PHNI-JP/c101203020.lua
+++ PHNI-JP/c101203020.lua
@@ -3,51 +3,53 @@
 --Ashaki
 local s,id=GetID()
 function s.initial_effect(c)
-	--send 1 Ritual Spell from your Deck to the GY, then you can add 1 LIGHT Warrior or Dragon Ritual Monster from your Deck or GY to your hand
+	--Send 1 Ritual Spell from your Deck to the GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCountLimit(1,id)
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_TOGRAVE+CATEGORY_SEARCH)
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCost(s.thcost)
-	e1:SetTarget(s.thtg)
-	e1:SetOperation(s.thop)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(s.tgcost)
+	e1:SetTarget(s.tgtg)
+	e1:SetOperation(s.tgop)
 	c:RegisterEffect(e1)
 	--Ritual Summon 1 LIGHT Warrior or Dragon Ritual Monster from your hand
-	local e2=Ritual.CreateProc(c,RITPROC_GREATER,s.filter,nil,aux.Stringid(id,1),nil,nil,nil,nil,LOCATION_HAND)
-	e2:SetCountLimit(1,{id,1})
+	local e2=Ritual.CreateProc(c,RITPROC_GREATER,
+		function(rit_c) return rit_c:IsAttribute(ATTRIBUTE_LIGHT) and rit_c:IsRace(RACE_WARRIOR|RACE_DRAGON) end,
+		nil,aux.Stringid(id,1))
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,{id,1})
 	e2:SetCost(aux.bfgcost)
 	c:RegisterEffect(e2)
 end
-function s.filter(c)
-	return c:IsRitualMonster() and c:IsAttribute(ATTRIBUTE_LIGHT) and (c:IsRace(RACE_WARRIOR) or c:IsRace(RACE_DRAGON))
-end
-function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.tgcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return c:IsDiscardable() end
-	Duel.SendtoGrave(c,REASON_COST+REASON_DISCARD)
+	Duel.SendtoGrave(c,REASON_COST|REASON_DISCARD)
 end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return Duel.IsExistingMatchingCard(aux.FilterBoolFunction(Card.IsRitualSpell),tp,LOCATION_DECK,0,1,nil) end
+function s.tgfilter(c)
+	return c:IsRitualSpell() and c:IsAbleToGrave()
+end
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
-	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK|LOCATION_GRAVE)
 end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,aux.FilterBoolFunction(Card.IsRitualSpell),tp,LOCATION_DECK,0,1,1,nil)
-	if #g>0 then
-		Duel.SendtoGrave(g,REASON_EFFECT)
-		local mg=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.filter),tp,LOCATION_DECK+LOCATION_GRAVE,0,nil)
-		if #mg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
-			Duel.BreakEffect()
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-			local sg=mg:Select(tp,1,1,nil)
-			Duel.SendtoHand(sg,nil,REASON_EFFECT)
-			Duel.ConfirmCards(1-tp,sg)
-		end
+function s.thfilter(c)
+	return c:IsRitualMonster() and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_WARRIOR|RACE_DRAGON) and c:IsAbleToHand()
+end
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 and Duel.SendtoGrave(g,REASON_EFFECT)>0 and g:GetFirst():IsLocation(LOCATION_GRAVE)
+		and Duel.IsExistingMatchingCard(aux.NecroValleyFilter(s.thfilter),tp,LOCATION_DECK|LOCATION_GRAVE,0,1,nil)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter),tp,LOCATION_DECK|LOCATION_GRAVE,0,1,1,nil)
+		Duel.BreakEffect()
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
 	end
 end
--- PHNI-JP/c101203021.lua
+++ PHNI-JP/c101203021.lua
@@ -3,108 +3,87 @@
 --Ashaki
 local s,id=GetID()
 function s.initial_effect(c)
-	--Special Summon
+	--Special Summon procedure
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(s.spproccon)
-	e1:SetTarget(s.spproctg)
-	e1:SetOperation(s.spprocop)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.selfspcon)
+	e1:SetTarget(s.selfsptg)
+	e1:SetOperation(s.selfspop)
 	c:RegisterEffect(e1)
-	--You can return this card to the hand; Special Summon 1 LIGHT Warrior or Dragon Ritual Monster from your hand or Deck
+	--Special Summon 1 LIGHT Warrior or Dragon Ritual Monster from your hand or Deck
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCountLimit(1,id)
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TODECK)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetCode(EVENT_CHAINING)
 	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(function(e,tp,eg,ep) return ep==1-tp end)
 	e2:SetCost(s.spcost)
-	e2:SetCondition(s.spcon)
 	e2:SetTarget(s.sptg)
 	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
 end
-function s.spproccon(e,c)
+function s.cfilter(c)
+	return c:IsSpell() and c:IsAbleToDeckAsCost()
+end
+function s.rescon(sg,e,tp,mg)
+	return sg:IsExists(Card.IsRitualSpell,1,nil)
+end
+function s.selfspcon(e,c)
 	if c==nil then return true end
 	local tp=e:GetHandlerPlayer()
-	local rg1=Duel.GetMatchingGroup(aux.FilterBoolFunction(Card.IsSpell),tp,LOCATION_HAND+LOCATION_GRAVE,0,e:GetHandler())
-	local rg2=Duel.GetMatchingGroup(aux.FilterBoolFunction(Card.IsRitualSpell),tp,LOCATION_HAND+LOCATION_GRAVE,0,e:GetHandler())
-	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and aux.SelectUnselectGroup(rg1,e,tp,2,2,nil,0)
-	and aux.SelectUnselectGroup(rg2,e,tp,1,1,nil,0)
+	local rg=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_HAND|LOCATION_GRAVE,0,e:GetHandler())
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and aux.SelectUnselectGroup(rg,e,tp,2,2,s.rescon,0)
 end
-function s.spproctg(e,tp,eg,ep,ev,re,r,rp)
-	local rg1=Duel.GetMatchingGroup(aux.FilterBoolFunction(Card.IsRitualSpell),tp,LOCATION_HAND+LOCATION_GRAVE,0,nil,tp)
-	local mg1=aux.SelectUnselectGroup(rg1,e,tp,1,1,nil,1,tp,HINTMSG_TODECK,nil,nil,true)
-	if #mg1>0 then
-		local sg=mg1:GetFirst()
-		local rg2=Duel.GetMatchingGroup(aux.FilterBoolFunction(Card.IsSpell),tp,LOCATION_HAND+LOCATION_GRAVE,0,e:GetHandler())
-		rg2:RemoveCard(sg)
-		local mg2=aux.SelectUnselectGroup(rg2,e,tp,1,1,nil,1,tp,HINTMSG_TODECK,nil,nil,true)
-		mg1:Merge(mg2)
-	end
-	if #mg1==2 then
-		mg1:KeepAlive()
-		e:SetLabelObject(mg1)
+function s.selfsptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
+	local rg=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_HAND|LOCATION_GRAVE,0,e:GetHandler())
+	local g=aux.SelectUnselectGroup(rg,e,tp,2,2,s.rescon,1,tp,HINTMSG_TODECK,nil,nil,true)
+	if #g>0 then
+		g:KeepAlive()
+		e:SetLabelObject(g)
 		return true
 	end
 	return false
 end
-function s.spprocop(e,tp,eg,ep,ev,re,r,rp,c)
+function s.selfspop(e,tp,eg,ep,ev,re,r,rp,c)
 	local g=e:GetLabelObject()
 	if not g then return end
 	Duel.HintSelection(g,true)
 	Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST)
 	g:DeleteGroup()
 end
-function s.spfilter(c,e,tp)
-	return c:IsRitualMonster() and c:IsAttribute(ATTRIBUTE_LIGHT) and (c:IsRace(RACE_WARRIOR) or c:IsRace(RACE_DRAGON)) and c:IsCanBeSpecialSummoned(e,0,tp,false,true)
-end
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToHandAsCost() end
-	Duel.SendtoHand(e:GetHandler(),nil,REASON_COST)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToHandAsCost() end
+	Duel.SendtoHand(c,nil,REASON_COST)
 end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return ep==1-tp
+function s.spfilter(c,e,tp)
+	return c:IsRitualMonster() and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_WARRIOR|RACE_DRAGON)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,true)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetMZoneCount(tp,e:GetHandler())>0
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,LOCATION_HAND+LOCATION_DECK)
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND|LOCATION_DECK)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_MZONE)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetMZoneCount(tp,e:GetHandler())<=0 then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
-	local tc=g:GetFirst()
-	if tc and Duel.SpecialSummon(tc,0,tp,tp,false,true,POS_FACEUP)~=0 then
-		tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
-		local e2=Effect.CreateEffect(e:GetHandler())
-		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e2:SetCode(EVENT_PHASE+PHASE_END)
-		e2:SetCountLimit(1)
-		e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-		e2:SetLabel(Duel.GetTurnCount()+1)
-		e2:SetLabelObject(tc)
-		e2:SetCondition(s.descon)
-		e2:SetOperation(s.desop)
-		Duel.RegisterEffect(e2,tp)
+	local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
+	if sc and Duel.SpecialSummon(sc,0,tp,tp,false,true,POS_FACEUP)>0 then
+		local turn_count=Duel.GetTurnCount()
+		--Shuffle it into the Deck during the End Phase of the next turn
+		aux.DelayedOperation(sc,PHASE_END,id,e,tp,
+			function(ag) Duel.SendtoDeck(ag,nil,SEQ_DECKSHUFFLE,REASON_EFFECT) end,
+			function() return Duel.GetTurnCount()==turn_count+1 end,
+			nil,2,aux.Stringid(id,2)
+		)
 	end
-end
-function s.descon(e,tp,eg,ep,ev,re,r,rp)
-	local tc=e:GetLabelObject()
-	if tc:GetFlagEffect(id)~=0 then
-		return Duel.GetTurnCount()==e:GetLabel()
-	else
-		e:Reset()
-		return false
-	end
-end
-function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=e:GetLabelObject()
-	Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
 end
